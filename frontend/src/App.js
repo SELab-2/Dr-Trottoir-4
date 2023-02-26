e517964d68a10852e2851de63287695418f1af23
sevrijss <Seppe.VanRijsselberghe@UGent.be>
2023-02-26 16:43:31 +0100

    remap backend port to work on server

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -7,7 +7,7 @@ function App() {
     const [data, setData] = useState(null);
 
     useEffect(() => {
-        fetch('http://localhost:8000/test/')
+        fetch('http://localhost:2002/test/')
             .then(res => res.json())
             .then(data => setData(data.data));
     })
