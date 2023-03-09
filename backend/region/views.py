32a222ed7597e1f99005f82b5cadfae64afe10f1
sevrijss <Seppe.VanRijsselberghe@UGent.be>
2023-03-09 18:51:40 +0100

    implement patch

--- backend/region/views.py
+++ backend/region/views.py
@@ -36,6 +36,25 @@ class RegionIndividualView(APIView):
         serializer = RegionSerializer(region_instance)
         return Response(serializer.data, status=status.HTTP_200_OK)
 
+    def patch(self, request, region_id):
+        """
+        edit info about a Region with given id
+        """
+        region_instance = Region.objects.get(id=region_id)
+        if not region_instance:
+            return Response(
+                {"res": "Object with given region id does not exist."},
+                status=status.HTTP_400_BAD_REQUEST
+            )
+        data = request.data
+        name = data.get("name")
+        print(name)
+        if not name:
+            return Response('"name" field is required', status.HTTP_400_BAD_REQUEST)
+        region_instance.region = name
+        region_instance.save()
+        return Response(status=status.HTTP_204_NO_CONTENT)
+
 
 class AllRegionsView(APIView):
     def get(self, request):
