0810b087ae77dc4780f3260772768d9b324564b2
sevrijss <Seppe.VanRijsselberghe@UGent.be>
2023-03-09 21:16:16 +0100

    post implemented testing required

--- backend/building_on_tour/views.py
+++ backend/building_on_tour/views.py
@@ -6,6 +6,40 @@ from rest_framework import status
 from rest_framework.response import Response
 from rest_framework.views import APIView
 
+
+
+class Default(APIView):
+    def post(self, request):
+        """
+        Create a new BuildingOnTour with data from post
+        """
+        data = request.data
+        tour = data.get("tour")
+        building = data.get("tour")
+        index = data.get("tour")
+        if not tour or not building or not index:
+            return Response('"tour", "building" and "index" fields are required', status.HTTP_400_BAD_REQUEST)
+        # checking if tour id exists
+        t = Tour.objects.filter(id=tour)
+        if len(t) != 1:
+            return Response('"tour" field was not valid', status.HTTP_400_BAD_REQUEST)
+        tour_instance = t[0]
+        # checking if building id exists
+        b = Building.objects.filter(id=building)
+        if len(b) != 1:
+            return Response('"building" field was not valid', status.HTTP_400_BAD_REQUEST)
+        building_isntance = b[0]
+        newTour = BuildingOnTour(tour=tour_instance, building=building_isntance, index=index)
+        try:
+            newTour.full_clean()
+        except ValidationError as e:
+            print(e)
+            return Response(e.message_dict["__all__"], status.HTTP_400_BAD_REQUEST)
+        newTour.save()
+        serializer = BuildingTourSerializer(newTour)
+        return Response(serializer.data, status.HTTP_201_CREATED)
+
+
 class AllBuildingToursView(APIView):
     def get(self, request):
         """
