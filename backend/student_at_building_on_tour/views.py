ff9da4a9a0b09574c96f9bf3881b6b3ecbfe7b71
sevrijss <Seppe.VanRijsselberghe@UGent.be>
2023-03-11 16:50:39 +0100

    suburl for student filtering implemented

--- backend/student_at_building_on_tour/views.py
+++ backend/student_at_building_on_tour/views.py
@@ -1,8 +1,44 @@
-from base.models import StudentAtBuildingOnTour
+from base.models import StudentAtBuildingOnTour, User
 from base.serializers import StudBuildTourSerializer
 from rest_framework import status
 from rest_framework.response import Response
 from rest_framework.views import APIView
+
+
+#
+#
+# class Default(APIView):
+#     def post(self, request):
+#         data = request.data
+#         building = data.get("building")
+#         tour = data.get("tour")
+#         date = data.get("date")
+#         if not building or not tour or not date:
+#             return Response('"building", "tour" and "date" fields are required', status.HTTP_400_BAD_REQUEST)
+#         candidates = Building.objects.filter(id=building)
+#         if len(candidates) != 1:
+#             return Response('"Building" field was not valid', status=status.HTTP_400_BAD_REQUEST)
+#         building_instance = candidates[0]
+#         candidates2 = Tour.objects.filter(id=tour)
+#         if len(candidates2) != 1:
+#             return Response('"Tour" field was not valid', status=status.HTTP_400_BAD_REQUEST)
+#         tour_instance = candidates2[0]
+#
+#         sbt = StudentAtBuildingOnTour(building=building_instance, tour=tour_instance, date=date)
+#         sbt.save()
+#         return Response(status=status.HTTP_201_CREATED)
+
+
+class build_tour_per_studentView(APIView):
+    def get(self, request, student_id):
+        stud_candidates = User.objects.filter(id=student_id)
+        if len(stud_candidates) != 1:
+            return Response('no entries for given student is', status=status.HTTP_400_BAD_REQUEST)
+        student = stud_candidates[0]
+        pictureBuilding_instance = StudentAtBuildingOnTour.objects.filter(student=student)
+
+        serializer = StudBuildTourSerializer(pictureBuilding_instance, many=True)
+        return Response(serializer.data, status=status.HTTP_200_OK)
 
 
 class stud_build_tourIndividualView(APIView):
@@ -29,8 +65,6 @@ class stud_build_tourIndividualView(APIView):
             )
         possible_instances[0].delete()
         return Response(status=status.HTTP_204_NO_CONTENT)
-
-
 
 
 class AllView(APIView):
