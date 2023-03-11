1a7256c65b0bcdc8a454f1fc1cc9b026a5066a48
sevrijss <Seppe.VanRijsselberghe@UGent.be>
2023-03-11 16:29:38 +0100

    start individual view

--- backend/student_at_building_on_tour/views.py
+++ backend/student_at_building_on_tour/views.py
@@ -4,6 +4,35 @@ from rest_framework import status
 from rest_framework.response import Response
 from rest_framework.views import APIView
 
+
+class stud_build_tourIndividualView(APIView):
+    def get(self, request, id):
+        pictureBuilding_instance = StudentAtBuildingOnTour.objects.filter(id=id)
+
+        if len(pictureBuilding_instance) != 1:
+            return Response(
+                {"res": "Object with given id does not exist."},
+                status=status.HTTP_400_BAD_REQUEST
+            )
+        serializer = StudBuildTourSerializer(pictureBuilding_instance[0])
+        return Response(serializer.data, status=status.HTTP_200_OK)
+
+    def delete(self, request, id):
+        """
+        delete from the database
+        """
+        possible_instances = StudentAtBuildingOnTour.objects.filter(id=id)
+        if len(possible_instances) != 1:
+            return Response(
+                {"res": "Object with given id does not exist."},
+                status=status.HTTP_400_BAD_REQUEST
+            )
+        possible_instances[0].delete()
+        return Response(status=status.HTTP_204_NO_CONTENT)
+
+
+
+
 class AllView(APIView):
     def get(self, request):
         """
