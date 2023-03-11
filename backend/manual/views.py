4b2a4bc954a1c13e48489374f6f13cdcd93377d8
sevrijss <Seppe.VanRijsselberghe@UGent.be>
2023-03-11 18:24:04 +0100

    post works

--- backend/manual/views.py
+++ backend/manual/views.py
@@ -1,12 +1,36 @@
 from base.models import Manual, Building
 from base.serializers import ManualSerializer
+from django.core.exceptions import ValidationError
 from rest_framework import status
 from rest_framework.response import Response
 from rest_framework.views import APIView
 
 
 class Default(APIView):
-    pass
+    def post(self, request):
+        data = request.data
+        building = data.get("building")
+        file = request.FILES.get("file")
+        # version is optional
+        version = data.get("version_number")
+        if not building or not file:
+            return Response('"building" and "file" fields are required',
+                            status.HTTP_400_BAD_REQUEST)
+        if not version:
+            # latest version ophalen
+            instances = Manual.objects.all()
+            version = max([instance.version_number for instance in instances]) + 1
+        candidates = Building.objects.filter(id=building)
+        if len(candidates) != 1:
+            return Response('"Building" field was not valid', status=status.HTTP_400_BAD_REQUEST)
+        pb = Manual(building=candidates[0], file=file, version_number=version)
+        try:
+            pb.full_clean()
+        except ValidationError as e:
+            return Response(e.message_dict, status=status.HTTP_400_BAD_REQUEST)
+        pb.save()
+        serializer = ManualSerializer(pb)
+        return Response(serializer.data, status=status.HTTP_201_CREATED)
 
 
 class ManualView(APIView):
