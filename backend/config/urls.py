8358ba38a45f6c49ef294a545764c27b9c5fca04
sevrijss <Seppe.VanRijsselberghe@UGent.be>
2023-03-11 14:09:39 +0100

    fix media urls

--- backend/config/urls.py
+++ backend/config/urls.py
@@ -13,26 +13,28 @@ Including another URLconf
     1. Import the include() function: from django.urls import include, path
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
+from authentication import urls as authentication_urls
+from building import urls as building_urls
+from django.conf.urls.static import static
 from django.contrib import admin
 from django.urls import path, include, re_path, reverse_lazy
 from django.views.generic import RedirectView
-from rest_framework.routers import DefaultRouter
-
-from authentication import urls as authentication_urls
-from building import urls as building_urls
 from picture_building import urls as picture_building_urls
+from rest_framework.routers import DefaultRouter
 from users import urls as user_urls
+
+from .settings import MEDIA_URL, MEDIA_ROOT
 
 router = DefaultRouter()
 # NOTE: This is a temporary view to test 'IsAuthenticated"
-router.register(r'users', UserViewSet)
+# router.register(r'users', UserViewSet)
 
 urlpatterns = [
-    path('admin/', admin.site.urls),
-    path('', include(router.urls)),
-    path('authentication/', include(authentication_urls)),
-    path('building/', include(building_urls)),
-    path('picture_building/', include(picture_building_urls)),
-    path('user/', include(user_urls)),
-    re_path(r'^$', RedirectView.as_view(url=reverse_lazy('api'), permanent=False)),
-]
+                  path('admin/', admin.site.urls),
+                  path('', include(router.urls)),
+                  path('authentication/', include(authentication_urls)),
+                  path('building/', include(building_urls)),
+                  path('picture_building/', include(picture_building_urls)),
+                  path('user/', include(user_urls)),
+                  re_path(r'^$', RedirectView.as_view(url=reverse_lazy('api'), permanent=False)),
+              ] + static(MEDIA_URL, document_root=MEDIA_ROOT)
