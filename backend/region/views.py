9d49673b098302bb854b4c8f32aeba0df0fa0fbf
sevrijss <Seppe.VanRijsselberghe@UGent.be>
2023-03-12 11:52:51 +0100

    region cleanup with helper functions

--- backend/region/views.py
+++ backend/region/views.py
@@ -1,33 +1,32 @@
 from base.models import Region
 from base.serializers import RegionSerializer
-from django.core.exceptions import NON_FIELD_ERRORS
-from django.core.exceptions import ValidationError
-from rest_framework import status
-from rest_framework.response import Response
+from rest_framework import permissions
 from rest_framework.views import APIView
+from util.request_response_util import *
 
 
 class Default(APIView):
+    permission_classes = [permissions.IsAuthenticated]
+
     def post(self, request):
         """
-        Create a new region with name from post
+        Create a new region
         """
-        data = request.data
-        name = data.get("name")
-        if not name:
-            return Response('"name" field is required', status.HTTP_400_BAD_REQUEST)
-        b = Region(region=name)
-        try:
-            b.full_clean()
-        except ValidationError as e:
-            print(e)
-            return Response(e.message_dict, status.HTTP_400_BAD_REQUEST)
-        b.save()
-        serializer = RegionSerializer(b)
-        return Response(serializer.data, status.HTTP_201_CREATED)
+        data = request.data.dict()
+
+        region_instance = Region()
+        for key in data.keys():
+            if key in vars(region_instance):
+                setattr(region_instance, key, data[key])
+        if r := try_full_clean_and_save(region_instance):
+            return r
+
+        serializer = RegionSerializer(region_instance)
+        return post_success(serializer)
 
 
 class RegionIndividualView(APIView):
+    permission_classes = [permissions.IsAuthenticated]
 
     def get(self, request, region_id):
         """
@@ -36,55 +35,46 @@ class RegionIndividualView(APIView):
         region_instance = Region.objects.filter(id=region_id)
 
         if len(region_instance) != 1:
-            return Response(
-                {"res": "Object with given region id does not exist."},
-                status=status.HTTP_400_BAD_REQUEST
-            )
+            return bad_request(object_name="Region")
         serializer = RegionSerializer(region_instance[0])
-        return Response(serializer.data, status=status.HTTP_200_OK)
+        return get_success(serializer)
 
     def patch(self, request, region_id):
         """
-        edit info about a Region with given id
+        Edit Region with given id
         """
         region_instances = Region.objects.filter(id=region_id)
         if len(region_instances) != 1:
-            return Response(
-                {"res": "Object with given region id does not exist."},
-                status=status.HTTP_400_BAD_REQUEST
-            )
+            return bad_request(object_name="Region")
         region_instance = region_instances[0]
-        data = request.data
-        name = data.get("name")
-        if not name:
-            return Response('"name" field is required', status.HTTP_400_BAD_REQUEST)
-        region_instance.region = name
-        try:
-            region_instance.full_clean()
-        except ValidationError as e:
-            return Response(e.message_dict, status=status.HTTP_400_BAD_REQUEST)
-        region_instance.save()
-        return Response(status=status.HTTP_204_NO_CONTENT)
+        data = request.data.dict()
+        for key in data.keys():
+            if key in vars(region_instance):
+                setattr(region_instance, key, data[key])
+
+        if r := try_full_clean_and_save(region_instance):
+            return r
+
+        return patch_succes(RegionSerializer(region_instance))
 
     def delete(self, request, region_id):
         """
-        delete a region from the database
+        delete a region with given id
         """
         region_instances = Region.objects.filter(id=region_id)
         if len(region_instances) != 1:
-            return Response(
-                {"res": "Object with given region id does not exist."},
-                status=status.HTTP_400_BAD_REQUEST
-            )
+            return bad_request(object_name="Region")
         region_instances[0].delete()
-        return Response(status=status.HTTP_204_NO_CONTENT)
+        return delete_succes()
 
 
 class AllRegionsView(APIView):
+    permission_classes = [permissions.IsAuthenticated]
+
     def get(self, request):
         """
         Get all regions
         """
         region_instances = Region.objects.all()
         serializer = RegionSerializer(region_instances, many=True)
-        return Response(serializer.data, status=status.HTTP_200_OK)
+        return get_succes(serializer)
