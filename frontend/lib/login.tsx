6db8f7364f1ec16ef10497628126af2b56738680
Emma N <neirinck.emma@ugent.be>
2023-03-12 21:49:05 +0100

    interceptors implemented

--- frontend/lib/login.tsx
+++ frontend/lib/login.tsx
@@ -1,39 +1,28 @@
-import {Router, useRouter} from 'next/router';
-
+import api from '../lib/axios';
 const login = async (email: string, password: string, router : any): Promise<void> => {
 
-  const host : string = `http://${process.env.NEXT_PUBLIC_API_HOST}:${process.env.NEXT_PUBLIC_API_PORT}${process.env.NEXT_PUBLIC_API_LOGIN}`;
+  const host : string =
+      `http://${process.env.NEXT_PUBLIC_API_HOST}:${process.env.NEXT_PUBLIC_API_PORT}/user/login/`;
   console.log(JSON.stringify({email, password}))
-
-  const axios = require('axios');
 
   const data = {
     email: email,
     password: password,
   };
 
-  axios.post(host, data, { withCredentials: true })
+  api.post(host, data, {
+      headers: { 'Content-Type': 'application/json' },
+      withCredentials: true
+  })
     .then((response: { status : number, data: any; })  => {
       console.log(response);
       if (response.status == 200) {
-          router.push("/welcome");
+          router.push('/welcome');
       }
     })
     .catch((error: any) => {
       console.log(error);
     });
-
-  // const resp = await fetch(host, {
-  //   method: "POST",
-  //   headers: { "Content-Type": "application/json" },
-  //   body: JSON.stringify({ email, password }),
-  // });
-  // if (resp.status !== 200) {
-  //   console.log("No can do")
-  // } else {
-  //   console.log(resp)
-  //   router.push("/welcome");
-  // }
 };
 
 export default login;
