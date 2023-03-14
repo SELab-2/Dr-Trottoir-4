f7b7678ed61b1cb2bc3ff7901cd42c34aef023d4
n00bS-oWn-m3 <sebastiaan@onyx.be>
2023-03-14 17:31:14 +0100

    #18 do some tests

--- formatting_test_file.py
+++ formatting_test_file.py
@@ -1,17 +1,26 @@
-j = [1, 2, 3]
+j = [1,
+     2,
+     3]
 
-if 1 == 1 and 2 == 2:
+if 1 ==1 \
+        and 2 == 2:
     pass
 
 
 def foo():
-    print("All the newlines above me should be deleted!")
+
+
+    print("All "
+          "the newlines above me should be deleted!")
 
 
 if True:
     print("No newline above me!")
 
-    print("There is a newline above me, and that's OK!")
+
+    print("There is a newline above "
+          ""
+          "me, and that's OK!")
 
 
 class Point:
