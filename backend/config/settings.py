745ffd2378f11a48a594119d99d5e97cf6e63f6d
sevrijss <Seppe.VanRijsselberghe@UGent.be>
2023-03-15 21:27:46 +0100

    cors allow

--- backend/config/settings.py
+++ backend/config/settings.py
@@ -116,8 +116,8 @@ MIDDLEWARE = [
 
 CORS_ALLOW_ALL_ORIGINS = False
 CORS_ALLOW_CREDENTIALS = True
-CORS_ALLOWED_ORIGINS = ['http://localhost:2002', 'http://localhost:443', 'http://localhost:80']
-CSRF_TRUSTED_ORIGINS = ['http://localhost:2002', 'http://localhost:443', 'http://localhost:80']
+CORS_ALLOWED_ORIGINS = ['http://localhost:2002', 'http://localhost:443', 'http://localhost:80', "http://localhost"]
+CSRF_TRUSTED_ORIGINS = ['http://localhost:2002', 'http://localhost:443', 'http://localhost:80', "http://localhost"]
 
 ROOT_URLCONF = 'config.urls'
 
