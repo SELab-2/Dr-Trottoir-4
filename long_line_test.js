07325c85bb0d4cacab0c61bca63600e6013a26ec
n00bS-oWn-m3 <sebastiaan@onyx.be>
2023-03-18 16:56:05 +0100

    #18 test long lines

---
+++ long_line_test.js
@@ -0,0 +1,7 @@
+const function_with_long_body_and_lots_of_parameters = function(first, second, third, fourth, fifth, sixth, seventh, eighth, nineth, tenth) {
+    if (first + first === second && third + third === fourth && fifth + fifth === sixth && seventh + seventh === eighth && nineth + nineth === tenth) {
+        console.log("Hooray, this line is wayyyyyyyyy tooooooo long, so let's see if you can fix this. I believe in you :DDDDDDDDDDDD");
+    }
+}
+
+function_with_long_body_and_lots_of_parameters(1, 2, 4, 8, 16, 32, 64, 128, 256, 512)
---
+++ long_line_test.py
@@ -0,0 +1,5 @@
+def function_with_long_body_and_lots_of_parameters(first, second, third, fourth, fifth, sixth, seventh, eighth, nineth, tenth):
+    if first + first == second and third + third == fourth and fifth + fifth == sixth and seventh + seventh == eighth and nineth + nineth == tenth:
+        print("Hooray, this line is wayyyyyyyyy tooooooo long, so let's see if you can fix this. I believe in you :DDDDDDDDDDDD")
+
+function_with_long_body_and_lots_of_parameters(1, 2, 4, 8, 16, 32, 64, 128, 256, 512)
