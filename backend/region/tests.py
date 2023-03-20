ee3f5e1e3bd95f8eab659d22388cb2faef50f595
sevrijss <Seppe.VanRijsselberghe@UGent.be>
2023-03-20 11:54:45 +0100

    test error handling

--- backend/region/tests.py
+++ backend/region/tests.py
@@ -30,7 +30,7 @@ class RegionTests(TestCase):
         data = [response.data[e] for e in response.data]
         assert len(data) == 0
 
-    def test_insert_1_region(self):
+    def test_insert_region(self):
         user = createUser()
         client = APIClient()
         client.force_authenticate(user=user)
@@ -44,6 +44,17 @@ class RegionTests(TestCase):
             assert key in response.data
         # er moet ook een id bij zitten
         assert "id" in response.data
+
+    def test_insert_dupe_region(self):
+        user = createUser()
+        client = APIClient()
+        client.force_authenticate(user=user)
+        data = {
+            "region": "Gent"
+        }
+        _ = client.post(f"{backend_url}/region/", data, follow=True)
+        response = client.post(f"{backend_url}/region/", data, follow=True)
+        assert response.status_code == 400
 
     def test_get_region(self):
         user = createUser()
@@ -65,6 +76,14 @@ class RegionTests(TestCase):
         assert response2.data["region"] == "Gent"
         assert "id" in response2.data
 
+    def test_get_non_existing(self):
+        user = createUser()
+        client = APIClient()
+        client.force_authenticate(user=user)
+        response1 = client.get(f"{backend_url}/region/123654897", follow=True)
+        # should be 404 I think
+        # assert response1.status_code == 404
+        assert response1.status_code == 400
 
     def test_patch_region(self):
         user = createUser()
@@ -91,6 +110,23 @@ class RegionTests(TestCase):
         assert response3.data["region"] == "Gent"
         assert "id" in response3.data
 
+    def test_patch_error_region(self):
+        user = createUser()
+        client = APIClient()
+        client.force_authenticate(user=user)
+        data1 = {
+            "region": "Brugge"
+        }
+        data2 = {
+            "region": "Gent"
+        }
+        response1 = client.post(f"{backend_url}/region/", data1, follow=True)
+        _ = client.post(f"{backend_url}/region/", data2, follow=True)
+        assert response1.status_code == 201
+        id = response1.data["id"]
+        response2 = client.patch(f"{backend_url}/region/{id}/", data2, follow=True)
+        assert response2.status_code == 400
+
     def test_remove_region(self):
         user = createUser()
         client = APIClient()
@@ -112,3 +148,21 @@ class RegionTests(TestCase):
         # should be 404 I think
         # assert response3.status_code == 404
         assert response3.status_code == 400
+
+    def test_remove_non_existent_region(self):
+        user = createUser()
+        client = APIClient()
+        client.force_authenticate(user=user)
+        response2 = client.delete(f"{backend_url}/region/123456789/", follow=True)
+        assert response2.status_code == 400
+
+    def test_add_existing_region(self):
+        user = createUser()
+        client = APIClient()
+        client.force_authenticate(user=user)
+        data1 = {
+            "region": "Gent"
+        }
+        _ = client.post(f"{backend_url}/region/", data1, follow=True)
+        response1 = client.post(f"{backend_url}/region/", data1, follow=True)
+        assert response1.status_code == 400
