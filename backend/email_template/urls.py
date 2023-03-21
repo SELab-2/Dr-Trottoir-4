6ca0ca92eebe0f64ec258d431fc954d88275a392
TiboStr <TiboStr@users.noreply.github.com>
2023-03-21 00:16:59 +0000

    Auto formatted code

--- backend/email_template/urls.py
+++ backend/email_template/urls.py
@@ -1,13 +1,9 @@
 from django.urls import path
 
-from .views import (
-    DefaultEmailTemplate,
-    EmailTemplateIndividualView,
-    EmailTemplateAllView
-)
+from .views import DefaultEmailTemplate, EmailTemplateIndividualView, EmailTemplateAllView
 
 urlpatterns = [
     path("all/", EmailTemplateAllView.as_view()),
     path("<int:email_template_id>/", EmailTemplateIndividualView.as_view()),
-    path("", DefaultEmailTemplate.as_view())
+    path("", DefaultEmailTemplate.as_view()),
 ]
