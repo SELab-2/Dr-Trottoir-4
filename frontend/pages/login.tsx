e0eb03c31bcbd144e2dff4d93efa33d76b77c3ff
Emma N <neirinck.emma@ugent.be>
2023-03-21 22:56:32 +0100

    added bootstrap to login page

--- frontend/pages/login.tsx
+++ frontend/pages/login.tsx
@@ -2,10 +2,9 @@ import BaseHeader from "@/components/header/BaseHeader";
 import styles from "styles/Login.module.css";
 import Image from "next/image";
 import filler_logo from "../public/filler_logo.png";
-import Link from "next/link";
 import login from "../lib/login";
-import { FormEvent, useState } from "react";
-import { useRouter } from "next/router";
+import {FormEvent, useState} from "react";
+import {useRouter} from "next/router";
 
 export default function Login() {
     const router = useRouter();
@@ -23,48 +22,57 @@ export default function Login() {
 
     return (
         <>
-            <BaseHeader />
-            <div className={styles.main_container}>
-                <div className={styles.filler_container}>
-                    <Image src={filler_logo} alt="My App Logo" className={styles.filler_image} />
-                </div>
-                <div className={styles.login_container}>
-                    <p className={styles.title}>Login.</p>
-                    <form onSubmit={handleSubmit}>
-                        <label className={styles.text} htmlFor="email">
-                            E-mailadres:
-                        </label>
-                        <input
-                            type="text"
-                            className={styles.input}
-                            value={username}
-                            onChange={(e: React.ChangeEvent<HTMLInputElement>) => setUsername(e.target.value)}
-                        />
-                        <label className={styles.text} htmlFor="password">
-                            Wachtwoord:
-                        </label>
-                        <input
-                            type="password"
-                            className={styles.input}
-                            value={password}
-                            onChange={(e: React.ChangeEvent<HTMLInputElement>) => setPassword(e.target.value)}
-                            required
-                        />
-                        <button className={styles.button} type="submit">
-                            Login
-                        </button>
-                    </form>
-                    <p className={styles.text}>
-                        <Link href="/reset-password">
-                            <u>Forgot Password</u>
-                        </Link>
-                    </p>
-                    <p className={styles.text}>
-                        Don't have an account?{" "}
-                        <Link href="/signup">
-                            <u>Sign up here</u>
-                        </Link>
-                    </p>
+            <BaseHeader/>
+            <div className="container py-5 h-100">
+                <div className="row d-flex justify-content-center align-items-center h-100">
+                    <div className="col col-xl-10">
+                        <div className="card">
+                            <div className="row g-0">
+                                <div className="col-md-6 col-lg-5 d-none d-md-block">
+                                    <Image src={filler_logo} alt="My App Logo" className={styles.filler_image}/>
+                                </div>
+                                <div className="col-md-6 col-lg-7 d-flex align-items-center">
+                                    <div className="card-body p-4 p-lg-5 text-black">
+                                        <form onSubmit={handleSubmit}>
+
+                                            <div className="d-flex align-items-center mb-3 pb-1">
+                                                <i className="fas fa-cubes fa-2x me-3"/>
+                                                <span className="h1 fw-bold mb-0">Login.</span>
+                                            </div>
+
+                                            <div className="form-outline mb-4">
+                                                <label className="form-label">E-mailadres</label>
+                                                <input type="email"
+                                                       className={`form-control form-control-lg ${styles.input}`}
+                                                       value={username}
+                                                       onChange={(e: React.ChangeEvent<HTMLInputElement>) => setUsername(e.target.value)}
+                                                />
+                                            </div>
+
+                                            <div className="form-outline mb-4">
+                                                <label className="form-label">Wachtwoord</label>
+                                                <input type="password"
+                                                       className={`form-control form-control-lg ${styles.input}`}
+                                                       value={password}
+                                                       onChange={(e: React.ChangeEvent<HTMLInputElement>) => setPassword(e.target.value)}
+                                                       required/>
+                                            </div>
+
+                                            <div className="pt-1 mb-4">
+                                                <button className={`btn btn-dark btn-lg btn-block ${styles.button}`} type="button">Login
+                                                </button>
+                                            </div>
+
+                                            <a className="small text-muted" href="/reset-password">Wachtwoord vergeten?</a>
+                                            <p className="mb-5 pb-lg-2">Heb je nog geen account? <a href="/signup"
+                                            >Registreer je hier!</a></p>
+                                        </form>
+
+                                    </div>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
                 </div>
             </div>
         </>
