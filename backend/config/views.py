d0bd66839c8f6f7baa6d62060ae4e40511d8b8e1
jonathancasters <jonathancasters284@gmail.com>
2023-03-22 17:34:49 +0100

    added documentation for root endpoint

--- backend/config/views.py
+++ backend/config/views.py
@@ -3,10 +3,13 @@ from rest_framework.permissions import IsAuthenticated
 from rest_framework.response import Response
 from rest_framework.views import APIView
 
+from drf_spectacular.utils import extend_schema
+
 
 class RootDefault(APIView):
     permission_classes = [IsAuthenticated]
 
+    @extend_schema(responses={200: None, 400: None, 403: None, 401: None})
     def get(self):
         return Response(
             {"message", "Hello from the DrTrottoir API!"},
