ddd6ef9b84aa6738f7e74779f8869a4b2b6a08f0
simvadnbu <simivdb@gmail.com>
2023-03-22 16:36:26 +0100

    some final cleanups

---
+++ backend/config/secrets.sample.py
@@ -0,0 +1,5 @@
+# This file is an example of how the secrets.py file should look like
+DJANGO_SECRET_KEY = "example-key-for-hashing"
+
+SECRET_EMAIL_USER = "example@gmail.com"
+SECRET_EMAIL_USER_PSWD = "password"
