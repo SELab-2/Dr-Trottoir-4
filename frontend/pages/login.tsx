677c186a22996aa23d579eff1860237950f7687d
emneirin <emneirin@users.noreply.github.com>
2023-03-22 19:35:54 +0000

    Auto formatted code

--- frontend/pages/login.tsx
+++ frontend/pages/login.tsx
@@ -40,52 +40,64 @@ export default function Login() {
 
     return (
         <>
-            <BaseHeader/>
+            <BaseHeader />
             <div className="container py-5 h-100">
                 <div className="row d-flex justify-content-center align-items-center h-100">
                     <div className="col col-xl-10">
                         <div className="card">
                             <div className="row g-0">
                                 <div className="col-md-6 col-lg-5 d-none d-md-block">
-                                    <Image src={filler_image} alt="My App Logo" className={styles.filler_image}/>
+                                    <Image src={filler_image} alt="My App Logo" className={styles.filler_image} />
                                 </div>
                                 <div className="col-md-6 col-lg-7 d-flex align-items-center">
                                     <div className="card-body p-4 p-lg-5 text-black">
                                         <form onSubmit={handleSubmit}>
-
                                             <div className="d-flex align-items-center mb-3 pb-1">
-                                                <i className="fas fa-cubes fa-2x me-3"/>
+                                                <i className="fas fa-cubes fa-2x me-3" />
                                                 <span className="h1 fw-bold mb-0">Login.</span>
                                             </div>
 
                                             <div className="form-outline mb-4">
                                                 <label className="form-label">E-mailadres</label>
-                                                <input type="email"
-                                                       className={`form-control form-control-lg ${styles.input}`}
-                                                       value={username}
-                                                       onChange={(e: React.ChangeEvent<HTMLInputElement>) => setUsername(e.target.value)}
+                                                <input
+                                                    type="email"
+                                                    className={`form-control form-control-lg ${styles.input}`}
+                                                    value={username}
+                                                    onChange={(e: React.ChangeEvent<HTMLInputElement>) =>
+                                                        setUsername(e.target.value)
+                                                    }
                                                 />
                                             </div>
 
                                             <div className="form-outline mb-4">
                                                 <label className="form-label">Wachtwoord</label>
-                                                <input type="password"
-                                                       className={`form-control form-control-lg ${styles.input}`}
-                                                       value={password}
-                                                       onChange={(e: React.ChangeEvent<HTMLInputElement>) => setPassword(e.target.value)}
-                                                       required/>
+                                                <input
+                                                    type="password"
+                                                    className={`form-control form-control-lg ${styles.input}`}
+                                                    value={password}
+                                                    onChange={(e: React.ChangeEvent<HTMLInputElement>) =>
+                                                        setPassword(e.target.value)
+                                                    }
+                                                    required
+                                                />
                                             </div>
 
                                             <div className="pt-1 mb-4">
-                                                <button className={`btn btn-dark btn-lg btn-block ${styles.button}`} type="submit">Login
+                                                <button
+                                                    className={`btn btn-dark btn-lg btn-block ${styles.button}`}
+                                                    type="submit"
+                                                >
+                                                    Login
                                                 </button>
                                             </div>
 
-                                            <a className="small text-muted" href="/reset-password">Wachtwoord vergeten?</a>
-                                            <p className="mb-5 pb-lg-2">Heb je nog geen account? <a href="/signup"
-                                            >Registreer je hier!</a></p>
+                                            <a className="small text-muted" href="/reset-password">
+                                                Wachtwoord vergeten?
+                                            </a>
+                                            <p className="mb-5 pb-lg-2">
+                                                Heb je nog geen account? <a href="/signup">Registreer je hier!</a>
+                                            </p>
                                         </form>
-
                                     </div>
                                 </div>
                             </div>
--- frontend/pages/signup.tsx
+++ frontend/pages/signup.tsx
@@ -1,5 +1,5 @@
-import {useRouter} from "next/router";
-import React, {FormEvent, useState} from "react";
+import { useRouter } from "next/router";
+import React, { FormEvent, useState } from "react";
 import BaseHeader from "@/components/header/BaseHeader";
 import styles from "@/styles/Login.module.css";
 import Image from "next/image";
@@ -31,82 +31,101 @@ export default function Signup() {
 
     return (
         <>
-            <BaseHeader/>
+            <BaseHeader />
             <div className="container py-5 h-100">
                 <div className="row d-flex justify-content-center align-items-center h-100">
                     <div className="col col-xl-10">
                         <div className="card">
                             <div className="row g-0">
                                 <div className="col-md-6 col-lg-5 d-none d-md-block">
-                                    <Image src={fire} alt="My App Logo" className={styles.filler_image}/>
+                                    <Image src={fire} alt="My App Logo" className={styles.filler_image} />
                                 </div>
                                 <div className="col-md-6 col-lg-7 d-flex align-items-center">
                                     <div className="card-body p-4 p-lg-5 text-black">
                                         <form onSubmit={handleSubmit}>
-
                                             <div className="d-flex align-items-center mb-3 pb-1">
-                                                <i className="fas fa-cubes fa-2x me-3"/>
+                                                <i className="fas fa-cubes fa-2x me-3" />
                                                 <span className="h1 fw-bold mb-0">Sign up.</span>
                                             </div>
 
                                             <div className="form-outline mb-4">
                                                 <label className="form-label">Voornaam</label>
-                                                <input type="text"
-                                                       className={`form-control form-control-lg ${styles.input}`}
-                                                       value={firstname}
-                                                       onChange={(e: React.ChangeEvent<HTMLInputElement>) => setFirstname(e.target.value)}
-                                                       required
+                                                <input
+                                                    type="text"
+                                                    className={`form-control form-control-lg ${styles.input}`}
+                                                    value={firstname}
+                                                    onChange={(e: React.ChangeEvent<HTMLInputElement>) =>
+                                                        setFirstname(e.target.value)
+                                                    }
+                                                    required
                                                 />
                                             </div>
 
                                             <div className="form-outline mb-4">
                                                 <label className="form-label">Achternaam</label>
-                                                <input type="text"
-                                                       className={`form-control form-control-lg ${styles.input}`}
-                                                       value={lastname}
-                                                       onChange={(e: React.ChangeEvent<HTMLInputElement>) => setLastname(e.target.value)}
-                                                       required
+                                                <input
+                                                    type="text"
+                                                    className={`form-control form-control-lg ${styles.input}`}
+                                                    value={lastname}
+                                                    onChange={(e: React.ChangeEvent<HTMLInputElement>) =>
+                                                        setLastname(e.target.value)
+                                                    }
+                                                    required
                                                 />
                                             </div>
 
                                             <div className="form-outline mb-4">
                                                 <label className="form-label">E-mailadres</label>
-                                                <input type="email"
-                                                       className={`form-control form-control-lg ${styles.input}`}
-                                                       value={email}
-                                                       onChange={(e: React.ChangeEvent<HTMLInputElement>) => setEmail(e.target.value)}
-                                                       required
+                                                <input
+                                                    type="email"
+                                                    className={`form-control form-control-lg ${styles.input}`}
+                                                    value={email}
+                                                    onChange={(e: React.ChangeEvent<HTMLInputElement>) =>
+                                                        setEmail(e.target.value)
+                                                    }
+                                                    required
                                                 />
                                             </div>
 
                                             <div className="form-outline mb-4">
                                                 <label className="form-label">Wachtwoord</label>
-                                                <input type="password"
-                                                       className={`form-control form-control-lg ${styles.input}`}
-                                                       value={password1}
-                                                       onChange={(e: React.ChangeEvent<HTMLInputElement>) => setPassword1(e.target.value)}
-                                                       required/>
+                                                <input
+                                                    type="password"
+                                                    className={`form-control form-control-lg ${styles.input}`}
+                                                    value={password1}
+                                                    onChange={(e: React.ChangeEvent<HTMLInputElement>) =>
+                                                        setPassword1(e.target.value)
+                                                    }
+                                                    required
+                                                />
                                             </div>
 
                                             <div className="form-outline mb-4">
                                                 <label className="form-label">Bevestig wachtwoord</label>
-                                                <input type="password"
-                                                       className={`form-control form-control-lg ${styles.input}`}
-                                                       value={password2}
-                                                       onChange={(e: React.ChangeEvent<HTMLInputElement>) => setPassword2(e.target.value)}
-                                                       required/>
+                                                <input
+                                                    type="password"
+                                                    className={`form-control form-control-lg ${styles.input}`}
+                                                    value={password2}
+                                                    onChange={(e: React.ChangeEvent<HTMLInputElement>) =>
+                                                        setPassword2(e.target.value)
+                                                    }
+                                                    required
+                                                />
                                             </div>
 
                                             <div className="pt-1 mb-4">
-                                                <button className={`btn btn-dark btn-lg btn-block ${styles.button}`}
-                                                        type="submit">Sign up
+                                                <button
+                                                    className={`btn btn-dark btn-lg btn-block ${styles.button}`}
+                                                    type="submit"
+                                                >
+                                                    Sign up
                                                 </button>
                                             </div>
 
-                                            <p className="mb-5 pb-lg-2">Heb je al een account? <a href="/login"
-                                            >Ga naar login</a></p>
+                                            <p className="mb-5 pb-lg-2">
+                                                Heb je al een account? <a href="/login">Ga naar login</a>
+                                            </p>
                                         </form>
-
                                     </div>
                                 </div>
                             </div>
