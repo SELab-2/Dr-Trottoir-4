826848f58e29f59984d21ed882c5a41738ec25cd
Tibo Stroo <tibo.stroo@hotmail.be>
2023-03-22 18:31:16 +0100

    Added API description to API root (#103)

--- backend/config/views.py
+++ backend/config/views.py
@@ -9,7 +9,8 @@ from drf_spectacular.utils import extend_schema
 class RootDefault(APIView):
     permission_classes = [IsAuthenticated]
 
-    @extend_schema(responses={200: None, 400: None, 403: None, 401: None})
+    @extend_schema(responses={200: None, 400: None, 403: None, 401: None},
+                   description="If you are logged in, you should see \"Hello from the DrTrottoir API!\".")
     def get(self, request):
         return Response(
             {"message", "Hello from the DrTrottoir API!"},
