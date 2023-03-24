c4de043eedb0217e610e884f597130c186f23675
simvadnbu <simivdb@gmail.com>
2023-03-24 12:15:39 +0100

    Added error messages for signup & login

--- frontend/pages/login.tsx
+++ frontend/pages/login.tsx
@@ -2,29 +2,31 @@ import BaseHeader from "@/components/header/BaseHeader";
 import styles from "styles/Login.module.css";
 import Image from "next/image";
 import filler_image from "../public/filler_image.png";
-import Link from "next/link";
-import { login, verifyToken } from "@/lib/login";
-import { FormEvent, useEffect, useState } from "react";
-import { useRouter } from "next/router";
+import {login, verifyToken} from "@/lib/login";
+import React, {FormEvent, useEffect, useState} from "react";
+import {useRouter} from "next/router";
 
 export default function Login() {
     const router = useRouter();
     const [username, setUsername] = useState<string>("");
     const [password, setPassword] = useState<string>("");
+    const [errorMessages, setErrorMessages] = useState<string[]>( []);
 
     // try and log in to the application using existing refresh token
     useEffect(() => {
         verifyToken().then(
-            async (res) => {
-                console.log(res);
+            async () => {
                 await router.push("/welcome");
             },
             (err) => {
-                console.error("Error: token is not valid");
                 console.error(err);
             }
         );
     }, [verifyToken]);
+
+    useEffect(() => {
+        console.log(errorMessages);
+    }, [errorMessages]);
 
     const handleSubmit = async (event: FormEvent): Promise<void> => {
         event.preventDefault();
@@ -33,28 +35,45 @@ export default function Login() {
                 await router.push("/welcome");
             },
             (err) => {
-                console.error(err);
+                let errorRes = err.response;
+                if (errorRes.status === 400) {
+                    if (errorRes.data.non_field_errors) {
+                        setErrorMessages(errorRes.data.non_field_errors);
+                    }
+                } else {
+                    console.error(err);
+                }
             }
         );
     };
 
     return (
         <>
-            <BaseHeader />
+            <BaseHeader/>
             <div className="container py-5 h-100">
                 <div className="row d-flex justify-content-center align-items-center h-100">
                     <div className="col col-xl-10">
                         <div className="card">
                             <div className="row g-0">
                                 <div className="col-md-6 col-lg-5 d-none d-md-block">
-                                    <Image src={filler_image} alt="My App Logo" className={styles.filler_image} />
+                                    <Image src={filler_image} alt="My App Logo" className={styles.filler_image}/>
                                 </div>
                                 <div className="col-md-6 col-lg-7 d-flex align-items-center">
                                     <div className="card-body p-4 p-lg-5 text-black">
                                         <form onSubmit={handleSubmit}>
                                             <div className="d-flex align-items-center mb-3 pb-1">
-                                                <i className="fas fa-cubes fa-2x me-3" />
+                                                <i className="fas fa-cubes fa-2x me-3"/>
                                                 <span className="h1 fw-bold mb-0">Login.</span>
+                                            </div>
+
+                                            <div className="help-block mb-4">
+                                                <ul>
+                                                    {
+                                                        errorMessages.map((err, i) => (
+                                                            <li className="has-error text-danger" key={i}>{err}</li>
+                                                        ))
+                                                    }
+                                                </ul>
                                             </div>
 
                                             <div className="form-outline mb-4">
@@ -63,6 +82,7 @@ export default function Login() {
                                                     type="email"
                                                     className={`form-control form-control-lg ${styles.input}`}
                                                     value={username}
+                                                    placeholder="name@example.com"
                                                     onChange={(e: React.ChangeEvent<HTMLInputElement>) =>
                                                         setUsername(e.target.value)
                                                     }
@@ -75,10 +95,15 @@ export default function Login() {
                                                     type="password"
                                                     className={`form-control form-control-lg ${styles.input}`}
                                                     value={password}
-                                                    onChange={(e: React.ChangeEvent<HTMLInputElement>) =>
-                                                        setPassword(e.target.value)
-                                                    }
+                                                    onChange={(e: React.ChangeEvent<HTMLInputElement>) => {
+                                                        setPassword(e.target.value);
+                                                        e.target.setCustomValidity("");
+                                                    }}
+                                                    onInvalid={(e: React.ChangeEvent<HTMLInputElement>) => {
+                                                        e.target.setCustomValidity("Wachtwoord is verplicht.");
+                                                    }}
                                                     required
+                                                    placeholder="Wachtwoord123"
                                                 />
                                             </div>
 
