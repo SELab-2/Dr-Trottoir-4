3468506bb6a95cca806e6e0a270fda7390c7de8c
TiboStr <TiboStr@users.noreply.github.com>
2023-03-24 22:27:16 +0000

    Auto formatted code

--- backend/users/user_utils.py
+++ backend/users/user_utils.py
@@ -19,10 +19,10 @@ def add_regions_to_user(user_instance, regions_raw):
         if not type(regions) == list:
             raise SyntaxError()
     except SyntaxError:
-        return Response({"message": "Invalid syntax. Regions must be a list of id's"},
-                        status=status.HTTP_400_BAD_REQUEST)
+        return Response(
+            {"message": "Invalid syntax. Regions must be a list of id's"}, status=status.HTTP_400_BAD_REQUEST
+        )
 
     for region in regions:
-
         if r := try_adding_region_to_user_instance(user_instance, region):
             return r
