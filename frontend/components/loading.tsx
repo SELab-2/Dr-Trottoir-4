9308bbf31f2f8b249889a6f64529fb0b0aa3282f
simvadnbu <simivdb@gmail.com>
2023-03-25 15:45:20 +0100

    Added loading component

---
+++ frontend/components/loading.tsx
@@ -0,0 +1,11 @@
+import React from "react";
+
+function Loading() {
+    return (
+        <div className="h-100 d-flex align-items-center justify-content-center">
+            <div className="spinner-border text-dark" role="status"/>
+        </div>
+    );
+}
+
+export default Loading;
