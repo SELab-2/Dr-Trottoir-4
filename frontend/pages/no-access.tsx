8c58233257139407d13363c32293b758a5c76b75
simvadnbu <simvadnbu@users.noreply.github.com>
2023-03-27 09:26:27 +0000

    Auto formatted code

--- frontend/pages/no-access.tsx
+++ frontend/pages/no-access.tsx
@@ -2,13 +2,13 @@ import BaseHeader from "@/components/header/BaseHeader";
 import styles from "styles/Welcome.module.css";
 import React from "react";
 import Link from "next/link";
-import {useRouter} from "next/router";
+import { useRouter } from "next/router";
 
 export default function NoAccess() {
     const router = useRouter();
     return (
         <>
-            <BaseHeader/>
+            <BaseHeader />
             <p className={styles.title}>Deze pagina bestaat niet of je hebt geen toegang.</p>
             <Link href="/login">Go to login</Link>
         </>
