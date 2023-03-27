6330438450d9bb03f6899ae8b5455fb3ef969be7
simvadnbu <simivdb@gmail.com>
2023-03-27 11:27:33 +0200

    Removed unnecessary imports

--- frontend/pages/no-access.tsx
+++ frontend/pages/no-access.tsx
@@ -2,10 +2,8 @@ import BaseHeader from "@/components/header/BaseHeader";
 import styles from "styles/Welcome.module.css";
 import React from "react";
 import Link from "next/link";
-import {useRouter} from "next/router";
 
 export default function NoAccess() {
-    const router = useRouter();
     return (
         <>
             <BaseHeader/>
