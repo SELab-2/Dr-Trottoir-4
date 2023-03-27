6489eecbf9989fa38eb491ed4bc752c62954191b
simvadnbu <simivdb@gmail.com>
2023-03-27 15:36:26 +0200

    Fix merge conflicts

--- frontend/pages/no-access.tsx
+++ frontend/pages/no-access.tsx
@@ -6,7 +6,7 @@ import Link from "next/link";
 export default function NoAccess() {
     return (
         <>
-            <BaseHeader/>
+            <BaseHeader />
             <p className={styles.title}>Deze pagina bestaat niet of je hebt geen toegang.</p>
             <Link href="/login">Go to login</Link>
         </>
