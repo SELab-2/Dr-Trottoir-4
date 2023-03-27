c71c010b878394108f95254f8cc57691cd2bf17f
simvadnbu <simivdb@gmail.com>
2023-03-27 10:30:55 +0200

    Added back button on no-access page

--- frontend/pages/no-access.tsx
+++ frontend/pages/no-access.tsx
@@ -1,14 +1,18 @@
 import BaseHeader from "@/components/header/BaseHeader";
 import styles from "styles/Welcome.module.css";
+import React from "react";
+import Link from "next/link";
 
 export default function NoAccess() {
-    
+
     return (
         <>
-            <>
-                <BaseHeader/>
-                <p className={styles.title}>Deze pagina bestaat niet of je hebt geen toegang.</p>
-            </>
+            <BaseHeader/>
+            <p className={styles.title}>Deze pagina bestaat niet of je hebt geen toegang.</p>
+            <button className={`btn btn-dark btn-lg btn-block`} onClick={() => window.history.go(-2)}>
+                Go back
+            </button>
+            <Link href="/login">Go to login</Link>
         </>
     );
 }
