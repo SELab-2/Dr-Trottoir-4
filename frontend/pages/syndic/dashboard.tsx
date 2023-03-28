0a667fe811e351ae02b16c1b2a4f4db8c3d13555
Tibo Stroo <tibo.stroo@hotmail.be>
2023-03-28 11:30:20 +0200

    Added react-bootstrap

--- frontend/pages/syndic/dashboard.tsx
+++ frontend/pages/syndic/dashboard.tsx
@@ -3,14 +3,17 @@ import styles from "styles/Welcome.module.css";
 import soon from "public/coming_soon.png";
 import Image from "next/image";
 import LogoutButton from "@/components/logoutbutton";
-import { withAuthorisation } from "@/components/withAuthorisation";
-import { useRouter } from "next/router";
-import { TiPencil } from "react-icons/ti";
-import { BuildingInterface, getBuildingInfo, getBuildingsFromOwner } from "@/lib/building";
-import { useEffect, useState } from "react";
-import { AxiosResponse } from "axios/index";
+import {withAuthorisation} from "@/components/withAuthorisation";
+import {useRouter} from "next/router";
+import {TiPencil} from "react-icons/ti";
+import {BuildingInterface, getBuildingInfo, getBuildingsFromOwner} from "@/lib/building";
+import {useEffect, useState} from "react";
+import {AxiosResponse} from "axios/index";
+import {Button, Form, Modal} from 'react-bootstrap';
 
-interface ParsedUrlQuery {}
+
+interface ParsedUrlQuery {
+}
 
 interface DashboardQuery extends ParsedUrlQuery {
     id?: string;
@@ -20,8 +23,8 @@ function SyndicDashboard() {
     const router = useRouter();
     const query = router.query as DashboardQuery;
 
-    // @ts-ignore
     const [building, setBuilding] = useState<BuildingInterface | null>(null);
+    const [editBuilding, setEditBuilding] = useState(false);
 
     useEffect(() => {
         if (!query.id) {
@@ -43,7 +46,7 @@ function SyndicDashboard() {
 
     return (
         <>
-            <BaseHeader />
+            <BaseHeader/>
 
             <div>
                 <a
@@ -61,8 +64,46 @@ function SyndicDashboard() {
 
             <h1 className={styles.title}>Welcome to the Syndic Dashboard!</h1>
 
+            <Modal show={editBuilding} onHide={() => setEditBuilding(false)}>
+                <Modal.Header closeButton>
+                    Bewerk gebouw
+                </Modal.Header>
+                <Modal.Body>
+                    <Form>
+                        <Form.Group className="mb-3" controlId="formPatchBuilding">
+                            <Form.Label>Stad</Form.Label>
+                            <Form.Control defaultValue={building?.city} placeholder="Vul stad in"/>
+
+
+                            <Form.Label>Public id</Form.Label>
+                            <Form.Control defaultValue={building?.public_id}/>
+                            <Form.Text className="text-muted">
+                                De inwoners van uw gebouw kunnen info over vuilnisophaling zien op de link TODO
+                            </Form.Text>
+                        </Form.Group>
+
+
+                        <Button variant="danger" onClick={e => {
+                            e.preventDefault();
+                            setEditBuilding(false);
+                        }
+                        }>
+                            Annuleer
+                        </Button>
+                        <Button variant="primary" type="submit">
+                            Opslaan (TODO: PATCH request)
+                        </Button>
+
+                    </Form>
+                </Modal.Body>
+            </Modal>
+
+
             <h1>
-                Gebouw <TiPencil></TiPencil>
+                Gebouw <TiPencil onClick={e => {
+                e.preventDefault();
+                setEditBuilding(true);
+            }}></TiPencil>
             </h1>
             <p>Naam: {building?.name}</p>
             <p>Stad: {building?.city}</p>
@@ -71,15 +112,15 @@ function SyndicDashboard() {
             <p>Nr: {building?.house_number}</p>
             <p>Bus: {building?.bus}</p>
             <p>Client id: {building?.client_id}</p>
-            <p>Duration: {building?.duration}</p>
+            <p>Duration: {"" + building?.duration}</p>
             <p>Region: {building?.region_id}</p>
             <p>Public id: {building?.public_id}</p>
 
             <p>
                 https://www.figma.com/proto/9yLULhNn8b8SlsWlOnRSpm/SeLab2-mockup?node-id=16-1310&scaling=contain&page-id=0%3A1&starting-point-node-id=118%3A1486
             </p>
-            <Image src={soon} alt="Site coming soon" className={styles.image} />
-            <LogoutButton />
+            <Image src={soon} alt="Site coming soon" className={styles.image}/>
+            <LogoutButton/>
         </>
     );
 }
