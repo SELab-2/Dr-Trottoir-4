f71d3ed9e1d0d2ed77441847e94727faf25fcd02
TiboStr <TiboStr@users.noreply.github.com>
2023-03-28 00:21:52 +0000

    Auto formatted code

--- frontend/pages/syndic/dashboard.tsx
+++ frontend/pages/syndic/dashboard.tsx
@@ -3,15 +3,14 @@ import styles from "styles/Welcome.module.css";
 import soon from "public/coming_soon.png";
 import Image from "next/image";
 import LogoutButton from "@/components/logoutbutton";
-import {withAuthorisation} from "@/components/withAuthorisation";
-import {useRouter} from "next/router";
-import {TiPencil} from "react-icons/ti";
-import {BuildingInterface, getBuildingInfo, getBuildingsFromOwner} from "@/lib/building";
-import {useEffect, useState} from "react";
-import {AxiosResponse} from "axios/index";
+import { withAuthorisation } from "@/components/withAuthorisation";
+import { useRouter } from "next/router";
+import { TiPencil } from "react-icons/ti";
+import { BuildingInterface, getBuildingInfo, getBuildingsFromOwner } from "@/lib/building";
+import { useEffect, useState } from "react";
+import { AxiosResponse } from "axios/index";
 
-interface ParsedUrlQuery {
-}
+interface ParsedUrlQuery {}
 
 interface DashboardQuery extends ParsedUrlQuery {
     id?: string;
@@ -30,31 +29,41 @@ function SyndicDashboard() {
         }
 
         async function fetchBuilding() {
-            getBuildingInfo(query.id).then((buildings: AxiosResponse<any>) => {
-                setBuilding(buildings.data);
-            }).catch((error) => {
-                console.log(error);
-            });
+            getBuildingInfo(query.id)
+                .then((buildings: AxiosResponse<any>) => {
+                    setBuilding(buildings.data);
+                })
+                .catch((error) => {
+                    console.log(error);
+                });
         }
 
         fetchBuilding();
     }, [query.id]);
 
-
     return (
         <>
-            <BaseHeader/>
+            <BaseHeader />
 
-            <div><a onClick={e => {
-                e.preventDefault();
-                router.push("/syndic/building");
-            }}> ⮌ Terug naar het overzicht</a></div>
+            <div>
+                <a
+                    onClick={(e) => {
+                        e.preventDefault();
+                        router.push("/syndic/building");
+                    }}
+                >
+                    {" "}
+                    ⮌ Terug naar het overzicht
+                </a>
+            </div>
 
             {JSON.stringify(building)}
 
             <h1 className={styles.title}>Welcome to the Syndic Dashboard!</h1>
 
-            <h1>Gebouw <TiPencil></TiPencil></h1>
+            <h1>
+                Gebouw <TiPencil></TiPencil>
+            </h1>
             <p>Naam: {building?.name}</p>
             <p>Stad: {building?.city}</p>
             <p>Postcode: {building?.postal_code}</p>
@@ -66,12 +75,11 @@ function SyndicDashboard() {
             <p>Region: {building?.region_id}</p>
             <p>Public id: {building?.public_id}</p>
 
-
             <p>
                 https://www.figma.com/proto/9yLULhNn8b8SlsWlOnRSpm/SeLab2-mockup?node-id=16-1310&scaling=contain&page-id=0%3A1&starting-point-node-id=118%3A1486
             </p>
-            <Image src={soon} alt="Site coming soon" className={styles.image}/>
-            <LogoutButton/>
+            <Image src={soon} alt="Site coming soon" className={styles.image} />
+            <LogoutButton />
         </>
     );
 }
