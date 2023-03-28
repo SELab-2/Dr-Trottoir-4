ee09b9226656017cdfebd591ad9420fdfe3ad036
Tibo Stroo <tibo.stroo@hotmail.be>
2023-03-28 21:28:33 +0200

    Swapped syndig building and dashboard (#188 & #189)

--- frontend/pages/syndic/building.tsx
+++ frontend/pages/syndic/building.tsx
@@ -1,88 +1,125 @@
 import BaseHeader from "@/components/header/BaseHeader";
-import { BuildingInterface, getBuildingsFromOwner } from "@/lib/building";
-import router from "next/router";
-import { useEffect, useState } from "react";
-import { withAuthorisation } from "@/components/withAuthorisation";
-import SyndicDashboard from "@/pages/syndic/dashboard";
-import { AxiosResponse } from "axios";
+import {BuildingInterface, getBuildingInfo} from "@/lib/building";
+import {useRouter} from "next/router";
+import {useEffect, useState} from "react";
+import {withAuthorisation} from "@/components/withAuthorisation";
+import {AxiosResponse} from "axios";
+import styles from "@/styles/Welcome.module.css";
+import {Button, Form, Modal} from "react-bootstrap";
+import {TiPencil} from "react-icons/ti";
+import Image from "next/image";
+import soon from "@/public/coming_soon.png";
+import LogoutButton from "@/components/logoutbutton";
+
+interface ParsedUrlQuery {
+}
+
+interface DashboardQuery extends ParsedUrlQuery {
+    id?: string;
+}
 
 function SyndicBuilding() {
-    const [id, setId] = useState("");
-    const [buildings, setBuildings] = useState([]);
+    const router = useRouter();
+    const query = router.query as DashboardQuery;
+
+    const [building, setBuilding] = useState<BuildingInterface | null>(null);
+    const [editBuilding, setEditBuilding] = useState(false);
 
     useEffect(() => {
-        setId(sessionStorage.getItem("id") || "");
-    }, []);
-
-    useEffect(() => {
-        console.log("hier");
-
-        if (!id) {
-            console.log(`nog geen id ${id}`);
+        if (!query.id) {
             return;
         }
 
-        console.log(`id is ${id}`);
-
-        async function fetchBuildings() {
-            getBuildingsFromOwner(id)
+        async function fetchBuilding() {
+            getBuildingInfo(query.id)
                 .then((buildings: AxiosResponse<any>) => {
-                    setBuildings(buildings.data);
+                    setBuilding(buildings.data);
                 })
                 .catch((error) => {
                     console.log(error);
                 });
         }
 
-        fetchBuildings();
-    }, [id]);
-
-    if (!id || !buildings) {
-        //TODO: loading component, how to use? Maybe with a wrappen and a state boolean?
-        return <div>loading...</div>;
-    }
+        fetchBuilding();
+    }, [query.id]);
 
     return (
         <>
-            <>
-                <BaseHeader />
+            <BaseHeader/>
 
-                <p>
-                    https://www.figma.com/proto/9yLULhNn8b8SlsWlOnRSpm/SeLab2-mockup?node-id=16-1145&scaling=contain&page-id=0%3A1&starting-point-node-id=118%3A1486
-                </p>
+            <div>
+                <a
+                    onClick={(e) => {
+                        e.preventDefault();
+                        router.push("/syndic/dashboard");
+                    }}
+                >
+                    {" "}
+                    ⮌ Terug naar het overzicht
+                </a>
+            </div>
 
-                <h1>Uw gebouwen</h1>
+            {JSON.stringify(building)}
 
-                <div className="row">
-                    {buildings.map((building: BuildingInterface) => {
-                        // console.log(building);
-                        return (
-                            <div
-                                className="col-md-4 mb-3"
-                                key={building.id}
-                                onClick={(e) => {
-                                    e.preventDefault();
-                                    router.push({
-                                        pathname: "dashboard",
-                                        query: { id: building.id },
-                                    });
-                                }}
-                            >
-                                <div className="card">
-                                    <div className="card-body">
-                                        <h5 className="card-title">
-                                            {building.name} {building.postal_code} {building.city}
-                                        </h5>
-                                        <p className="card-text">
-                                            {building.street} {building.house_number}{" "}
-                                        </p>
-                                    </div>
-                                </div>
-                            </div>
-                        );
-                    })}
-                </div>
-            </>
+            <h1 className={styles.title}>Welcome to the Syndic Dashboard!</h1>
+
+            <Modal show={editBuilding} onHide={() => setEditBuilding(false)}>
+                <Modal.Header closeButton>
+                    Bewerk gebouw
+                </Modal.Header>
+                <Modal.Body>
+                    <Form>
+                        <Form.Group className="mb-3" controlId="formPatchBuilding">
+                            <Form.Label>Stad</Form.Label>
+                            <Form.Control defaultValue={building?.city} placeholder="Vul stad in"/>
+
+
+                            <Form.Label>Public id</Form.Label>
+                            <Form.Control defaultValue={building?.public_id}/>
+                            <Form.Text className="text-muted">
+                                De inwoners van uw gebouw kunnen info over vuilnisophaling zien op de link TODO
+                            </Form.Text>
+                        </Form.Group>
+
+
+                        <Button variant="danger" onClick={e => {
+                            e.preventDefault();
+                            setEditBuilding(false);
+                        }
+                        }>
+                            Annuleer
+                        </Button>
+                        <Button variant="primary" type="submit">
+                            Opslaan (TODO: PATCH request)
+                        </Button>
+
+                    </Form>
+                </Modal.Body>
+            </Modal>
+
+
+            <h1>
+                Gebouw <TiPencil onClick={e => {
+                e.preventDefault();
+                setEditBuilding(true);
+            }}></TiPencil>
+            </h1>
+            <p>Naam: {building?.name}</p>
+            <p>Stad: {building?.city}</p>
+            <p>Postcode: {building?.postal_code}</p>
+            <p>Straat: {building?.street}</p>
+            <p>Nr: {building?.house_number}</p>
+            <p>Bus: {building?.bus}</p>
+            <p>Client id: {building?.client_id}</p>
+            <p>Duration: {"" + building?.duration}</p>
+            <p>Region: {building?.region_id}</p>
+            <p>Public id: {building?.public_id}</p>
+
+            <p>
+                https://www.figma.com/proto/9yLULhNn8b8SlsWlOnRSpm/SeLab2-mockup?node-id=16-1310&scaling=contain&page-id=0%3A1&starting-point-node-id=118%3A1486
+            </p>
+            <Image src={soon} alt="Site coming soon" className={styles.image}/>
+            <LogoutButton/>
         </>
     );
 }
--- frontend/pages/syndic/dashboard.tsx
+++ frontend/pages/syndic/dashboard.tsx
@@ -1,126 +1,88 @@
 import BaseHeader from "@/components/header/BaseHeader";
-import styles from "styles/Welcome.module.css";
-import soon from "public/coming_soon.png";
-import Image from "next/image";
-import LogoutButton from "@/components/logoutbutton";
 import {withAuthorisation} from "@/components/withAuthorisation";
-import {useRouter} from "next/router";
-import {TiPencil} from "react-icons/ti";
-import {BuildingInterface, getBuildingInfo, getBuildingsFromOwner} from "@/lib/building";
+import router from "next/router";
+import {BuildingInterface, getBuildingsFromOwner} from "@/lib/building";
 import {useEffect, useState} from "react";
 import {AxiosResponse} from "axios/index";
-import {Button, Form, Modal} from 'react-bootstrap';
 
-
-interface ParsedUrlQuery {
-}
-
-interface DashboardQuery extends ParsedUrlQuery {
-    id?: string;
-}
 
 function SyndicDashboard() {
-    const router = useRouter();
-    const query = router.query as DashboardQuery;
-
-    const [building, setBuilding] = useState<BuildingInterface | null>(null);
-    const [editBuilding, setEditBuilding] = useState(false);
+    const [id, setId] = useState("");
+    const [buildings, setBuildings] = useState([]);
 
     useEffect(() => {
-        if (!query.id) {
+        setId(sessionStorage.getItem("id") || "");
+    }, []);
+
+    useEffect(() => {
+        console.log("hier");
+
+        if (!id) {
+            console.log(`nog geen id ${id}`);
             return;
         }
 
-        async function fetchBuilding() {
-            getBuildingInfo(query.id)
+        console.log(`id is ${id}`);
+
+        async function fetchBuildings() {
+            getBuildingsFromOwner(id)
                 .then((buildings: AxiosResponse<any>) => {
-                    setBuilding(buildings.data);
+                    setBuildings(buildings.data);
                 })
                 .catch((error) => {
                     console.log(error);
                 });
         }
 
-        fetchBuilding();
-    }, [query.id]);
+        fetchBuildings();
+    }, [id]);
+
+    if (!id || !buildings) {
+        //TODO: loading component, how to use? Maybe with a wrappen and a state boolean?
+        return <div>loading...</div>;
+    }
 
     return (
         <>
-            <BaseHeader/>
+            <>
+                <BaseHeader/>
 
-            <div>
-                <a
-                    onClick={(e) => {
-                        e.preventDefault();
-                        router.push("/syndic/building");
-                    }}
-                >
-                    {" "}
-                    ⮌ Terug naar het overzicht
-                </a>
-            </div>
+                <p>
+                    https://www.figma.com/proto/9yLULhNn8b8SlsWlOnRSpm/SeLab2-mockup?node-id=16-1145&scaling=contain&page-id=0%3A1&starting-point-node-id=118%3A1486
+                </p>
 
-            {JSON.stringify(building)}
+                <h1>Uw gebouwen</h1>
 
-            <h1 className={styles.title}>Welcome to the Syndic Dashboard!</h1>
-
-            <Modal show={editBuilding} onHide={() => setEditBuilding(false)}>
-                <Modal.Header closeButton>
-                    Bewerk gebouw
-                </Modal.Header>
-                <Modal.Body>
-                    <Form>
-                        <Form.Group className="mb-3" controlId="formPatchBuilding">
-                            <Form.Label>Stad</Form.Label>
-                            <Form.Control defaultValue={building?.city} placeholder="Vul stad in"/>
-
-
-                            <Form.Label>Public id</Form.Label>
-                            <Form.Control defaultValue={building?.public_id}/>
-                            <Form.Text className="text-muted">
-                                De inwoners van uw gebouw kunnen info over vuilnisophaling zien op de link TODO
-                            </Form.Text>
-                        </Form.Group>
-
-
-                        <Button variant="danger" onClick={e => {
-                            e.preventDefault();
-                            setEditBuilding(false);
-                        }
-                        }>
-                            Annuleer
-                        </Button>
-                        <Button variant="primary" type="submit">
-                            Opslaan (TODO: PATCH request)
-                        </Button>
-
-                    </Form>
-                </Modal.Body>
-            </Modal>
-
-
-            <h1>
-                Gebouw <TiPencil onClick={e => {
-                e.preventDefault();
-                setEditBuilding(true);
-            }}></TiPencil>
-            </h1>
-            <p>Naam: {building?.name}</p>
-            <p>Stad: {building?.city}</p>
-            <p>Postcode: {building?.postal_code}</p>
-            <p>Straat: {building?.street}</p>
-            <p>Nr: {building?.house_number}</p>
-            <p>Bus: {building?.bus}</p>
-            <p>Client id: {building?.client_id}</p>
-            <p>Duration: {"" + building?.duration}</p>
-            <p>Region: {building?.region_id}</p>
-            <p>Public id: {building?.public_id}</p>
-
-            <p>
-                https://www.figma.com/proto/9yLULhNn8b8SlsWlOnRSpm/SeLab2-mockup?node-id=16-1310&scaling=contain&page-id=0%3A1&starting-point-node-id=118%3A1486
-            </p>
-            <Image src={soon} alt="Site coming soon" className={styles.image}/>
-            <LogoutButton/>
+                <div className="row">
+                    {buildings.map((building: BuildingInterface) => {
+                        // console.log(building);
+                        return (
+                            <div
+                                className="col-md-4 mb-3"
+                                key={building.id}
+                                onClick={(e) => {
+                                    e.preventDefault();
+                                    router.push({
+                                        pathname: "building",
+                                        query: {id: building.id},
+                                    });
+                                }}
+                            >
+                                <div className="card">
+                                    <div className="card-body">
+                                        <h5 className="card-title">
+                                            {building.name} {building.postal_code} {building.city}
+                                        </h5>
+                                        <p className="card-text">
+                                            {building.street} {building.house_number}{" "}
+                                        </p>
+                                    </div>
+                                </div>
+                            </div>
+                        );
+                    })}
+                </div>
+            </>
         </>
     );
 }
