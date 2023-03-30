db69f9aca88aeeec7bd5d59a4ffa051286363df8
simvadnbu <simivdb@gmail.com>
2023-03-30 16:11:07 +0200

    Added ability to add/remove buildings from tour + change information

--- frontend/pages/admin/data/tours/edit.tsx
+++ frontend/pages/admin/data/tours/edit.tsx
@@ -3,45 +3,49 @@ import React, {useEffect, useMemo, useState} from "react";
 import {useRouter} from "next/router";
 import {getTour, Tour} from "@/lib/tour";
 import {getRegion, Region} from "@/lib/region";
-import {BuildingInterface, getAddress, getAllBuildings} from "@/lib/building";
-import {BuildingOnTour, getAllBuildingsOnTour, getAllBuildingsOnTourWithTourID} from "@/lib/building-on-tour";
-import Building from "@/pages/syndic/building";
+import {BuildingInterface, getAllBuildings} from "@/lib/building";
+import {BuildingOnTour, getAllBuildingsOnTourWithTourID} from "@/lib/building-on-tour";
 import MaterialReactTable, {MRT_ColumnDef, MRT_Row} from "material-react-table";
-import {Box, IconButton, Tooltip} from "@mui/material";
-import {Delete, Edit} from "@mui/icons-material";
+import {Box, Tooltip} from "@mui/material";
 import {Button} from "react-bootstrap";
+import {withAuthorisation} from "@/components/with-authorisation";
 
 
-interface ParsedUrlQuery {}
+interface ParsedUrlQuery {
+}
 
 interface DataToursEditQuery extends ParsedUrlQuery {
     tour?: number;
 }
 
 type BuildingOnTourView = {
-    buildingName : string,
-    city : string,
-    postalCode : string,
-    street : string,
-    houseNumber : number,
-    bus : string,
-    buildingId : number,
-    index : number,
+    buildingName: string,
+    city: string,
+    postalCode: string,
+    street: string,
+    houseNumber: number,
+    bus: string,
+    buildingId: number,
+    index: number,
 }
 
 type BuildingNotOnTourView = {
-    buildingName : string,
-    city : string,
-    postalCode : string,
-    street : string,
-    houseNumber : number,
-    bus : string,
-    buildingId : number,
+    buildingName: string,
+    city: string,
+    postalCode: string,
+    street: string,
+    houseNumber: number,
+    bus: string,
+    buildingId: number,
 }
 
-export default function AdminDataToursEdit() {
+/**
+ * TODO: implement delete, post & patch
+ * @constructor
+ */
+function AdminDataToursEdit() {
     const router = useRouter();
-    const query : DataToursEditQuery = router.query as DataToursEditQuery;
+    const query: DataToursEditQuery = router.query as DataToursEditQuery;
     const [tour, setTour] = useState<Tour>();
     const [region, setRegion] = useState<Region>();
     const [allBuildingsInRegion, setAllBuildingsInRegionInRegion] = useState<BuildingInterface[]>([]);
@@ -49,6 +53,9 @@ export default function AdminDataToursEdit() {
 
     const [buildingsOnTourView, setBuildingsOnTourView] = useState<BuildingOnTourView[]>([]);
     const [buildingsNotOnTourView, setBuildingsNotOnTourView] = useState<BuildingNotOnTourView[]>([]);
+    const [tourName, setTourName] = useState<string>("");
+    const [isLoading, setIsLoading] = useState<boolean>(true);
+
 
     const columnsBuildingOnTourView = useMemo<MRT_ColumnDef<BuildingOnTourView>[]>(
         () => [
@@ -123,7 +130,7 @@ export default function AdminDataToursEdit() {
     );
 
     useEffect(() => {
-        if (! query.tour) {
+        if (!query.tour) {
             return;
         }
         getTour(query.tour).then(res => {
@@ -137,6 +144,8 @@ export default function AdminDataToursEdit() {
         if (!tour) {
             return;
         }
+        setTourName(tour.name);
+
         getRegion(tour.region).then(res => {
             setRegion(res.data);
         }, err => {
@@ -144,95 +153,106 @@ export default function AdminDataToursEdit() {
         });
 
         getAllBuildings().then(res => {
-            const allBuildings : BuildingInterface[] = res.data;
-            const buildingsInRegion = allBuildings.filter((building : BuildingInterface) => tour.region === building.region);
+            const allBuildings: BuildingInterface[] = res.data;
+            const buildingsInRegion = allBuildings.filter((building: BuildingInterface) => tour.region === building.region);
             setAllBuildingsInRegionInRegion(buildingsInRegion);
         }, err => {
             console.error(err);
         });
 
         getAllBuildingsOnTourWithTourID(tour.id).then(res => {
-           const allBuildingsOnTour : BuildingOnTour[] = res.data;
-           setBuildingsOnTour(allBuildingsOnTour);
+            const allBuildingsOnTour: BuildingOnTour[] = res.data;
+            setBuildingsOnTour(allBuildingsOnTour);
         }, err => {
             console.error(err);
         });
     }, [tour]);
 
     useEffect(() => {
+        setIsLoading(false);
         getBuildingsOnTourView();
         getBuildingsNotOnTourView();
     }, [allBuildingsInRegion, buildingsOnTour]);
 
+    /**
+     * Get the buildings in the region that are not on the tour.
+     */
     function getBuildingsOnTourView() {
-        let botV : BuildingOnTourView[] = buildingsOnTour.map((buildingOnTour : BuildingOnTour) => {
-            const building : BuildingInterface = allBuildingsInRegion.find((building : BuildingInterface) => buildingOnTour.building === building.id)!; // This will not be undefined hence '!'
+        let botV: BuildingOnTourView[] = buildingsOnTour.map((buildingOnTour: BuildingOnTour) => {
+            const building: BuildingInterface = allBuildingsInRegion.find((building: BuildingInterface) => buildingOnTour.building === building.id)!; // This will not be undefined hence '!'
             return {
-                buildingName : building?.name,
-                city : building?.city,
-                postalCode : building?.postal_code,
-                street : building?.street,
-                houseNumber : building?.house_number,
-                bus : building?.bus,
-                buildingId : building?.id,
-                index : buildingOnTour.index + 1,
+                buildingName: building?.name,
+                city: building?.city,
+                postalCode: building?.postal_code,
+                street: building?.street,
+                houseNumber: building?.house_number,
+                bus: building?.bus,
+                buildingId: building?.id,
+                index: buildingOnTour.index,
             };
         });
-        botV.sort((a : BuildingOnTourView, b : BuildingOnTourView) => a.index < b.index ? -1 : 1);
+        botV.sort((a: BuildingOnTourView, b: BuildingOnTourView) => a.index < b.index ? -1 : 1);
         setBuildingsOnTourView(botV);
     }
 
+    /**
+     * Get the buildings in the region that are not on the tour.
+     */
     function getBuildingsNotOnTourView() {
-        const allBuildingsNotOnTour = allBuildingsInRegion.filter((building : BuildingInterface) =>
-            ! buildingsOnTour.some((buildingOnTour : BuildingOnTour) => building.id === buildingOnTour.building)
+        const allBuildingsNotOnTour = allBuildingsInRegion.filter((building: BuildingInterface) =>
+            !buildingsOnTour.some((buildingOnTour: BuildingOnTour) => building.id === buildingOnTour.building)
         );
-        const bnotV : BuildingNotOnTourView[] = allBuildingsNotOnTour.map((building : BuildingInterface) => {
-           return {
-               buildingName : building.name,
-               city : building.city,
-               postalCode : building.postal_code,
-               street : building.street,
-               houseNumber : building.house_number,
-               bus : building.bus,
-               buildingId : building.id,
-           };
+        const bnotV: BuildingNotOnTourView[] = allBuildingsNotOnTour.map((building: BuildingInterface) => {
+            return {
+                buildingName: building.name,
+                city: building.city,
+                postalCode: building.postal_code,
+                street: building.street,
+                houseNumber: building.house_number,
+                bus: building.bus,
+                buildingId: building.id,
+            };
         });
         setBuildingsNotOnTourView(bnotV);
+    }
+
+    function addToBuildingOnTour(buildingNotOnTour: BuildingNotOnTourView) {
+        const index: number = buildingsOnTourView.length + 1;
+        const bot: BuildingOnTourView = {...buildingNotOnTour, index: index};
+        const i = buildingsNotOnTourView.indexOf(buildingNotOnTour);
+        if (i > -1) {
+            buildingsNotOnTourView.splice(i, 1);
+        }
+        setBuildingsNotOnTourView([...buildingsNotOnTourView]);
+        buildingsOnTourView.push(bot);
+        setBuildingsOnTourView([...buildingsOnTourView]);
+    }
+
+    function removeFromBuildingOnTour(buildingOnTourView: BuildingOnTourView) {
+        const removeIndex: number = buildingOnTourView.index - 1;
+        buildingsOnTourView.splice(removeIndex, 1);
+        buildingsOnTourView.forEach((b: BuildingOnTourView, index: number) => b.index = index + 1);
+        setBuildingsOnTourView([...buildingsOnTourView]);
+        const buildingNotOnTourView: BuildingNotOnTourView = {
+            buildingName: buildingOnTourView.buildingName,
+            city: buildingOnTourView.city,
+            postalCode: buildingOnTourView.postalCode,
+            street: buildingOnTourView.street,
+            houseNumber: buildingOnTourView.houseNumber,
+            bus: buildingOnTourView.bus,
+            buildingId: buildingOnTourView.buildingId,
+        };
+        buildingsNotOnTourView.push(buildingNotOnTourView);
+        setBuildingsNotOnTourView([...buildingsNotOnTourView]);
     }
 
     return (
         <>
             <>
-                <BaseHeader />
-                <p>Laatste aanpassing: {tour? (new Date(tour.modified_at)).toLocaleString() : ""}</p>
-                <p>Tour : {tour?.name}</p>
-                <p>Regio : {region?.region}</p>
-                <div>Gebouwen: </div>
+                <BaseHeader/>
                 <MaterialReactTable
-                    autoResetPageIndex={false}
                     columns={columnsBuildingOnTourView}
                     data={buildingsOnTourView}
-                    enableRowOrdering
-                    enableSorting={false}
-                    enableHiding={false}
-                    initialState={{ columnVisibility: { buildingId: false } }}
-                    muiTableBodyRowDragHandleProps={({ table }) => ({
-                        onDragEnd: () => {
-                            const { draggingRow, hoveredRow } = table.getState();
-                            if (hoveredRow && draggingRow) {
-                                buildingsOnTourView.splice(
-                                    (hoveredRow as MRT_Row<BuildingOnTourView>).index,
-                                    0,
-                                    buildingsOnTourView.splice(draggingRow.index, 1)[0],
-                                );
-                                buildingsOnTourView.forEach((view : BuildingOnTourView, index) => view.index = index + 1);
-                                setBuildingsOnTourView([...buildingsOnTourView]);
-                            }
-                        },
-                    })}
-                />
-                <div>Mogelijke toevoegingen:</div>
-                <MaterialReactTable
                     displayColumnDefOptions={{
                         'mrt-row-actions': {
                             muiTableHeadCellProps: {
@@ -241,21 +261,84 @@ export default function AdminDataToursEdit() {
                             header: 'Acties',
                         },
                     }}
-                    columns={columnsBuildingNotOnTourView}
-                    data={buildingsNotOnTourView}
+                    enablePagination={false}
                     enableEditing
                     // Don't show the tour_id
                     enableHiding={false}
-                    initialState={{ columnVisibility: { buildingId: false } }}
-                    renderRowActions={({ row, table }) => (
-                        <Box sx={{ display: 'flex', gap: '1rem' }}>
-                            <Tooltip arrow placement="left" title="Pas aan">
-                                <IconButton onClick={() => {
-                                    console.log("Add to active buildings")
-                                }}>
-                                    <Edit />
-                                </IconButton>
+                    enableBottomToolbar={false}
+                    initialState={{columnVisibility: {buildingId: false}}}
+                    state={{ isLoading: isLoading }}
+                    autoResetPageIndex={false}
+                    enableRowOrdering
+                    muiTableBodyRowDragHandleProps={({table}) => ({
+                        onDragEnd: () => {
+                            const {draggingRow, hoveredRow} = table.getState();
+                            if (hoveredRow && draggingRow) {
+                                buildingsOnTourView.splice(
+                                    (hoveredRow as MRT_Row<BuildingOnTourView>).index,
+                                    0,
+                                    buildingsOnTourView.splice(draggingRow.index, 1)[0],
+                                );
+                                buildingsOnTourView.forEach((view: BuildingOnTourView, index) => view.index = index + 1);
+                                setBuildingsOnTourView([...buildingsOnTourView]);
+                            }
+                        },
+                    })}
+                    renderRowActions={({row, table}) => (
+                        <Box sx={{display: 'flex', gap: '1rem'}}>
+                            <Tooltip arrow placement="left" title="Verwijder van ronde">
+                                <Button variant="warning" onClick={() => {
+                                    const buildingOnTourView: BuildingOnTourView = row.original;
+                                    removeFromBuildingOnTour(buildingOnTourView);
+                                }}>-</Button>
                             </Tooltip>
+                        </Box>
+                    )}
+                    renderTopToolbarCustomActions={() => (
+                        <Box sx={{display: 'flex', gap: '1rem'}}>
+                            <label>Ronde: </label>
+                            <input
+                                value={tourName}
+                                onChange={(e: React.ChangeEvent<HTMLInputElement>) => {
+                                setTourName(e.target.value);
+                            }}></input>
+                            <label>{region ? `Regio: ${region.region}` : ""}</label>
+                            <label>{tour ? `Laatste aanpassing: ${(new Date(tour.modified_at)).toLocaleString()}`: ""}</label>
+                        </Box>
+                    )}
+                />
+
+                <MaterialReactTable
+                    columns={columnsBuildingNotOnTourView}
+                    data={buildingsNotOnTourView}
+                    enableBottomToolbar={false}
+                    displayColumnDefOptions={{
+                        'mrt-row-actions': {
+                            muiTableHeadCellProps: {
+                                align: 'center',
+                            },
+                            header: 'Acties',
+                        },
+                    }}
+                    enablePagination={false}
+                    enableEditing
+                    state={{ isLoading: isLoading }}
+                    // Don't show the tour_id
+                    enableHiding={false}
+                    initialState={{columnVisibility: {buildingId: false}}}
+                    renderRowActions={({row, table}) => (
+                        <Box sx={{display: 'flex', gap: '1rem'}}>
+                            <Tooltip arrow placement="left" title="Voeg toe aan ronde">
+                                <Button variant="warning" onClick={() => {
+                                    const bnot: BuildingNotOnTourView = row.original;
+                                    addToBuildingOnTour(bnot);
+                                }}>+</Button>
+                            </Tooltip>
+                        </Box>
+                    )}
+                    renderTopToolbarCustomActions={() => (
+                        <Box sx={{display: 'flex', gap: '1rem'}}>
+                            <label>Gebouwen niet op deze ronde (regio {region?.region})</label>
                         </Box>
                     )}
                 />
@@ -266,3 +349,5 @@ export default function AdminDataToursEdit() {
         </>
     );
 }
+
+export default withAuthorisation(AdminDataToursEdit, ["Admin", "Student"]);
