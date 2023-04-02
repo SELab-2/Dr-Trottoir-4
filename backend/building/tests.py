603909b24779dae27882c93b103f2e873e154a5c
sevrijss <Seppe.VanRijsselberghe@UGent.be>
2023-04-02 21:54:28 +0200

    auth testing building

--- backend/building/tests.py
+++ backend/building/tests.py
@@ -1,6 +1,7 @@
 from django.test import TestCase
 from rest_framework.test import APIClient
 
+from base.models import User
 from base.test_settings import backend_url
 from util.data_generators import createUser, insert_dummy_region, insert_dummy_syndic
 
@@ -249,3 +250,190 @@ class BuildingTests(TestCase):
         _ = client.post(f"{backend_url}/building/", data1, follow=True)
         response1 = client.post(f"{backend_url}/building/", data1, follow=True)
         assert response1.status_code == 400
+
+
+class AuthorizationTests(TestCase):
+    roles = ["Default", "Admin", "Superstudent", "Student", "Syndic"]
+
+    def test_building_list(self):
+        codes = {
+            "Default": 403,
+            "Admin": 200,
+            "Superstudent": 200,
+            "Student": 403,
+            "Syndic": 403
+        }
+        for role in self.roles:
+            user = createUser(role)
+            client = APIClient()
+            client.force_authenticate(user=user)
+            resp = client.get(f"{backend_url}/building/all")
+            assert resp.status_code == codes[role]
+
+    def test_insert_building(self):
+        codes = {
+            "Default": 403,
+            "Admin": 201,
+            "Superstudent": 201,
+            "Student": 403,
+            "Syndic": 403
+        }
+        number = 1
+        for role in self.roles:
+            user = createUser(role)
+            client = APIClient()
+            client.force_authenticate(user=user)
+            r_id = insert_dummy_region()
+            s_id = insert_dummy_syndic()
+            data = {
+                "city": "Gent",
+                "postal_code": 9000,
+                "street": "Overpoort",
+                "house_number": number,  # unique (local) number to avoid collision errors
+                "client_number": "1234567890abcdef",
+                "duration": "1:00:00",
+                "region": r_id,
+                "syndic": s_id,
+                "name": "CB"
+            }
+            number += 1
+            resp = client.post(f"{backend_url}/building/", data, follow=True)
+            assert resp.status_code == codes[role]
+
+    def test_get_building(self):
+        codes = {
+            "Default": 403,
+            "Admin": 200,
+            "Superstudent": 200,
+            "Student": 200,
+            "Syndic": 403
+        }
+        adminUser = createUser()
+        adminClient = APIClient()
+        adminClient.force_authenticate(user=adminUser)
+        r_id = insert_dummy_region()
+        s_id = insert_dummy_syndic()
+        data = {
+            "city": "Gent",
+            "postal_code": 9000,
+            "street": "Overpoort",
+            "house_number": 10,
+            "client_number": "1234567890abcdef",
+            "duration": "1:00:00",
+            "region": r_id,
+            "syndic": s_id,
+            "name": "CB"
+        }
+        print(s_id)
+
+        response1 = adminClient.post(f"{backend_url}/building/", data, follow=True)
+        id = response1.data["id"]
+        assert response1.status_code == 201
+        for role in self.roles:
+            user = createUser(role)
+            if role == "Syndic":
+                print(user.id)
+            client = APIClient()
+            client.force_authenticate(user=user)
+            response2 = client.get(f"{backend_url}/building/{id}/", follow=True)
+            if response2.status_code != codes[role]:
+                print(f"role: {role}\tcode: {response2.status_code} (expected {codes[role]})")
+            assert response2.status_code == codes[role]
+        # testing special case when syndic is owner of building
+        user = User.objects.filter(id=s_id)[0]  # there should only be 1
+        client = APIClient()
+        client.force_authenticate(user=user)
+        response2 = client.get(f"{backend_url}/building/{id}/", follow=True)
+        assert response2.status_code == 200
+
+
+    def test_patch_building(self):
+        codes = {
+            "Default": 403,
+            "Admin": 200,
+            "Superstudent": 200,
+            "Student": 403,
+            "Syndic": 403
+        }
+        adminUser = createUser()
+        adminClient = APIClient()
+        adminClient.force_authenticate(user=adminUser)
+        r_id = insert_dummy_region()
+        s_id = insert_dummy_syndic()
+        data1 = {
+            "city": "Gent",
+            "postal_code": 9000,
+            "street": "Overpoort",
+            "house_number": 10,
+            "client_number": "1234567890abcdef",
+            "duration": "1:00:00",
+            "region": r_id,
+            "syndic": s_id,
+            "name": "CB"
+        }
+        data2 = {
+            "city": "Gent",
+            "postal_code": 9000,
+            "street": "De Zuid",
+            "house_number": 10,
+            "client_number": "1234567890abcdef",
+            "duration": "1:00:00",
+            "region": r_id,
+            "syndic": s_id,
+            "name": "CB"
+        }
+        response1 = adminClient.post(f"{backend_url}/building/", data1, follow=True)
+        id = response1.data["id"]
+        for role in self.roles:
+            user = createUser(role)
+            client = APIClient()
+            client.force_authenticate(user)
+            response2 = client.patch(f"{backend_url}/building/{id}/", data2, follow=True)
+            assert response2.status_code == codes[role]
+        # testing special case when syndic is owner of building
+        # patch shouldn't be allowed either way
+        user = User.objects.filter(id=s_id)[0]  # there should only be 1
+        client = APIClient()
+        client.force_authenticate(user=user)
+        response2 = client.patch(f"{backend_url}/building/{id}/", data2, follow=True)
+        assert response2.status_code == 403
+
+    def test_remove_building(self):
+        codes = {
+            "Default": 403,
+            "Admin": 204,
+            "Superstudent": 204,
+            "Student": 403,
+            "Syndic": 403
+        }
+        adminUser = createUser()
+        adminClient = APIClient()
+        adminClient.force_authenticate(user=adminUser)
+        r_id = insert_dummy_region()
+        s_id = insert_dummy_syndic()
+        data1 = {
+            "city": "Gent",
+            "postal_code": 9000,
+            "street": "Overpoort",
+            "house_number": 10,
+            "client_number": "1234567890abcdef",
+            "duration": "1:00:00",
+            "region": r_id,
+            "syndic": s_id,
+            "name": "CB"
+        }
+        exists = False
+        for role in self.roles:
+            if not exists:
+                # building toevoegen als admin
+                response1 = adminClient.post(f"{backend_url}/building/", data1, follow=True)
+                print(response1.status_code)
+                print(response1.data)
+                id = response1.data["id"]
+            # proberen verwijderen als `role`
+            user = createUser(role)
+            client = APIClient()
+            client.force_authenticate(user)
+            response2 = client.delete(f"{backend_url}/building/{id}/", follow=True)
+            assert response2.status_code == codes[role]
+            exists = codes[role] != 204
