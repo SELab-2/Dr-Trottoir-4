a5197a35ab971fad084e1fe1eb53d93c272d6de1
sevrijss <Seppe.VanRijsselberghe@UGent.be>
2023-04-03 11:44:31 +0200

    auth test building_on_tour

--- backend/building_on_tour/tests.py
+++ backend/building_on_tour/tests.py
@@ -1,7 +1,7 @@
 from django.test import TestCase
 from rest_framework.test import APIClient
 
-from base.test_settings import backend_url
+from base.test_settings import backend_url, roles
 from util.data_generators import createUser, insert_dummy_tour, insert_dummy_building
 
 
@@ -196,3 +196,149 @@ class BuildingOnTourTests(TestCase):
         _ = client.post(f"{backend_url}/building-on-tour/", data, follow=True)
         response1 = client.post(f"{backend_url}/building-on-tour/", data, follow=True)
         assert response1.status_code == 400
+
+
+class BuildingOnTourAuthorizationTests(TestCase):
+
+    def test_building_on_tour_list(self):
+        codes = {
+            "Default": 403,
+            "Admin": 200,
+            "Superstudent": 200,
+            "Student": 200,
+            "Syndic": 403
+        }
+        for role in roles:
+            user = createUser(role)
+            client = APIClient()
+            client.force_authenticate(user=user)
+            resp = client.get(f"{backend_url}/building-on-tour/all")
+            assert resp.status_code == codes[role]
+
+    def test_insert_building_on_tour(self):
+        codes = {
+            "Default": 403,
+            "Admin": 201,
+            "Superstudent": 201,
+            "Student": 403,
+            "Syndic": 403
+        }
+        adminUser = createUser()
+        adminClient = APIClient()
+        adminClient.force_authenticate(user=adminUser)
+        t_id = insert_dummy_tour()
+        b_id = insert_dummy_building()
+        for role in roles:
+            user = createUser(role)
+            client = APIClient()
+            client.force_authenticate(user=user)
+            data = {
+                "tour": t_id,
+                "building": b_id,
+                "index": 0
+            }
+            resp = client.post(f"{backend_url}/building-on-tour/", data, follow=True)
+            print(resp.data)
+            assert resp.status_code == codes[role]
+            if resp.status_code == 201:
+                print(f"[{role}]\tinserted, removing again")
+                id = resp.data["id"]
+                print(id)
+                adminClient.delete(f"{backend_url}/building-on-tour/{id}/", follow=True)
+
+    def test_get_building_on_tour(self):
+        codes = {
+            "Default": 403,
+            "Admin": 200,
+            "Superstudent": 200,
+            "Student": 200,
+            "Syndic": 403
+        }
+        adminUser = createUser()
+        adminClient = APIClient()
+        adminClient.force_authenticate(user=adminUser)
+        t_id = insert_dummy_tour()
+        b_id = insert_dummy_building()
+        data = {
+            "tour": t_id,
+            "building": b_id,
+            "index": 0
+        }
+        
+        response1 = adminClient.post(f"{backend_url}/building-on-tour/", data, follow=True)
+        id = response1.data["id"]
+        assert response1.status_code == 201
+        for role in roles:
+            user = createUser(role)
+            client = APIClient()
+            client.force_authenticate(user=user)
+            response2 = client.get(f"{backend_url}/building-on-tour/{id}/", follow=True)
+            if response2.status_code != codes[role]:
+                print(f"role: {role}\tcode: {response2.status_code} (expected {codes[role]})")
+            assert response2.status_code == codes[role]
+
+
+    def test_patch_building_on_tour(self):
+        codes = {
+            "Default": 403,
+            "Admin": 200,
+            "Superstudent": 200,
+            "Student": 403,
+            "Syndic": 403
+        }
+        adminUser = createUser()
+        adminClient = APIClient()
+        adminClient.force_authenticate(user=adminUser)
+        t_id = insert_dummy_tour()
+        b_id1 = insert_dummy_building()
+        b_id2 = insert_dummy_building(street="Zuid")
+        data1 = {
+            "tour": t_id,
+            "building": b_id1,
+            "index": 0
+        }
+        data2 = {
+            "tour": t_id,
+            "building": b_id2,
+            "index": 1
+        }
+        response1 = adminClient.post(f"{backend_url}/building-on-tour/", data1, follow=True)
+        id = response1.data["id"]
+        for role in roles:
+            user = createUser(role)
+            client = APIClient()
+            client.force_authenticate(user)
+            response2 = client.patch(f"{backend_url}/building-on-tour/{id}/", data2, follow=True)
+            assert response2.status_code == codes[role]
+
+    def test_remove_building_on_tour(self):
+        codes = {
+            "Default": 403,
+            "Admin": 204,
+            "Superstudent": 204,
+            "Student": 403,
+            "Syndic": 403
+        }
+        adminUser = createUser()
+        adminClient = APIClient()
+        adminClient.force_authenticate(user=adminUser)
+        t_id = insert_dummy_tour()
+        b_id = insert_dummy_building()
+        data1 = {
+            "tour": t_id,
+            "building": b_id,
+            "index": 0
+        }
+        exists = False
+        for role in roles:
+            if not exists:
+                # building toevoegen als admin
+                response1 = adminClient.post(f"{backend_url}/building-on-tour/", data1, follow=True)
+                id = response1.data["id"]
+            # proberen verwijderen als `role`
+            user = createUser(role)
+            client = APIClient()
+            client.force_authenticate(user)
+            response2 = client.delete(f"{backend_url}/building-on-tour/{id}/", follow=True)
+            assert response2.status_code == codes[role]
+            exists = codes[role] != 204
