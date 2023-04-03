a70ed01c7bd45666284d9b099b133a56249938e3
n00bS-oWn-m3 <sebastiaan@onyx.be>
2023-04-03 12:07:21 +0200

    #209 try using docker compose to run tests

--- runtests.sh
+++ runtests.sh
@@ -1,11 +1,2 @@
 #!/bin/bash
 docker compose exec backend python manage.py test building/tests.py role/tests.py building_comment/tests.py building_on_tour/tests.py garbage_collection/tests.py manual/tests.py region/tests.py tour/tests.py picture_building/tests.py student_at_building_on_tour/tests.py --with-coverage --cover-package=building,building_on_tour,garbage_collection,manual,region,tour,building_comment,picture_building,student_at_building_on_tour,role
-
-# wait script
-echo "Press any key to continue"
-while [ true ]; do
-  read -t 3 -n 1
-  if [ $? = 0 ]; then
-    exit
-  fi
-done
