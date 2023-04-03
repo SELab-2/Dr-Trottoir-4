5e0fbc072a252f3be24e9933ee0eee793ef349ba
sevrijss <Seppe.VanRijsselberghe@UGent.be>
2023-04-03 15:28:38 +0200

    auth testing manual

--- backend/manual/tests.py
+++ backend/manual/tests.py
@@ -1,9 +1,8 @@
 from django.test import TestCase
 from rest_framework.test import APIClient
 
-from base.test_settings import backend_url
+from base.test_settings import backend_url, roles
 from util.data_generators import createUser, insert_dummy_building, createMemoryFile
-
 
 f = createMemoryFile("backend/manual/lorem-ipsum.pdf")
 
@@ -192,3 +191,147 @@ class ManualTests(TestCase):
         response1 = client.post(f"{backend_url}/manual/", data, follow=True)
         # should be 201 because it's resolved internally
         assert response1.status_code == 201
+
+
+class ManualAuthorizationTests(TestCase):
+
+    def test_manual_list(self):
+        codes = {
+            "Default": 403,
+            "Admin": 200,
+            "Superstudent": 200,
+            "Student": 403,
+            "Syndic": 403
+        }
+        for role in roles:
+            user = createUser(role)
+            client = APIClient()
+            client.force_authenticate(user=user)
+            resp = client.get(f"{backend_url}/manual/all")
+            assert resp.status_code == codes[role]
+
+    def test_insert_lobby(self):
+        codes = {
+            "Default": 403,
+            "Admin": 201,
+            "Superstudent": 201,
+            "Student": 403,
+            "Syndic": 201
+        }
+        adminUser = createUser()
+        adminClient = APIClient()
+        adminClient.force_authenticate(user=adminUser)
+        b_id = insert_dummy_building()
+        data = {
+            "building": b_id,
+            "file": f,
+            "version_number": 0
+        }
+        for role in roles:
+            user = createUser(role)
+            client = APIClient()
+            client.force_authenticate(user=user)
+
+            resp = client.post(f"{backend_url}/manual/", data, follow=True)
+            assert resp.status_code == codes[role]
+            if resp.status_code == 201:
+                id = resp.data["id"]
+                adminClient.delete(f"{backend_url}/manual/{id}/", follow=True)
+
+    def test_get_lobby(self):
+        codes = {
+            "Default": 403,
+            "Admin": 200,
+            "Superstudent": 200,
+            "Student": 200,
+            "Syndic": 403
+        }
+        adminUser = createUser()
+        adminClient = APIClient()
+        adminClient.force_authenticate(user=adminUser)
+
+        b_id = insert_dummy_building()
+        data = {
+            "building": b_id,
+            "file": f,
+            "version_number": 0
+        }
+
+        response1 = adminClient.post(f"{backend_url}/manual/", data, follow=True)
+
+        id = response1.data["id"]
+        assert response1.status_code == 201
+        for role in roles:
+            user = createUser(role)
+            client = APIClient()
+            client.force_authenticate(user=user)
+            response2 = client.get(f"{backend_url}/manual/{id}/", follow=True)
+            if response2.status_code != codes[role]:
+                print(f"role: {role}\tcode: {response2.status_code} (expected {codes[role]})")
+            assert response2.status_code == codes[role]
+
+    def test_patch_lobby(self):
+        codes = {
+            "Default": 403,
+            "Admin": 200,
+            "Superstudent": 200,
+            "Student": 403,
+            "Syndic": 403
+        }
+        adminUser = createUser()
+        adminClient = APIClient()
+        adminClient.force_authenticate(user=adminUser)
+
+        b_id = insert_dummy_building()
+        data1 = {
+            "building": b_id,
+            "file": f,
+            "version_number": 0
+        }
+        data2 = {
+            "building": b_id,
+            "file": f,
+            "version_number": 1
+        }
+
+        response1 = adminClient.post(f"{backend_url}/manual/", data1, follow=True)
+        id = response1.data["id"]
+        for role in roles:
+            user = createUser(role)
+            client = APIClient()
+            client.force_authenticate(user)
+            response2 = client.patch(f"{backend_url}/manual/{id}/", data2, follow=True)
+            assert response2.status_code == codes[role]
+
+    def test_remove_lobby(self):
+        codes = {
+            "Default": 403,
+            "Admin": 204,
+            "Superstudent": 204,
+            "Student": 403,
+            "Syndic": 403
+        }
+        adminUser = createUser()
+        adminClient = APIClient()
+        adminClient.force_authenticate(user=adminUser)
+
+        b_id = insert_dummy_building()
+        data1 = {
+            "building": b_id,
+            "file": f,
+            "version_number": 0
+        }
+
+        exists = False
+        for role in roles:
+            if not exists:
+                # building toevoegen als admin
+                response1 = adminClient.post(f"{backend_url}/manual/", data1, follow=True)
+                id = response1.data["id"]
+            # proberen verwijderen als `role`
+            user = createUser(role)
+            client = APIClient()
+            client.force_authenticate(user)
+            response2 = client.delete(f"{backend_url}/manual/{id}/", follow=True)
+            assert response2.status_code == codes[role]
+            exists = codes[role] != 204
