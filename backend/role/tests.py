35f064940b5895acb90934ae86f3472e4497e328
sevrijss <Seppe.VanRijsselberghe@UGent.be>
2023-04-03 16:44:03 +0200

    role auth testing

--- backend/role/tests.py
+++ backend/role/tests.py
@@ -1,7 +1,7 @@
 from django.test import TestCase
 from rest_framework.test import APIClient
 
-from base.test_settings import backend_url
+from base.test_settings import backend_url, roles
 from util.data_generators import createUser
 
 
@@ -132,3 +132,125 @@ class RoleTests(TestCase):
         _ = client.post(f"{backend_url}/role/", data1, follow=True)
         response1 = client.post(f"{backend_url}/role/", data1, follow=True)
         assert response1.status_code == 400
+
+
+class RoleAuthorizationTests(TestCase):
+
+    def test_role_list(self):
+        codes = {
+            "Default": 403,
+            "Admin": 200,
+            "Superstudent": 200,
+            "Student": 403,
+            "Syndic": 403
+        }
+        for role in roles:
+            user = createUser(role)
+            client = APIClient()
+            client.force_authenticate(user=user)
+            resp = client.get(f"{backend_url}/role/all")
+            assert resp.status_code == codes[role]
+
+    def test_insert_role(self):
+        codes = {
+            "Default": 403,
+            "Admin": 201,
+            "Superstudent": 403,
+            "Student": 403,
+            "Syndic": 403
+        }
+        adminUser = createUser()
+        adminClient = APIClient()
+        adminClient.force_authenticate(user=adminUser)
+
+        data = {"name": "Test", "rank": 2, "description": "testRole"}
+
+        for role in roles:
+            user = createUser(role)
+            client = APIClient()
+            client.force_authenticate(user=user)
+
+            resp = client.post(f"{backend_url}/role/", data, follow=True)
+            assert resp.status_code == codes[role]
+            if resp.status_code == 201:
+                id = resp.data["id"]
+                adminClient.delete(f"{backend_url}/role/{id}/", follow=True)
+
+    def test_get_role(self):
+        codes = {
+            "Default": 403,
+            "Admin": 200,
+            "Superstudent": 200,
+            "Student": 403,
+            "Syndic": 403
+        }
+        adminUser = createUser()
+        adminClient = APIClient()
+        adminClient.force_authenticate(user=adminUser)
+
+        data = {"name": "Test", "rank": 2, "description": "testRole"}
+
+        response1 = adminClient.post(f"{backend_url}/role/", data, follow=True)
+
+        id = response1.data["id"]
+        assert response1.status_code == 201
+        for role in roles:
+            user = createUser(role)
+            client = APIClient()
+            client.force_authenticate(user=user)
+            response2 = client.get(f"{backend_url}/role/{id}/", follow=True)
+            if response2.status_code != codes[role]:
+                print(f"role: {role}\tcode: {response2.status_code} (expected {codes[role]})")
+            assert response2.status_code == codes[role]
+
+    def test_patch_role(self):
+        codes = {
+            "Default": 403,
+            "Admin": 200,
+            "Superstudent": 200,
+            "Student": 403,
+            "Syndic": 403
+        }
+        adminUser = createUser()
+        adminClient = APIClient()
+        adminClient.force_authenticate(user=adminUser)
+
+        data1 = {"name": "Test", "rank": 2, "description": "testRole"}
+        data2 = {"name": "Test2", "rank": 2, "description": "testRole"}
+
+        response1 = adminClient.post(f"{backend_url}/role/", data1, follow=True)
+        id = response1.data["id"]
+        for role in roles:
+            user = createUser(role)
+            client = APIClient()
+            client.force_authenticate(user)
+            response2 = client.patch(f"{backend_url}/role/{id}/", data2, follow=True)
+            assert response2.status_code == codes[role]
+
+    def test_remove_role(self):
+        codes = {
+            "Default": 403,
+            "Admin": 204,
+            "Superstudent": 204,
+            "Student": 403,
+            "Syndic": 403
+        }
+        adminUser = createUser()
+        adminClient = APIClient()
+        adminClient.force_authenticate(user=adminUser)
+
+        data1 = {"name": "Test", "rank": 2, "description": "testRole"}
+
+        exists = False
+        for role in roles:
+            if not exists:
+                # building toevoegen als admin
+                response1 = adminClient.post(f"{backend_url}/role/", data1, follow=True)
+                id = response1.data["id"]
+            # proberen verwijderen als `role`
+            user = createUser(role)
+            client = APIClient()
+            client.force_authenticate(user)
+            response2 = client.delete(f"{backend_url}/role/{id}/", follow=True)
+            assert response2.status_code == codes[role]
+            exists = codes[role] != 204
