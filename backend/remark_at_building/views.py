90610f614c7eb638500170479428bba93b1e3e5f
jonathancasters <jonathancasters284@gmail.com>
2023-04-03 16:38:00 +0200

    IndividualView RemarkAtBuilding

--- backend/remark_at_building/views.py
+++ backend/remark_at_building/views.py
@@ -5,7 +5,8 @@ from rest_framework.views import APIView
 from base.models import RemarkAtBuilding
 from base.permissions import IsSuperStudent, IsAdmin, IsStudent, OwnerAccount
 from base.serializers import RemarkAtBuildingSerializer
-from util.request_response_util import post_docs, request_to_dict, set_keys_of_instance, try_full_clean_and_save
+from util.request_response_util import post_docs, request_to_dict, set_keys_of_instance, try_full_clean_and_save, \
+    not_found, get_success
 
 TRANSLATE = {
     "student_on_tour": "student_on_tour_id",
@@ -34,3 +35,23 @@ class Default(APIView):
         self.check_object_permissions(request, remark_at_building.student_on_tour.student)
 
         return post_docs(self.serializer_class(remark_at_building))
+
+
+class RemarkAtBuildingIndividualView(APIView):
+    permission_classes = [IsAuthenticated, IsAdmin | IsSuperStudent | (IsStudent & OwnerAccount)]
+    serializer_class = RemarkAtBuildingSerializer
+
+    @extend_schema(responses=get_success(serializer_class))
+    def get(self, request, remark_at_building_id):
+        """
+        Get info about a remark at building with a given id
+        """
+        remark_at_building_instance = RemarkAtBuilding.objects.filter(
+            id=remark_at_building_id).first()
+
+        if not remark_at_building_instance:
+            return not_found(object_name="RemarkAtBuilding")
+
+        self.check_object_permissions(request, remark_at_building_instance.student_on_tour.student)
+
+        return get_success(self.serializer_class(remark_at_building_instance))
