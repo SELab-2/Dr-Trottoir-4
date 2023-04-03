ccd8bc7369a595e23e024e76dc94c903060c38a2
jonathancasters <jonathancasters284@gmail.com>
2023-04-03 16:37:46 +0200

    IndividualView PictureOfRemark

--- backend/picture_of_remark/views.py
+++ backend/picture_of_remark/views.py
@@ -5,7 +5,8 @@ from rest_framework.views import APIView
 from base.models import PictureOfRemark, StudentOnTour
 from base.permissions import IsAdmin, IsSuperStudent, IsStudent, OwnerAccount
 from base.serializers import PictureOfRemarkSerializer
-from util.request_response_util import post_docs, request_to_dict, set_keys_of_instance, try_full_clean_and_save
+from util.request_response_util import post_docs, request_to_dict, set_keys_of_instance, try_full_clean_and_save, \
+    get_success, not_found
 
 TRANSLATE = {"remark_at_building": "remark_at_building_id"}
 
@@ -31,3 +32,23 @@ class Default(APIView):
         self.check_object_permissions(request, picture_of_remark_instance.remark_at_building.student_on_tour.student)
 
         return post_docs(self.serializer_class(picture_of_remark_instance))
+
+
+class PictureOfRemarkIndividualView(APIView):
+    permission_classes = [IsAuthenticated, IsAdmin | IsSuperStudent | (IsStudent & OwnerAccount)]
+    serializer_class = PictureOfRemarkSerializer
+
+    @extend_schema(responses=get_success(serializer_class))
+    def get(self, request, remark_at_building_id):
+        """
+        Get info about a remark at building with a given id
+        """
+        picture_of_remark = PictureOfRemark.objects.filter(
+            id=remark_at_building_id).first()
+
+        if not picture_of_remark:
+            return not_found(object_name="PictureOfRemark")
+
+        self.check_object_permissions(request, picture_of_remark.student_on_tour.student)
+
+        return get_success(self.serializer_class(picture_of_remark))
