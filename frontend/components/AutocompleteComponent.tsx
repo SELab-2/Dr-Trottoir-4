731240b81cff49c488fca0c49796c92c982804ba
Sheng Tao <shengtao.tian1@gmail.com>
2023-04-03 20:50:20 +0200

    #164 fixex type errors

---
+++ frontend/components/AutocompleteComponent.tsx
@@ -0,0 +1,56 @@
+import React, {useState, useEffect} from "react";
+import TextField from "@mui/material/TextField";
+import Autocomplete, {AutocompleteChangeReason, AutocompleteRenderInputParams} from "@mui/material/Autocomplete";
+import {AxiosResponse} from "axios/index";
+
+interface Props {
+    value: string;
+    label: string;
+    fetchOptions: () =>Promise<AxiosResponse<any>>;
+    onChange: (value: string) => void;
+    mapping: (value: any) => any; //this is any because it has to be generic
+}
+
+const AutocompleteComponent: React.FC<Props> = ({value, label, fetchOptions, onChange, mapping}) => {
+    const [inputValue, setInputValue] = useState("");
+    const [options, setOptions] = useState<string[]>([]);
+
+    useEffect(() => {
+        async function fetch() {
+            try {
+                const res = await fetchOptions();
+                const availableOptions = res.data.map(mapping)
+                setOptions(availableOptions);
+            } catch (err) {
+                console.error(err);
+            }
+        }
+
+        fetch().then();
+    }, [fetchOptions]);
+
+    return (
+        <Autocomplete
+            value={value}
+            inputValue={inputValue}
+            onChange={(
+                e: React.SyntheticEvent,
+                newValue: string | null,
+            ) => {
+                onChange(newValue ?? "");
+            }}
+            onInputChange={(
+                e: React.SyntheticEvent,
+                newInputValue: string,
+            ) => {
+                setInputValue(newInputValue);
+            }}
+            options={options}
+            renderInput={(params: AutocompleteRenderInputParams) => (
+                <TextField {...params} label={label} variant="outlined" fullWidth/>
+            )}
+        />
+    );
+};
+
+export default AutocompleteComponent;
--- frontend/components/regionAutocomplete.tsx
+++ frontend/components/regionAutocomplete.tsx
@@ -2,6 +2,7 @@ import React, {useState, useEffect} from "react";
 import TextField from "@mui/material/TextField";
 import Autocomplete, {AutocompleteChangeReason} from "@mui/material/Autocomplete";
 import {getAllRegions, Region} from "@/lib/region";
+import AutocompleteComponent from "@/components/AutocompleteComponent";
 
 interface Props {
     value: string;
@@ -9,45 +10,14 @@ interface Props {
 }
 
 const RegionAutocomplete: React.FC<Props> = ({value, onChange}) => {
-    const [inputValue, setInputValue] = useState("");
-    const [regions, setRegions] = useState<string[]>([]);
-
-    useEffect(() => {
-        async function fetchRegions() {
-            try {
-                const response = await getAllRegions();
-                const availableRegions = response.data.map((region: Region) => region.region);
-                setRegions(availableRegions);
-            } catch (err) {
-                console.error(err);
-            }
-        }
-
-        fetchRegions().then();
-    }, []);
 
     return (
-        <Autocomplete
+        <AutocompleteComponent
             value={value}
-            inputValue={inputValue}
-            onChange={(
-                e: React.SyntheticEvent,
-                newValue: string,
-                reason: AutocompleteChangeReason
-            ) => {
-                onChange(newValue);
-            }}
-            onInputChange={(
-                e: React.SyntheticEvent,
-                newInputValue: string,
-            ) => {
-                setInputValue(newInputValue);
-            }}
-            options={regions}
-            renderInput={(params: JSX.IntrinsicAttributes) => (
-                <TextField {...params} label="Regio" variant="outlined" fullWidth/>
-            )}
-        />
+            label="Regio"
+            fetchOptions={getAllRegions}
+            onChange={onChange}
+            mapping={(region: Region) => region.region}/>
     );
 };
 
