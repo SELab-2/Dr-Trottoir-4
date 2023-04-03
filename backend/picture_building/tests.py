a0a750c54f1864d7588e79812c52237803af023c
sevrijss <Seppe.VanRijsselberghe@UGent.be>
2023-04-03 15:39:38 +0200

    basic testing pictures

--- backend/picture_building/tests.py
+++ backend/picture_building/tests.py
@@ -1,7 +1,7 @@
 from django.test import TestCase
 from rest_framework.test import APIClient
 
-from base.test_settings import backend_url
+from base.test_settings import backend_url, roles
 from util.data_generators import createUser, insert_dummy_building, createMemoryFile
 
 f = createMemoryFile("backend/picture_building/scrambled1.png")
@@ -206,3 +206,159 @@ class PictureTests(TestCase):
         response1 = client.post(f"{backend_url}/picture-building/", data, follow=True)
         # should be 201 because it's resolved internally
         assert response1.status_code == 201
+
+
+class PictureAuthorizationTests(TestCase):
+
+    def test_picture_list(self):
+        codes = {
+            "Default": 403,
+            "Admin": 200,
+            "Superstudent": 200,
+            "Student": 403,
+            "Syndic": 403
+        }
+        for role in roles:
+            user = createUser(role)
+            client = APIClient()
+            client.force_authenticate(user=user)
+            resp = client.get(f"{backend_url}/picture-building/all")
+            if role == "Student":
+                print(resp.status_code)
+                print(resp.data)
+            assert resp.status_code == codes[role]
+
+    def test_insert_picture(self):
+        codes = {
+            "Default": 403,
+            "Admin": 201,
+            "Superstudent": 201,
+            "Student": 201,
+            "Syndic": 403
+        }
+        adminUser = createUser()
+        adminClient = APIClient()
+        adminClient.force_authenticate(user=adminUser)
+        b_id = insert_dummy_building()
+        data = {
+            "building": b_id,
+            "picture": f,
+            "description": "kinda noisy",
+            "timestamp": "2023-03-08T12:08:29+01:00",
+            "type": "AA",
+        }
+        for role in roles:
+            user = createUser(role)
+            client = APIClient()
+            client.force_authenticate(user=user)
+
+            resp = client.post(f"{backend_url}/picture-building/", data, follow=True)
+            assert resp.status_code == codes[role]
+            if resp.status_code == 201:
+                id = resp.data["id"]
+                adminClient.delete(f"{backend_url}/picture-building/{id}/", follow=True)
+
+    def test_get_picture(self):
+        codes = {
+            "Default": 403,
+            "Admin": 200,
+            "Superstudent": 200,
+            "Student": 200,
+            "Syndic": 403
+        }
+        adminUser = createUser()
+        adminClient = APIClient()
+        adminClient.force_authenticate(user=adminUser)
+
+        b_id = insert_dummy_building()
+        data = {
+            "building": b_id,
+            "picture": f,
+            "description": "kinda noisy",
+            "timestamp": "2023-03-08T12:08:29+01:00",
+            "type": "AA",
+        }
+
+        response1 = adminClient.post(f"{backend_url}/picture-building/", data, follow=True)
+
+        id = response1.data["id"]
+        assert response1.status_code == 201
+        for role in roles:
+            user = createUser(role)
+            client = APIClient()
+            client.force_authenticate(user=user)
+            response2 = client.get(f"{backend_url}/picture-building/{id}/", follow=True)
+            if response2.status_code != codes[role]:
+                print(f"role: {role}\tcode: {response2.status_code} (expected {codes[role]})")
+            assert response2.status_code == codes[role]
+
+    def test_patch_picture(self):
+        codes = {
+            "Default": 403,
+            "Admin": 200,
+            "Superstudent": 200,
+            "Student": 200,
+            "Syndic": 403
+        }
+        adminUser = createUser()
+        adminClient = APIClient()
+        adminClient.force_authenticate(user=adminUser)
+
+        b_id = insert_dummy_building()
+        data1 = {
+            "building": b_id,
+            "picture": f,
+            "description": "kinda noisy",
+            "timestamp": "2023-03-08T12:08:29+01:00",
+            "type": "AA",
+        }
+        data2 = {
+            "building": b_id,
+            "picture": f,
+            "description": "kinda noisy",
+            "timestamp": "2023-03-08T12:08:29+01:00",
+            "type": "AA",
+        }
+
+        response1 = adminClient.post(f"{backend_url}/picture-building/", data1, follow=True)
+        id = response1.data["id"]
+        for role in roles:
+            user = createUser(role)
+            client = APIClient()
+            client.force_authenticate(user)
+            response2 = client.patch(f"{backend_url}/picture-building/{id}/", data2, follow=True)
+            assert response2.status_code == codes[role]
+
+    def test_remove_picture(self):
+        codes = {
+            "Default": 403,
+            "Admin": 204,
+            "Superstudent": 204,
+            "Student": 204,
+            "Syndic": 403
+        }
+        adminUser = createUser()
+        adminClient = APIClient()
+        adminClient.force_authenticate(user=adminUser)
+
+        b_id = insert_dummy_building()
+        data1 = {
+            "building": b_id,
+            "picture": f,
+            "description": "kinda noisy",
+            "timestamp": "2023-03-08T12:08:29+01:00",
+            "type": "AA",
+        }
+        exists = False
+        for role in roles:
+            if not exists:
+                # building toevoegen als admin
+                response1 = adminClient.post(f"{backend_url}/picture-building/", data1, follow=True)
+                id = response1.data["id"]
+            # proberen verwijderen als `role`
+            user = createUser(role)
+            client = APIClient()
+            client.force_authenticate(user)
+            response2 = client.delete(f"{backend_url}/picture-building/{id}/", follow=True)
+            assert response2.status_code == codes[role]
+            exists = codes[role] != 204
