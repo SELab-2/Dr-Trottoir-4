a18828da12072c994ae16706c89d9625c4868cf1
n00bS-oWn-m3 <sebastiaan@onyx.be>
2023-04-04 09:15:36 +0200

    made file executable

--- runtests.sh
+++ runtests.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
-exec -T backend python manage.py test building/tests.py role/tests.py building_comment/tests.py building_on_tour/tests.py garbage_collection/tests.py manual/tests.py region/tests.py tour/tests.py picture_building/tests.py student_at_building_on_tour/tests.py --with-coverage --cover-package=building,building_on_tour,garbage_collection,manual,region,tour,building_comment,picture_building,student_at_building_on_tour,role
+docker-compose exec -T backend python manage.py test building/tests.py role/tests.py building_comment/tests.py building_on_tour/tests.py garbage_collection/tests.py manual/tests.py region/tests.py tour/tests.py picture_building/tests.py student_at_building_on_tour/tests.py --with-coverage --cover-package=building,building_on_tour,garbage_collection,manual,region,tour,building_comment,picture_building,student_at_building_on_tour,role
 
 # wait script
 echo "Press any key to continue"
