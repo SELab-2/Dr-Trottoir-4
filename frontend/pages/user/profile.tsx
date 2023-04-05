b7beaca52c084c3ea9f509b946db55a8985d4ae1
simvadnbu <simvadnbu@users.noreply.github.com>
2023-04-05 12:13:04 +0000

    Auto formatted code

--- frontend/pages/user/profile.tsx
+++ frontend/pages/user/profile.tsx
@@ -1,16 +1,16 @@
-import React, {useEffect, useState} from "react";
-import {getCurrentUser, getUserRole, patchUser, User} from "@/lib/user";
+import React, { useEffect, useState } from "react";
+import { getCurrentUser, getUserRole, patchUser, User } from "@/lib/user";
 import styles from "@/styles/Login.module.css";
 import PhoneInput from "react-phone-input-2";
-import {useTranslation} from "react-i18next";
-import {getAllRegions, RegionInterface} from "@/lib/region";
+import { useTranslation } from "react-i18next";
+import { getAllRegions, RegionInterface } from "@/lib/region";
 import AdminHeader from "@/components/header/adminHeader";
 import StudentHeader from "@/components/header/studentHeader";
 import SyndicHeader from "@/components/header/syndicHeader";
-import {getAndSetErrors} from "@/lib/error";
+import { getAndSetErrors } from "@/lib/error";
 
 export default function UserProfile() {
-    const {t} = useTranslation();
+    const { t } = useTranslation();
     const [user, setUser] = useState<User | null>(null);
     const [firstName, setFirstName] = useState<string>("");
     const [lastName, setLastName] = useState<string>("");
@@ -24,19 +24,22 @@ export default function UserProfile() {
     const [succesPatch, setSuccessPatch] = useState<boolean>(false);
 
     useEffect(() => {
-        getCurrentUser().then(res => {
-            const u: User = res.data;
-            setUserInfo(u);
-        }, err => {
-            console.error(err);
-        });
+        getCurrentUser().then(
+            (res) => {
+                const u: User = res.data;
+                setUserInfo(u);
+            },
+            (err) => {
+                console.error(err);
+            }
+        );
         getAllRegions().then((res) => {
             const regions: RegionInterface[] = res.data;
             setAllRegions(regions);
         });
     }, []);
 
-    function setUserInfo(u : User) {
+    function setUserInfo(u: User) {
         setRole(getUserRole(u.role.toString()));
         setUser(u);
         setFirstName(u.first_name);
@@ -66,35 +69,32 @@ export default function UserProfile() {
         if (selectedRegions !== user?.region) {
             patchBody["region"] = JSON.stringify(selectedRegions); // convert list to string
         }
-        patchUser(user.id, patchBody).then((res) => {
-            console.log(res.data);
-            const u : User = res.data;
-            setUserInfo(u);
-            setSuccessPatch(true);
-        }, err => {
-            let errorRes = err.response;
-            if (errorRes && errorRes.status === 400) {
-                getAndSetErrors(Object.entries(errorRes.data), setErrorMessages);
-            } else if (errorRes && errorRes.status === 403) {
-                const errorData: [any, string][] = Object.entries(errorRes.data);
-                setErrorMessages(errorData.map((val) => val[1]));
-            } else {
-                console.error(err);
+        patchUser(user.id, patchBody).then(
+            (res) => {
+                console.log(res.data);
+                const u: User = res.data;
+                setUserInfo(u);
+                setSuccessPatch(true);
+            },
+            (err) => {
+                let errorRes = err.response;
+                if (errorRes && errorRes.status === 400) {
+                    getAndSetErrors(Object.entries(errorRes.data), setErrorMessages);
+                } else if (errorRes && errorRes.status === 403) {
+                    const errorData: [any, string][] = Object.entries(errorRes.data);
+                    setErrorMessages(errorData.map((val) => val[1]));
+                } else {
+                    console.error(err);
+                }
             }
-        });
+        );
     }
 
     return (
         <>
-            {
-                (["Admin", "Superstudent"].includes(role)) && (<AdminHeader/>)
-            }
-            {
-                ("Student" === role) && (<StudentHeader/>)
-            }
-            {
-                ("Syndic" === role) && (<SyndicHeader/>)
-            }
+            {["Admin", "Superstudent"].includes(role) && <AdminHeader />}
+            {"Student" === role && <StudentHeader />}
+            {"Syndic" === role && <SyndicHeader />}
             {errorMessages.length !== 0 && (
                 <div className={"visible alert alert-danger alert-dismissible fade show"}>
                     <ul>
@@ -105,20 +105,14 @@ export default function UserProfile() {
                     <button type="button" className="btn-close" onClick={() => setErrorMessages([])}></button>
                 </div>
             )}
-            {
-                (succesPatch) && (
-                    <div className={"visible alert alert-success alert-dismissible fade show"}>
-                        <strong>Succes!</strong> Uw profiel werd met succes gewijzigd!
-                        <button
-                            type="button"
-                            className="btn-close"
-                            onClick={() => setSuccessPatch(false)}
-                        ></button>
-                    </div>
-                )
-            }
+            {succesPatch && (
+                <div className={"visible alert alert-success alert-dismissible fade show"}>
+                    <strong>Succes!</strong> Uw profiel werd met succes gewijzigd!
+                    <button type="button" className="btn-close" onClick={() => setSuccessPatch(false)}></button>
+                </div>
+            )}
             <div className="d-flex align-items-center mb-3 pb-1">
-                <i className="fas fa-cubes fa-2x me-3"/>
+                <i className="fas fa-cubes fa-2x me-3" />
                 <span className="h1 fw-bold mb-0">Profiel</span>
             </div>
 
@@ -185,19 +179,25 @@ export default function UserProfile() {
 
             <div className="form-outline mb-4">
                 <label className="form-label">Regio's waarin u wilt werken:</label>
-                <select className="form-select" multiple aria-label="multiple select example"
-                        title={"Dit is enkel van toepassing voor (super-)studenten/admins."}
-                        value={selectedRegions.map(n => n.toString())}
-                        onChange={(e: React.ChangeEvent<HTMLSelectElement>) => {
-                            const options: HTMLOptionElement[] = Array.from(e.target.selectedOptions);
-                            const values: number[] = options.map(option => Number(option.value));
-                            setSelectedRegions(values);
-                        }}>
-                    {
-                        allRegions?.map((r: RegionInterface) => {
-                            return (<option value={r.id} key={r.id}>{r.region}</option>)
-                        })
-                    }
+                <select
+                    className="form-select"
+                    multiple
+                    aria-label="multiple select example"
+                    title={"Dit is enkel van toepassing voor (super-)studenten/admins."}
+                    value={selectedRegions.map((n) => n.toString())}
+                    onChange={(e: React.ChangeEvent<HTMLSelectElement>) => {
+                        const options: HTMLOptionElement[] = Array.from(e.target.selectedOptions);
+                        const values: number[] = options.map((option) => Number(option.value));
+                        setSelectedRegions(values);
+                    }}
+                >
+                    {allRegions?.map((r: RegionInterface) => {
+                        return (
+                            <option value={r.id} key={r.id}>
+                                {r.region}
+                            </option>
+                        );
+                    })}
                 </select>
             </div>
 
