2a4a13dda19812e3d065747fef4c323797fa223e
Sheng Tao <shengtao.tian1@gmail.com>
2023-04-05 22:44:14 +0200

    #276 started on region page

--- frontend/lib/region.ts
+++ frontend/lib/region.ts
@@ -6,6 +6,10 @@ export interface RegionInterface {
     region: string;
 }
 
+export interface RegionPostInterface {
+    region: string;
+}
+
 export async function getAllRegions(): Promise<AxiosResponse<any>> {
     const request_url: string = `${process.env.NEXT_PUBLIC_BASE_API_URL}${process.env.NEXT_PUBLIC_API_ALL_REGIONS}`;
     return await api.get(request_url);
@@ -13,6 +17,15 @@ export async function getAllRegions(): Promise<AxiosResponse<any>> {
 
 export async function getRegion(regionId: number): Promise<AxiosResponse<any>> {
     const request_url: string = `${process.env.NEXT_PUBLIC_BASE_API_URL}${process.env.NEXT_PUBLIC_API_REGION}${regionId}`;
-    console.log(request_url)
+    return await api.get(request_url);
+}
+
+export async function patchRegion(regionId: number): Promise<AxiosResponse<any>> {
+    const request_url: string = `${process.env.NEXT_PUBLIC_BASE_API_URL}${process.env.NEXT_PUBLIC_API_REGION}${regionId}`;
+    return await api.get(request_url);
+}
+
+export async function postRegion(region: RegionPostInterface): Promise<AxiosResponse<any>> {
+    const request_url: string = `${process.env.NEXT_PUBLIC_BASE_API_URL}${process.env.NEXT_PUBLIC_API_REGION}`;
     return await api.get(request_url);
 }
---
+++ frontend/pages/admin/data/regions/index.tsx
@@ -0,0 +1,211 @@
+import AdminHeader from "@/components/header/adminHeader";
+import React, {useEffect, useMemo, useState} from "react";
+import {
+    RegionInterface,
+    RegionPostInterface,
+    getAllRegions,
+    postRegion,
+    patchRegion,
+} from "@/lib/region";
+import {withAuthorisation} from "@/components/withAuthorisation";
+import MaterialReactTable, {type MRT_ColumnDef} from "material-react-table";
+import {
+    Box,
+    IconButton,
+    Tooltip,
+    Dialog,
+    DialogTitle,
+    DialogContent,
+    DialogActions,
+    TextField,
+    Button
+} from "@mui/material";
+import {Edit, Delete} from "@mui/icons-material";
+import {useRouter} from "next/router"
+
+interface RegionView extends RegionInterface {
+}
+
+function AdminDataRegions() {
+    const router = useRouter();
+    const [regions, setRegions] = useState<RegionView[]>([]);
+    const [loading, setLoading] = useState<boolean>(true);
+    const [selectedRegion, setSelectedRegion] = useState<RegionView | null>(null);
+    const [regionName, setRegionName] = useState<string>("");
+    const [addDialogOpen, setAddDialogOpen] = useState<boolean>(false);
+    const [editDialogOpen, setEditDialogOpen] = useState<boolean>(false);
+    const [deleteDialogOpen, setDeleteDialogOpen] = useState<boolean>(false);
+
+
+    const columns = useMemo<MRT_ColumnDef<RegionView>[]>(
+        () => [
+            {
+                accessorKey: "region",
+                header: "Region Name",
+            },
+        ],
+        []
+    );
+
+    useEffect(() => {
+        getAllRegions().then(
+            (res) => {
+                const regions: RegionInterface[] = res.data;
+                setRegions(regions);
+                setLoading(false);
+            },
+            (err) => {
+                console.error(err);
+                setLoading(false);
+            }
+        );
+    }, []);
+
+    async function addNewRegion() {
+        try {
+            const newRegion = {region: regionName};
+            const res = await postRegion(newRegion);
+            setRegions([...regions, res.data]);
+            setRegionName("");
+            setAddDialogOpen(false);
+        } catch (error) {
+            console.error(error);
+        }
+    }
+
+    async function updateRegion() {
+        if (selectedRegion) {
+            try {
+                const updatedRegion = {...selectedRegion, region: regionName};
+                await patchRegion(selectedRegion.id);
+                setRegions(
+                    regions.map((region) =>
+                        region.id === selectedRegion.id ? updatedRegion : region
+                    )
+                );
+                setSelectedRegion(null);
+                setRegionName("");
+                setEditDialogOpen(false);
+            } catch (error) {
+                console.error(error);
+            }
+        }
+    }
+
+    return (
+        <>
+            <AdminHeader/>
+            <MaterialReactTable
+                displayColumnDefOptions={{
+                    "mrt-row-actions": {
+                        muiTableHeadCellProps: {
+                            align: "center",
+                        },
+                        header: "Actions",
+                    },
+                }}
+                enablePagination={false}
+                enableBottomToolbar={false}
+                columns={columns}
+                data={regions}
+                state={{isLoading: loading}}
+                enableEditing
+                enableHiding={false}
+                renderRowActions={({row}) => (
+                    <Box sx={{display: "flex", gap: "1rem"}}>
+                        <Tooltip arrow placement="left" title="Edit">
+                            <IconButton
+                                onClick={() => {
+                                    const region: RegionView = row.original;
+                                    setSelectedRegion(region);
+                                    setRegionName(region.region);
+                                    setEditDialogOpen(true);
+                                }}
+                            >
+                                <Edit/>
+                            </IconButton>
+                        </Tooltip>
+                        <Tooltip arrow placement="right" title="Verwijder">
+                            <IconButton
+                                onClick={() => {
+                                    const regionView: RegionView = row.original;
+                                    setSelectedRegion(regionView);
+                                    setDeleteDialogOpen(true);
+                                }}
+                            >
+                                <Delete/>
+                            </IconButton>
+                        </Tooltip>
+                    </Box>
+                )}
+                renderTopToolbarCustomActions={() => (
+                    <Button onClick={() => setAddDialogOpen(true)} variant="warning">
+                        Add New Region
+                    </Button>
+                )}
+            />
+            <Dialog open={addDialogOpen} onClose={() => setAddDialogOpen(false)}>
+                <DialogTitle>Add New Region</DialogTitle>
+                <DialogContent>
+                    <TextField
+                        autoFocus
+                        margin="dense"
+                        id="name"
+                        label="Region Name"
+                        type="text"
+                        fullWidth
+                        value={regionName}
+                        onChange={(e) => setRegionName(e.target.value)}
+                    />
+                </DialogContent>
+                <DialogActions>
+                    <Button onClick={() => setAddDialogOpen(false)} variant="secondary">
+                        Cancel
+                    </Button>
+                    <Button onClick={addNewRegion} variant="primary">
+                        Add
+                    </Button>
+                </DialogActions>
+            </Dialog>
+            <Dialog open={editDialogOpen} onClose={() => setEditDialogOpen(false)}>
+                <DialogTitle>Edit Region</DialogTitle>
+                <DialogContent>
+                    <TextField
+                        autoFocus
+                        margin="dense"
+                        id="name"
+                        label="Region Name"
+                        type="text"
+                        fullWidth
+                        value={regionName}
+                        onChange={(e) => setRegionName(e.target.value)}
+                    />
+                </DialogContent>
+                <DialogActions>
+                    <Button onClick={() => setEditDialogOpen(false)} variant="secondary">
+                        Cancel
+                    </Button>
+                    <Button onClick={updateRegion} variant="primary">
+                        Save
+                    </Button>
+                </DialogActions>
+            </Dialog>
+            <DeleteConfirmationDialog
+                open={deleteDialogOpen}
+                title="Verwijder Regio"
+                description="Weet u zeker dat u deze regio wilt verwijderen?"
+                handleClose={() => setDeleteDialogOpen(false)}
+                handleConfirm={() => {
+                    if (deletingRegion) {
+                        removeRegion(deletingRegion);
+                    }
+                    setDeleteDialogOpen(false);
+                }}
+                confirmButtonText="Verwijderen"
+                cancelButtonText="Annuleren"
+            />
+        </>
+    );
+}
+
+export default withAuthorisation(AdminDataRegions, ["Admin", "Superstudent"]);
