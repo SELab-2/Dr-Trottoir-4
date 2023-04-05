f72c89c2999f26264445e1e26bf37cbe9cfc9dd5
Sheng Tao <shengtao.tian1@gmail.com>
2023-04-05 12:49:51 +0200

    #164 replaced Region with RegionInterface in regionAutocomplete.tsx

--- frontend/components/autocompleteComponents/regionAutocomplete.tsx
+++ frontend/components/autocompleteComponents/regionAutocomplete.tsx
@@ -1,5 +1,5 @@
 import React from "react";
-import { getAllRegions, Region } from "@/lib/region";
+import { getAllRegions, RegionInterface } from "@/lib/region";
 import AutocompleteComponent, { GenericProps } from "@/components/autocompleteComponents/autocompleteComponent";
 
 const RegionAutocomplete: React.FC<GenericProps> = ({ value, onChange, setObjectId, required }) => {
@@ -9,7 +9,7 @@ const RegionAutocomplete: React.FC<GenericProps> = ({ value, onChange, setObject
             label={`Regio${required ? "*" : ""}`}
             fetchOptions={getAllRegions}
             onChange={onChange}
-            mapping={(region: Region) => region.region}
+            mapping={(region: RegionInterface) => region.region}
             searchField={"region"}
             searchTermHandler={(value) => value}
             setObjectId={setObjectId}
