76e2d2ab37619e53742ad7424b8e7209388f9f23
simvadnbu <simivdb@gmail.com>
2023-04-05 17:09:32 +0200

    Changed multiselect to checkbox

--- frontend/pages/user/profile.tsx
+++ frontend/pages/user/profile.tsx
@@ -1,16 +1,16 @@
-import React, { useEffect, useState } from "react";
-import { getCurrentUser, getUserRole, patchUser, User } from "@/lib/user";
+import React, {useEffect, useState} from "react";
+import {getCurrentUser, getUserRole, patchUser, User} from "@/lib/user";
 import styles from "@/styles/Login.module.css";
 import PhoneInput from "react-phone-input-2";
-import { useTranslation } from "react-i18next";
-import { getAllRegions, RegionInterface } from "@/lib/region";
+import {useTranslation} from "react-i18next";
+import {getAllRegions, RegionInterface} from "@/lib/region";
 import AdminHeader from "@/components/header/adminHeader";
 import StudentHeader from "@/components/header/studentHeader";
 import SyndicHeader from "@/components/header/syndicHeader";
-import { getAndSetErrors } from "@/lib/error";
+import {getAndSetErrors} from "@/lib/error";
 
 export default function UserProfile() {
-    const { t } = useTranslation();
+    const {t} = useTranslation();
     const [user, setUser] = useState<User | null>(null);
     const [firstName, setFirstName] = useState<string>("");
     const [lastName, setLastName] = useState<string>("");
@@ -90,11 +90,15 @@ export default function UserProfile() {
         );
     }
 
+    useEffect(() => {
+        console.log(selectedRegions);
+    }, [selectedRegions])
+
     return (
         <>
-            {["Admin", "Superstudent"].includes(role) && <AdminHeader />}
-            {"Student" === role && <StudentHeader />}
-            {"Syndic" === role && <SyndicHeader />}
+            {["Admin", "Superstudent"].includes(role) && <AdminHeader/>}
+            {"Student" === role && <StudentHeader/>}
+            {"Syndic" === role && <SyndicHeader/>}
             {errorMessages.length !== 0 && (
                 <div className={"visible alert alert-danger alert-dismissible fade show"}>
                     <ul>
@@ -111,96 +115,105 @@ export default function UserProfile() {
                     <button type="button" className="btn-close" onClick={() => setSuccessPatch(false)}></button>
                 </div>
             )}
-            <div className="d-flex align-items-center mb-3 pb-1">
-                <i className="fas fa-cubes fa-2x me-3" />
-                <span className="h1 fw-bold mb-0">Profiel</span>
-            </div>
+            <form>
+                <div className="d-flex align-items-center mb-3 pb-1">
+                    <i className="fas fa-cubes fa-2x me-3"/>
+                    <span className="h1 fw-bold mb-0">Profiel</span>
+                </div>
 
-            <div className="form-outline mb-4">
-                <label className="form-label">Voornaam</label>
-                <input
-                    type="text"
-                    className={`form-control form-control-lg ${styles.input}`}
-                    value={firstName}
-                    onChange={(e: React.ChangeEvent<HTMLInputElement>) => {
-                        setFirstName(e.target.value);
-                        e.target.setCustomValidity("");
-                    }}
-                    onInvalid={(e: React.ChangeEvent<HTMLInputElement>) => {
-                        e.target.setCustomValidity("Voornaam is verplicht.");
-                    }}
-                    required
-                />
-            </div>
+                <div className="form-outline mb-4">
+                    <label className="form-label">Voornaam</label>
+                    <input
+                        type="text"
+                        className={`form-control form-control-lg ${styles.input}`}
+                        value={firstName}
+                        onChange={(e: React.ChangeEvent<HTMLInputElement>) => {
+                            setFirstName(e.target.value);
+                            e.target.setCustomValidity("");
+                        }}
+                        onInvalid={(e: React.ChangeEvent<HTMLInputElement>) => {
+                            e.target.setCustomValidity("Voornaam is verplicht.");
+                        }}
+                        required
+                    />
+                </div>
 
-            <div className="form-outline mb-4">
-                <label className="form-label">Achternaam</label>
-                <input
-                    type="text"
-                    className={`form-control form-control-lg ${styles.input}`}
-                    value={lastName}
-                    onChange={(e: React.ChangeEvent<HTMLInputElement>) => {
-                        setLastName(e.target.value);
-                        e.target.setCustomValidity("");
-                    }}
-                    onInvalid={(e: React.ChangeEvent<HTMLInputElement>) => {
-                        e.target.setCustomValidity("Achternaam is verplicht.");
-                    }}
-                    required
-                />
-            </div>
+                <div className="form-outline mb-4">
+                    <label className="form-label">Achternaam</label>
+                    <input
+                        type="text"
+                        className={`form-control form-control-lg ${styles.input}`}
+                        value={lastName}
+                        onChange={(e: React.ChangeEvent<HTMLInputElement>) => {
+                            setLastName(e.target.value);
+                            e.target.setCustomValidity("");
+                        }}
+                        onInvalid={(e: React.ChangeEvent<HTMLInputElement>) => {
+                            e.target.setCustomValidity("Achternaam is verplicht.");
+                        }}
+                        required
+                    />
+                </div>
 
-            <div className="form-outline mb-4">
-                <label className="form-label">E-mailadres</label>
-                <input
-                    type="email"
-                    className={`form-control form-control-lg ${styles.input}`}
-                    value={email}
-                    onChange={(e: React.ChangeEvent<HTMLInputElement>) => {
-                        setEmail(e.target.value);
-                    }}
-                    required
-                />
-            </div>
+                <div className="form-outline mb-4">
+                    <label className="form-label">E-mailadres</label>
+                    <input
+                        type="email"
+                        className={`form-control form-control-lg ${styles.input}`}
+                        value={email}
+                        onChange={(e: React.ChangeEvent<HTMLInputElement>) => {
+                            setEmail(e.target.value);
+                        }}
+                        required
+                    />
+                </div>
 
-            <div className="form-outline mb-4">
-                <label className="form-label">Gsm-nummer</label>
-                <PhoneInput
-                    country={"be"}
-                    value={phoneNumber}
-                    preferredCountries={["be", "nl"]}
-                    onChange={(phone) => setPhoneNumber("+" + phone)}
-                />
-            </div>
+                <div className="form-outline mb-4">
+                    <label className="form-label">Gsm-nummer</label>
+                    <PhoneInput
+                        country={"be"}
+                        value={phoneNumber}
+                        preferredCountries={["be", "nl"]}
+                        onChange={(phone) => setPhoneNumber("+" + phone)}
+                    />
+                </div>
 
-            <div className="form-outline mb-4">
-                <label className="form-label">{`Rol : ${user ? t(getUserRole(user.role.toString())) : ""}`}</label>
-            </div>
+                <div className="form-outline mb-4">
+                    <label className="form-label">{`Rol : ${user ? t(getUserRole(user.role.toString())) : ""}`}</label>
+                </div>
 
-            <div className="form-outline mb-4">
-                <label className="form-label">Regio's waarin u wilt werken:</label>
-                <select
-                    className="form-select"
-                    multiple
-                    aria-label="multiple select example"
-                    title={"Dit is enkel van toepassing voor (super-)studenten/admins."}
-                    value={selectedRegions.map((n) => n.toString())}
-                    onChange={(e: React.ChangeEvent<HTMLSelectElement>) => {
-                        const options: HTMLOptionElement[] = Array.from(e.target.selectedOptions);
-                        const values: number[] = options.map((option) => Number(option.value));
-                        setSelectedRegions(values);
-                    }}
-                >
-                    {allRegions?.map((r: RegionInterface) => {
-                        return (
-                            <option value={r.id} key={r.id}>
-                                {r.region}
-                            </option>
-                        );
-                    })}
-                </select>
-            </div>
-
+                <div className="form-outline mb-4">
+                    <label className="form-label">Regio's waarin u wilt werken:</label>
+                    {
+                        allRegions?.map((r: RegionInterface) => {
+                            return (
+                                <div className="form-check" key={r.id}>
+                                    <input className="form-check-input" type="checkbox" value={r.id}
+                                           id={r.id.toString()}
+                                           checked={selectedRegions.some((n: number) => n === r.id)}
+                                           onChange={(e: React.ChangeEvent<HTMLInputElement>) => {
+                                               const regionId = Number(e.target.value)
+                                               const regions = [...selectedRegions];
+                                               if (e.target.checked && ! selectedRegions.find((el : number) => el === regionId)) {
+                                                   regions.push(regionId);
+                                                   setSelectedRegions(regions);
+                                               } else if (! e.target.checked && selectedRegions.find((el : number) => el === regionId)) {
+                                                   const i = regions.indexOf(regionId);
+                                                   if (i > -1) {
+                                                       regions.splice(i, 1);
+                                                   }
+                                                   setSelectedRegions(regions);
+                                               }
+                                           }}/>
+                                    <label className="form-check-label" htmlFor={r.id.toString()}>
+                                        {r.region}
+                                    </label>
+                                </div>
+                            );
+                        })
+                    }
+                </div>
+            </form>
             <div className="pt-1 mb-4">
                 <button className={`btn btn-dark btn-lg btn-block ${styles.button}`} onClick={submit}>
                     Pas aan
