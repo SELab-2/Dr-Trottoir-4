514b1c9c1d7597c35ddde74f56e626a9140d222d
n00bS-oWn-m3 <sebastiaan@onyx.be>
2023-04-05 17:04:22 +0200

    route to building and support going from mail to communication

--- frontend/pages/admin/communication.tsx
+++ frontend/pages/admin/communication.tsx
@@ -3,15 +3,20 @@ import { BuildingComment, getAllBuildingComments } from "@/lib/building-comment"
 import { EmailTemplate, getAllEmailTemplates } from "@/lib/email-template";
 import { useEffect, useState, ChangeEvent } from "react";
 import styles from 'styles/Welcome.module.css';
-import { Dropdown, DropdownButton, FloatingLabel, Form, FormControl } from "react-bootstrap";
+import { Button, Dropdown, DropdownButton, FloatingLabel, Form, FormControl, Offcanvas } from "react-bootstrap";
 import TemplateAutocomplete from "@/components/autocompleteComponents/templateAutocomplete";
 import { BuildingInterface, getAllBuildings } from "@/lib/building";
 import { getAllUsers, User, userSearchString } from "@/lib/user";
 import SyndicAutoCompleteComponent from "@/components/autocompleteComponents/syndicAutoCompleteComponent";
+import router, { useRouter } from "next/router";
 
+interface ParsedUrlQuery {}
+
+interface DataCommunicationQuery extends ParsedUrlQuery {
+    template?: number;
+}
 
 export default function AdminCommunication() {
-    const [loading, setLoading] = useState(true);
     const [allTemplates, setAllTemplates] = useState<EmailTemplate[]>([]);
     const [allComments, setAllComments] = useState<BuildingComment[]>([]);
     const [allBuildings, setAllBuildings] = useState<BuildingInterface[]>([]);
@@ -22,10 +27,11 @@ export default function AdminCommunication() {
     const [templateText, setTemplateText] = useState("");
     const [updatedTemplateText, setUpdatedTemplateText] = useState("");
     const [templateId, setTemplateId] = useState("");
-    const [commentId, setCommentId] = useState("");
     const [syndicId, setSyndicId] = useState("");
     const [buildingId, setBuildingId] = useState("");
-    
+    const router = useRouter();
+    const query: DataCommunicationQuery = router.query as DataCommunicationQuery;
+
     const replaceVariable = (str: string, variable: string, value: string) : string => {
         const regex = new RegExp(`{{\\s*${variable}\\s*}}`, 'g');
         return str.replace(regex, value);
@@ -53,15 +59,32 @@ export default function AdminCommunication() {
             setUpdatedTemplateText(changedVariablesText);
     }
 
+    async function routeToBuilding(buildingId: string) {
+        await router.push({
+            pathname: `building/`,
+            query: { building: buildingId },
+        });
+    }
+
+
     useEffect(() => {
+        
         getAllEmailTemplates().then(res => {
             const emailTemplates : EmailTemplate[] = res.data;
             setAllTemplates(emailTemplates);
-            setSelectedTemplate(emailTemplates[0].name);
-            setTemplateText(emailTemplates[0].template);
+            let currentTemplate = emailTemplates[0];
+            if (query.template) {
+                console.log(emailTemplates);
+                console.log(query.template);
+                currentTemplate = emailTemplates.find(e => e.id === Number(query.template)) || emailTemplates[0];
+            }
+                setSelectedTemplate(currentTemplate.name);
+                setTemplateId(currentTemplate.id.toString());
+                setTemplateText(currentTemplate.template);
         }, err => {
             console.error(err);
         });
+        
 
         getAllBuildingComments().then(res => {
             const buildingComments : BuildingComment[] = res.data;
@@ -82,7 +105,7 @@ export default function AdminCommunication() {
             const buildings : BuildingInterface[] = res.data;
             setAllBuildings(buildings);
         });
-    }, []);
+    }, [router.isReady]);
 
     useEffect(() => {
         const changedVariablesText = fillInVariables(templateText);
@@ -107,33 +130,48 @@ export default function AdminCommunication() {
             <>
                 <AdminHeader/>
                 <p className={styles.title}>Communicatie extern</p>
-                <p className={styles.text}>Kies een template:</p>
                 <div style={{ display: 'flex', width:"100%"  }}>
-                    <div style={{ width: "35%" }}>
-                        <TemplateAutocomplete
-                            value={selectedTemplate}
-                            onChange={setSelectedTemplate}
-                            setObjectId={setTemplateId}
-                            required={false}
-                        ></TemplateAutocomplete>
-                    </div>
-                    <div style={{ width: "35%" }}>
-                        <SyndicAutoCompleteComponent
-                            value={selectedSyndic}
-                            onChange={setSelectedSyndic}
-                            setObjectId={setSyndicId}
-                            required={true}
-                        ></SyndicAutoCompleteComponent>
-                    </div>
+                    <div style={{ width: "10%" }}>
                     
-                
+                    </div>
+                    <div style={{ display: 'flex', width:"100%"  }}>
+                        <div style={{ width: "33%" }}>
+                            <TemplateAutocomplete
+                                value={selectedTemplate}
+                                onChange={setSelectedTemplate}
+                                setObjectId={setTemplateId}
+                                required={false}
+                            ></TemplateAutocomplete>
+                        </div>
+                        <div style={{ width: "33%" }}>
+                            <SyndicAutoCompleteComponent
+                                value={selectedSyndic}
+                                onChange={setSelectedSyndic}
+                                setObjectId={setSyndicId}
+                                required={false}
+                            ></SyndicAutoCompleteComponent>
+                        </div>
+                        <div style={{ width: "33%" }}>
+                        <Button variant="secondary" size="lg"
+                        onClick={() => {routeToBuilding(buildingId).then()}}>
+                            Building
+                        </Button>
+
+                        </div>
+                    </div>
+                    <div style={{ width: "10%" }}>
+                        
+                    </div>
                 </div>
                 <div style={{ display: 'flex'}}>
+                    <div style={{ width: "10%" }}>
+                    
+                    </div>
                     <FloatingLabel
                         controlId="floatingTextarea"
                         label="Email"
                         className="mb-3"
-                        style={{ width: "70%" }}
+                        style={{ width: "100%" }}
                     >
                         <Form.Control 
                         as="textarea" 
@@ -143,7 +181,7 @@ export default function AdminCommunication() {
                         onChange={handleEditTemplate}
                         />
                     </FloatingLabel>
-                    <div style={{ width: "30%" }}>
+                    <div style={{ width: "10%" }}>
                     
                     </div>
                 </div>
