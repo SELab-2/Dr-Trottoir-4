f2adb2267d92afdcf23713e12c9547a2679e66bc
GashinRS <GashinRS@users.noreply.github.com>
2023-04-05 21:14:28 +0000

    Auto formatted code

--- frontend/pages/admin/data/regions/index.tsx
+++ frontend/pages/admin/data/regions/index.tsx
@@ -1,30 +1,15 @@
 import AdminHeader from "@/components/header/adminHeader";
-import React, {useEffect, useMemo, useState} from "react";
-import {
-    RegionInterface,
-    getAllRegions,
-    postRegion,
-    patchRegion, deleteRegion,
-} from "@/lib/region";
-import {withAuthorisation} from "@/components/withAuthorisation";
-import MaterialReactTable, {type MRT_ColumnDef} from "material-react-table";
-import {
-    Box,
-    IconButton,
-    Tooltip,
-    Dialog,
-    DialogTitle,
-    DialogContent,
-    DialogActions,
-    TextField
-} from "@mui/material";
-import {Edit, Delete} from "@mui/icons-material";
-import {useRouter} from "next/router"
+import React, { useEffect, useMemo, useState } from "react";
+import { RegionInterface, getAllRegions, postRegion, patchRegion, deleteRegion } from "@/lib/region";
+import { withAuthorisation } from "@/components/withAuthorisation";
+import MaterialReactTable, { type MRT_ColumnDef } from "material-react-table";
+import { Box, IconButton, Tooltip, Dialog, DialogTitle, DialogContent, DialogActions, TextField } from "@mui/material";
+import { Edit, Delete } from "@mui/icons-material";
+import { useRouter } from "next/router";
 import DeleteConfirmationDialog from "@/components/deleteConfirmationDialog";
-import {Button} from "react-bootstrap";
+import { Button } from "react-bootstrap";
 
-interface RegionView extends RegionInterface {
-}
+interface RegionView extends RegionInterface {}
 
 function AdminDataRegions() {
     const router = useRouter();
@@ -35,7 +20,6 @@ function AdminDataRegions() {
     const [addDialogOpen, setAddDialogOpen] = useState<boolean>(false);
     const [editDialogOpen, setEditDialogOpen] = useState<boolean>(false);
     const [deleteDialogOpen, setDeleteDialogOpen] = useState<boolean>(false);
-
 
     const columns = useMemo<MRT_ColumnDef<RegionView>[]>(
         () => [
@@ -63,7 +47,7 @@ function AdminDataRegions() {
 
     async function addNewRegion() {
         try {
-            const newRegion = {region: regionName};
+            const newRegion = { region: regionName };
             const res = await postRegion(newRegion);
             setRegions([...regions, res.data]);
             setRegionName("");
@@ -76,13 +60,9 @@ function AdminDataRegions() {
     async function updateRegion() {
         if (selectedRegion) {
             try {
-                const updatedRegion = {...selectedRegion, region: regionName};
+                const updatedRegion = { ...selectedRegion, region: regionName };
                 await patchRegion(selectedRegion.id);
-                setRegions(
-                    regions.map((region) =>
-                        region.id === selectedRegion.id ? updatedRegion : region
-                    )
-                );
+                setRegions(regions.map((region) => (region.id === selectedRegion.id ? updatedRegion : region)));
                 setSelectedRegion(null);
                 setRegionName("");
                 setEditDialogOpen(false);
@@ -94,7 +74,7 @@ function AdminDataRegions() {
 
     return (
         <>
-            <AdminHeader/>
+            <AdminHeader />
             <MaterialReactTable
                 displayColumnDefOptions={{
                     "mrt-row-actions": {
@@ -108,11 +88,11 @@ function AdminDataRegions() {
                 enableBottomToolbar={false}
                 columns={columns}
                 data={regions}
-                state={{isLoading: loading}}
+                state={{ isLoading: loading }}
                 enableEditing
                 enableHiding={false}
-                renderRowActions={({row}) => (
-                    <Box sx={{display: "flex", gap: "1rem"}}>
+                renderRowActions={({ row }) => (
+                    <Box sx={{ display: "flex", gap: "1rem" }}>
                         <Tooltip arrow placement="left" title="Edit">
                             <IconButton
                                 onClick={() => {
@@ -122,7 +102,7 @@ function AdminDataRegions() {
                                     setEditDialogOpen(true);
                                 }}
                             >
-                                <Edit/>
+                                <Edit />
                             </IconButton>
                         </Tooltip>
                         <Tooltip arrow placement="right" title="Verwijder">
@@ -133,7 +113,7 @@ function AdminDataRegions() {
                                     setDeleteDialogOpen(true);
                                 }}
                             >
-                                <Delete/>
+                                <Delete />
                             </IconButton>
                         </Tooltip>
                     </Box>
