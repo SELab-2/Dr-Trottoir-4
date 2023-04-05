db723bf313de32ae47375f1694f64d21a5bcb293
n00bS-oWn-m3 <sebastiaan@onyx.be>
2023-04-05 18:20:21 +0200

    #173 basic functionality; basic styling

--- frontend/pages/admin/analysis/index.tsx
+++ frontend/pages/admin/analysis/index.tsx
@@ -1,13 +1,68 @@
 import AdminHeader from "@/components/header/adminHeader";
+import { Business, LocationOn, Schedule, School } from "@mui/icons-material";
+import { Grid, Paper, Typography } from "@mui/material";
+import Link from "next/link";
+import { Card, Col, Container, Row } from "react-bootstrap";
 
 export default function AdminAnalysis() {
+    interface CardProps {
+        id: number;
+        title: string;
+        description: string;
+        icon: React.ReactNode;
+        url: string;
+      }
+      
+      const cardData: CardProps[] = [
+        {
+            id: 1,
+            title: "Students",
+            description: "Overzicht gewerkte tijden per ronde en per gebouw",
+            icon: <School />,
+            url: "analysis/students",
+          },
+          {
+            id: 2,
+            title: "Tours",
+            description: "Overzicht gespendeerde tijd versus gebudgeteerde tijd",
+            icon: <LocationOn />,
+            url: "analysis/tours",
+          },
+          {
+            id: 3,
+            title: "Working Hours",
+            description: "Overzicht gewerkte tijden per student per week",
+            icon: <Schedule />,
+            url: "analysis/working-hours",
+          },
+      ];
+
     return (
         <>
             <>
                 <AdminHeader />
-                <p>
-                    https://www.figma.com/proto/9yLULhNn8b8SlsWlOnRSpm/SeLab2-mockup?node-id=81-383&scaling=contain&page-id=0%3A1&starting-point-node-id=118%3A1486
-                </p>
+                <Container fluid className="mt-3">
+                    <Row xs={1} sm={2} md={3} className="g-3">
+                    {cardData.map((data) => (
+                        <Col key={data.id}>
+                        <Link href={data.url}>
+                            <Card>
+                            <Card.Body>
+                                <div
+                                className="d-flex justify-content-center align-items-center mb-3"
+                                style={{ height: "60%", background: "#f1f1f1" }}
+                                >
+                                {data.icon}
+                                </div>
+                                <Card.Title>{data.title}</Card.Title>
+                                <Card.Text>{data.description}</Card.Text>
+                            </Card.Body>
+                            </Card>
+                        </Link>
+                        </Col>
+                    ))}
+                    </Row>
+                </Container>
             </>
         </>
     );
