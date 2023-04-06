e61abfd68d1cbbaa84e4f2c17e05807c3fab2929
n00bS-oWn-m3 <n00bS-oWn-m3@users.noreply.github.com>
2023-04-06 15:26:24 +0000

    Auto formatted code

--- backend/building_on_tour/tests.py
+++ backend/building_on_tour/tests.py
@@ -14,21 +14,13 @@ class BuildingOnTourTests(BaseTest):
     def test_insert_building_on_tour(self):
         t_id = insert_dummy_tour()
         b_id = insert_dummy_building()
-        self.data1 = {
-            "tour": t_id,
-            "building": b_id,
-            "index": 0
-        }
+        self.data1 = {"tour": t_id, "building": b_id, "index": 0}
         self.insert("building-on-tour/")
 
     def test_insert_dupe_building_on_tour(self):
         t_id = insert_dummy_tour()
         b_id = insert_dummy_building()
-        self.data1 = {
-            "tour": t_id,
-            "building": b_id,
-            "index": 0
-        }
+        self.data1 = {"tour": t_id, "building": b_id, "index": 0}
 
         self.insert_dupe(f"building-on-tour/")
 
@@ -45,37 +37,21 @@ class BuildingOnTourTests(BaseTest):
         BoT_id = insert_dummy_building_on_tour()
         t_id = insert_dummy_tour()
         b_id = insert_dummy_building()
-        self.data1 = {
-            "tour": t_id,
-            "building": b_id,
-            "index": 0
-        }
+        self.data1 = {"tour": t_id, "building": b_id, "index": 0}
         self.patch(f"building-on-tour/{BoT_id}")
 
     def test_patch_invalid_building_on_tour(self):
         t_id = insert_dummy_tour()
         b_id = insert_dummy_building()
-        self.data1 = {
-            "tour": t_id,
-            "building": b_id,
-            "index": 0
-        }
+        self.data1 = {"tour": t_id, "building": b_id, "index": 0}
         self.patch_invalid("building-on-tour/")
 
     def test_patch_error_building_on_tour(self):
         t_id = insert_dummy_tour()
         b_id1 = insert_dummy_building()
         b_id2 = insert_dummy_building(street="Zuid")
-        self.data1 = {
-            "tour": t_id,
-            "building": b_id1,
-            "index": 0
-        }
-        self.data2 = {
-            "tour": t_id,
-            "building": b_id2,
-            "index": 1
-        }
+        self.data1 = {"tour": t_id, "building": b_id1, "index": 0}
+        self.data2 = {"tour": t_id, "building": b_id2, "index": 1}
         self.patch_error("building-on-tour/")
 
     def test_remove_building_on_tour(self):
@@ -91,70 +67,32 @@ class BuildingOnTourAuthorizationTests(BaseAuthTest):
         super().__init__(methodName)
 
     def test_building_on_tour_list(self):
-        codes = {
-            "Default": 403,
-            "Admin": 200,
-            "Superstudent": 200,
-            "Student": 200,
-            "Syndic": 403
-        }
+        codes = {"Default": 403, "Admin": 200, "Superstudent": 200, "Student": 200, "Syndic": 403}
         self.list_view("building-on-tour/", codes)
 
     def test_insert_building_on_tour(self):
-        codes = {
-            "Default": 403,
-            "Admin": 201,
-            "Superstudent": 201,
-            "Student": 403,
-            "Syndic": 403
-        }
+        codes = {"Default": 403, "Admin": 201, "Superstudent": 201, "Student": 403, "Syndic": 403}
         t_id = insert_dummy_tour()
         b_id = insert_dummy_building()
-        self.data1 = {
-            "tour": t_id,
-            "building": b_id,
-            "index": 0
-        }
+        self.data1 = {"tour": t_id, "building": b_id, "index": 0}
         self.insert_view("building-on-tour/", codes)
 
     def test_get_building_on_tour(self):
-        codes = {
-            "Default": 403,
-            "Admin": 200,
-            "Superstudent": 200,
-            "Student": 200,
-            "Syndic": 403
-        }
+        codes = {"Default": 403, "Admin": 200, "Superstudent": 200, "Student": 200, "Syndic": 403}
         BoT_id = insert_dummy_building_on_tour()
         self.get_view(f"building-on-tour/{BoT_id}", codes)
 
     def test_patch_building_on_tour(self):
-        codes = {
-            "Default": 403,
-            "Admin": 200,
-            "Superstudent": 200,
-            "Student": 403,
-            "Syndic": 403
-        }
+        codes = {"Default": 403, "Admin": 200, "Superstudent": 200, "Student": 403, "Syndic": 403}
         BoT_id = insert_dummy_building_on_tour()
         t_id = insert_dummy_tour()
         b_id = insert_dummy_building(street="Zuid")
-        self.data2 = {
-            "tour": t_id,
-            "building": b_id,
-            "index": 1
-        }
+        self.data2 = {"tour": t_id, "building": b_id, "index": 1}
         self.patch_view(f"building-on-tour/{BoT_id}", codes)
 
     def test_remove_building_on_tour(self):
         def create():
             return insert_dummy_building_on_tour()
 
-        codes = {
-            "Default": 403,
-            "Admin": 204,
-            "Superstudent": 204,
-            "Student": 403,
-            "Syndic": 403
-        }
+        codes = {"Default": 403, "Admin": 204, "Superstudent": 204, "Student": 403, "Syndic": 403}
         self.remove_view("building-on-tour/", codes, create=create)
--- frontend/pages/admin/data/buildings/edit.tsx
+++ frontend/pages/admin/data/buildings/edit.tsx
@@ -1,6 +1,6 @@
-import React, {useEffect, useState, ChangeEvent, FormEvent, MouseEventHandler} from "react";
-import {useRouter} from "next/router";
-import {Form, Alert} from "react-bootstrap";
+import React, { useEffect, useState, ChangeEvent, FormEvent, MouseEventHandler } from "react";
+import { useRouter } from "next/router";
+import { Form, Alert } from "react-bootstrap";
 import {
     BuildingPostInterface,
     getBuildingInfo,
@@ -8,10 +8,10 @@ import {
     patchBuilding,
     postBuilding,
 } from "@/lib/building";
-import {getRegion} from "@/lib/region";
-import {getUserInfo, userSearchString} from "@/lib/user";
+import { getRegion } from "@/lib/region";
+import { getUserInfo, userSearchString } from "@/lib/user";
 import AdminHeader from "@/components/header/adminHeader";
-import {withAuthorisation} from "@/components/withAuthorisation";
+import { withAuthorisation } from "@/components/withAuthorisation";
 import RegionAutocomplete from "@/components/autocompleteComponents/regionAutocomplete";
 import SyndicAutoCompleteComponent from "@/components/autocompleteComponents/syndicAutoComplete";
 import PDFUploader from "@/components/pdfUploader";
@@ -117,7 +117,7 @@ function AdminDataBuildingsEdit() {
 
     return (
         <>
-            <AdminHeader/>
+            <AdminHeader />
             <div className={styles.container}>
                 <ConfirmationMessage
                     showConfirm={showConfirmation}
