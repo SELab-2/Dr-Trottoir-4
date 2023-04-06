07739b8870c50069059fc24f5590303abacb5d8a
Sheng Tao <shengtao.tian1@gmail.com>
2023-04-06 12:08:25 +0200

    #276 fixed region patch

--- frontend/lib/region.ts
+++ frontend/lib/region.ts
@@ -25,9 +25,13 @@ export async function deleteRegion(regionId: number): Promise<AxiosResponse<any>
     return await api.delete(request_url);
 }
 
-export async function patchRegion(regionId: number): Promise<AxiosResponse<any>> {
+export async function patchRegion(regionId: number, regionName: RegionPostInterface): Promise<AxiosResponse<any>> {
     const request_url: string = `${process.env.NEXT_PUBLIC_BASE_API_URL}${process.env.NEXT_PUBLIC_API_REGION}${regionId}`;
-    return await api.patch(request_url);
+    return await api.patch(request_url, JSON.stringify(regionName),
+        {
+            headers: {"Content-Type": "application/json"},
+        }
+    );
 }
 
 export async function postRegion(region: RegionPostInterface): Promise<AxiosResponse<any>> {
