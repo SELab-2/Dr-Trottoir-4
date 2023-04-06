8ee420c32c6f09c960ec81936b3be0d08d85071e
n00bS-oWn-m3 <n00bS-oWn-m3@users.noreply.github.com>
2023-04-06 13:30:09 +0000

    Auto formatted code

--- backend/base/migrations/0002_buildingontour_unique_index_on_tour.py
+++ backend/base/migrations/0002_buildingontour_unique_index_on_tour.py
@@ -4,14 +4,18 @@ from django.db import migrations, models
 
 
 class Migration(migrations.Migration):
-
     dependencies = [
-        ('base', '0001_initial'),
+        ("base", "0001_initial"),
     ]
 
     operations = [
         migrations.AddConstraint(
-            model_name='buildingontour',
-            constraint=models.UniqueConstraint(models.F('index'), models.F('tour'), name='unique_index_on_tour', violation_error_message='This index is already in use.'),
+            model_name="buildingontour",
+            constraint=models.UniqueConstraint(
+                models.F("index"),
+                models.F("tour"),
+                name="unique_index_on_tour",
+                violation_error_message="This index is already in use.",
+            ),
         ),
     ]
