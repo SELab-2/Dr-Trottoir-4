f4f3cacb3abb82a880e2063892e1d71cfc983b99
simvadnbu <simivdb@gmail.com>
2023-04-06 15:34:30 +0200

    add schedule

--- frontend/pages/student/schedule.tsx
+++ frontend/pages/student/schedule.tsx
@@ -1,6 +1,10 @@
 import StudentHeader from "@/components/header/studentHeader";
+import {useRouter} from "next/router";
+
 
 export default function StudentSchedule() {
+    const router = useRouter();
+
     return (
         <>
             <StudentHeader/>
