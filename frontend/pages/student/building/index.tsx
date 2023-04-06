909be72dd3b435da6d1138a8dd568873b7b09b28
simvadnbu <simivdb@gmail.com>
2023-04-06 23:35:22 +0200

    Added ability to download picture

--- frontend/pages/student/building/index.tsx
+++ frontend/pages/student/building/index.tsx
@@ -1,65 +1,55 @@
-import React, {useState} from "react";
+import React, {useEffect, useState} from "react";
 import {Button, Form} from "react-bootstrap";
 
 export default function StudentBuilding() {
 
     const [files, setFiles] = useState<File[]>([]);
-    const [numFiles, setNumFiles] = useState<number>(1);
-    const [startTime, setStartTime] = useState<Date>();
-    const [fileFields, setFileFields] = useState<JSX.Element[]>([
-        (
-            <Form.Group key={0}>
-                <Form.Label>Voeg bestand toe</Form.Label>
-                <Form.Control type="file" onChange={handleFileChange} accept="image/*"/>
-            </Form.Group>
-        )
-    ])
+
+    useEffect(() => {
+        console.log(files);
+    }, []);
 
     function handleFileChange(event: React.ChangeEvent<HTMLInputElement>) {
-        const newFiles: FileList | null = event.target.files;
-        if (!newFiles) {
+        const newFiles : FileList | null = event.target.files;
+        if (! newFiles) {
             return;
         }
-        if (numFiles === 1 && !startTime) {
-            setStartTime(new Date())
-        }
-        const f : File = newFiles[0];
-        setFiles([...files, f]);
+        setFiles([...files, newFiles[0]]);
     }
 
-    function handleSubmit(event: React.FormEvent<HTMLFormElement>) {
-        event.preventDefault();
-        console.log(files);
-    }
-
-    function removeFile(index: number) {
+   function handleRemoveFile(index: number) {
         const newFiles = [...files];
-        const newFileFields = [...fileFields];
-
         newFiles.splice(index, 1);
-        newFileFields.splice(index, 1);
-
         setFiles(newFiles);
-        setFileFields(newFileFields);
-        setNumFiles(numFiles - 1);
+    }
+
+    function handleSubmit(e : React.FormEvent<HTMLFormElement>) {
+        e.preventDefault();
+        // Submit files to server
+    }
+
+    function downloadFile(file: File) {
+        const fileUrl = URL.createObjectURL(file);
+        const fileName = file.name;
+        return (
+            <a href={fileUrl} download={fileName}>
+                {fileName}
+            </a>
+        );
     }
 
     return (
-        <div>
-            <Form onSubmit={handleSubmit}>
-                {fileFields}
-                <Button onClick={() => {
-                    setNumFiles(numFiles + 1);
-                    setFileFields([...fileFields, (
-                        <Form.Group key={fileFields.length}>
-                            <Form.Label>Voeg bestand toe</Form.Label>
-                            <Form.Control type="file" onChange={handleFileChange} accept="image/*"/>
-                            <Button onClick={() => removeFile(fileFields.length)}>Remove file</Button>
-                        </Form.Group>
-                    )])
-                }}>Upload nog bestanden</Button>
-                <Button type="submit">Upload</Button>
-            </Form>
-        </div>
+        <Form onSubmit={handleSubmit}>
+            <div>
+                <label className="form-label">Upload een foto</label>
+                <input className="form-control" type="file" onChange={handleFileChange} accept="image/*"/>
+            </div>
+            <br />
+            {files.map((file, index) => (
+                <div key={index}>{downloadFile(file)}</div>
+            ))}
+            <br />
+            <button type="submit">Submit</button>
+        </Form>
     );
 }
