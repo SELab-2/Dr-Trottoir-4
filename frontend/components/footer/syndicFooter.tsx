c4cdc8b4250adb4b50efd4b51814979551a54808
Tibo Stroo <tibo.stroo@hotmail.be>
2023-04-10 19:56:56 +0200

    Better syndic footer (#188, #189)

--- frontend/components/footer/syndicFooter.tsx
+++ frontend/components/footer/syndicFooter.tsx
@@ -2,18 +2,18 @@ import React from "react";
 
 const SyndicFooter = () => {
     return (
-        <nav className="navbar navbar-expand-sm navbar-dark bg-dark fixed-bottom"
-             style={{position: "absolute", bottom: "0", width: "100%"}}
-        >
-            <div className="container-fluid" style={{color: "yellow"}}>
+            <footer className="navbar navbar-expand-sm navbar-dark bg-dark"
+                 style={{position: "absolute", bottom: "0", width: "100%"}}
+            >
+                <div className="container-fluid" style={{color: "yellow"}}>
                 <span style={{margin: "auto auto"}}>
                     Vragen? Contacteer:{" "}
                     <a style={{color: "yellow", textDecoration: "underline"}} href="mailto:help@drtrottoir.be">
                         help@drtrottoir.be
                     </a>
                 </span>
-            </div>
-        </nav>
+                </div>
+            </footer>
     );
 };
 
