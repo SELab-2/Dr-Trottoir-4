e1a4348d991a16efbe1a0fb703d18cc50c8368d7
Tibo Stroo <tibo.stroo@hotmail.be>
2023-04-10 18:02:13 +0200

    Finished syndic building overview (#188)

--- frontend/components/footer/syndicFooter.tsx
+++ frontend/components/footer/syndicFooter.tsx
@@ -2,11 +2,13 @@ import React from "react";
 
 const SyndicFooter = () => {
     return (
-        <nav className="navbar navbar-expand-sm navbar-dark bg-dark fixed-bottom">
-            <div className="container-fluid" style={{ color: "yellow" }}>
-                <span style={{ margin: "auto auto" }}>
-                    Vragen? Contacteer{" "}
-                    <a style={{ color: "yellow", textDecoration: "underline" }} href="mailto:help@drtrottoir.be">
+        <nav className="navbar navbar-expand-sm navbar-dark bg-dark fixed-bottom"
+             style={{position: "absolute", bottom: "0", width: "100%"}}
+        >
+            <div className="container-fluid" style={{color: "yellow"}}>
+                <span style={{margin: "auto auto"}}>
+                    Vragen? Contacteer:{" "}
+                    <a style={{color: "yellow", textDecoration: "underline"}} href="mailto:help@drtrottoir.be">
                         help@drtrottoir.be
                     </a>
                 </span>
