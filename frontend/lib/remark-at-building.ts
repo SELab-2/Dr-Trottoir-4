b4ef21eb588ac7b9cc02ba7a06687017655554a8
Tibo Stroo <tibo.stroo@hotmail.be>
2023-04-10 23:22:38 +0200

    Create util files to query data for syndic views (#189)

---
+++ frontend/lib/remark-at-building.ts
@@ -0,0 +1,44 @@
+import {AxiosResponse} from "axios";
+import api from "@/lib/api/axios";
+
+export interface RemarkAtBuildingInterface {
+    id: number;
+    student_on_tour: number;
+    building: number;
+    timestamp?: Date;
+    remark?: string;
+    type: "AA" | "BI" | "VE" | "OP";
+}
+
+export function translateRemartAtBuildingType(type: RemarkAtBuildingInterface["type"]) {
+    switch (type) {
+        case "AA":
+            return "Aankomst";
+        case "BI":
+            return "Binnen";
+        case "VE":
+            return "Vertrek";
+        case "OP":
+            return "Opmerking";
+    }
+}
+
+export async function getRemarksAtBuildingOfSpecificBuilding(buildingId: number, mostRecent = false): Promise<AxiosResponse<any>> {
+    let request_url: string = `${process.env.NEXT_PUBLIC_BASE_API_URL}${process.env.NEXT_PUBLIC_API_REMARKS_OF_A_BUILDING}${buildingId}`;
+
+    if (mostRecent) {
+        request_url += `?most-recent=true`;
+    }
+
+    console.log(request_url);
+
+    return await api.get(request_url);
+}
+
+
+
+
+
+
+
+
