506fccfd78bfd69eb468f53f5457ae571343b0fb
simvadnbu <simvadnbu@users.noreply.github.com>
2023-04-13 14:48:07 +0000

    Auto formatted code

--- frontend/components/student/FileList.tsx
+++ frontend/components/student/FileList.tsx
@@ -1,34 +1,25 @@
-import {IconButton, Tooltip} from "@mui/material";
-import {Delete} from "@mui/icons-material";
+import { IconButton, Tooltip } from "@mui/material";
+import { Delete } from "@mui/icons-material";
 import React from "react";
 
-export function FileList(
-    {
-        files,
-        handleRemoveFile
-    }: {
-        files : File[];
-        handleRemoveFile : (index: number) => void;
-    }
-) {
-
+export function FileList({ files, handleRemoveFile }: { files: File[]; handleRemoveFile: (index: number) => void }) {
     return (
         <ol>
             {files.map((file, index) => {
                 const fileUrl = URL.createObjectURL(file); // Get the url to download the image
                 return (
                     <li key={index}>
-                        <a href={fileUrl} download style={{ textDecoration: "underline" }}>{file.name}</a>
+                        <a href={fileUrl} download style={{ textDecoration: "underline" }}>
+                            {file.name}
+                        </a>
                         <Tooltip arrow placement="right" title="Verwijder">
                             <IconButton onClick={() => handleRemoveFile(index)}>
-                                <Delete/>
+                                <Delete />
                             </IconButton>
                         </Tooltip>
                     </li>
                 );
-            }
-            )}
+            })}
         </ol>
     );
-
 }
