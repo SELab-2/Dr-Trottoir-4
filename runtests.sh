7d4fb0f384dd4a3eda2b87d2b7e8d50aab10a702
n00bS-oWn-m3 <sebastiaan@onyx.be>
2023-04-17 14:27:17 +0200

    possible fix

--- runtests.sh
+++ runtests.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
-docker compose exec -T backend python manage.py test lobby/tests.py email_template/tests.py building/tests.py role/tests.py building_comment/tests.py building_on_tour/tests.py garbage_collection/tests.py manual/tests.py region/tests.py tour/tests.py --with-coverage --cover-package=lobby,email_template,building,building_on_tour,garbage_collection,manual,region,tour,building_comment,role
+docker compose exec -t backend python manage.py test lobby/tests.py email_template/tests.py building/tests.py role/tests.py building_comment/tests.py building_on_tour/tests.py garbage_collection/tests.py manual/tests.py region/tests.py tour/tests.py --with-coverage --cover-package=lobby,email_template,building,building_on_tour,garbage_collection,manual,region,tour,building_comment,role
 
 # wait script
 echo "Press any key to continue"
