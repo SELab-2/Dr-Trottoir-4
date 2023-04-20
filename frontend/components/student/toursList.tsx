ab1c40e5a74a0fbb00bff392b19c547f5c288372
simvadnbu <simvadnbu@users.noreply.github.com>
2023-04-20 11:47:20 +0000

    Auto formatted code

--- frontend/components/student/toursList.tsx
+++ frontend/components/student/toursList.tsx
@@ -1,17 +1,17 @@
-import {StudentOnTour} from "@/lib/student-on-tour";
-import {Tour} from "@/lib/tour";
-import {RegionInterface} from "@/lib/region";
+import { StudentOnTour } from "@/lib/student-on-tour";
+import { Tour } from "@/lib/tour";
+import { RegionInterface } from "@/lib/region";
 
 /**
  * A component for the list of the tours a student is/was assigned to
  */
 export default function ToursList({
-                                      listTitle,
-                                      studentOnTours,
-                                      allTours,
-                                      allRegions,
-                                      onSelect,
-                                  }: {
+    listTitle,
+    studentOnTours,
+    allTours,
+    allRegions,
+    onSelect,
+}: {
     listTitle: String;
     studentOnTours: StudentOnTour[];
     allTours: Record<number, Tour>;
@@ -22,11 +22,7 @@ export default function ToursList({
         <>
             <div className="mt-3 mb-1 ms-2 me-2">
                 <span className="h1 fw-bold">{listTitle}</span>
-                {
-                    (studentOnTours.length === 0) && (
-                        <h5 className="mb-1">Er zijn geen ronde om weer te geven.</h5>
-                    )
-                }
+                {studentOnTours.length === 0 && <h5 className="mb-1">Er zijn geen ronde om weer te geven.</h5>}
                 {studentOnTours.length > 0 && (
                     <div className="list-group">
                         {studentOnTours.map((el) => {
