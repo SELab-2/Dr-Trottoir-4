605a1b88d4835f54f48d8abbd18f987b3743fc2a
Sheng Tao <shengtao.tian1@gmail.com>
2023-04-20 12:47:15 +0200

    #271 generalized PasswordInput

--- frontend/components/loginForm.tsx
+++ frontend/components/loginForm.tsx
@@ -14,6 +14,7 @@ import Visibility from '@mui/icons-material/Visibility';
 import VisibilityOff from '@mui/icons-material/VisibilityOff';
 import InputAdornment from '@mui/material/InputAdornment';
 import TextField from '@mui/material/TextField';
+import PasswordInput from "@/components/passwordInput";
 
 
 function LoginForm() {
@@ -77,38 +78,10 @@ function LoginForm() {
                 />
             </div>
 
-            <div className="form-outline mb-4">
-                <label className="form-label">Wachtwoord</label>
-                <div style={{position: 'relative'}}>
-                    <input
-                        type={showPassword ? 'text' : 'password'}
-                        className={`form-control form-control-lg ${styles.input}`}
-                        value={password}
-                        onChange={(e: React.ChangeEvent<HTMLInputElement>) => {
-                            setPassword(e.target.value);
-                            e.target.setCustomValidity("");
-                        }}
-                        onInvalid={(e: React.ChangeEvent<HTMLInputElement>) => {
-                            e.target.setCustomValidity("Wachtwoord is verplicht.");
-                        }}
-                        required
-                        placeholder="Wachtwoord"
-                        style={{paddingRight: '40px'}}
-                    />
-                    <IconButton
-                        onClick={handlePasswordVisibility}
-                        edge="end"
-                        style={{
-                            position: 'absolute',
-                            top: '50%',
-                            right: '12px',
-                            transform: 'translateY(-50%)',
-                        }}
-                    >
-                        {showPassword ? <Visibility/> : <VisibilityOff/>}
-                    </IconButton>
-                </div>
-            </div>
+            <PasswordInput value={password} setPassword={setPassword}
+                           handlePasswordVisibility={handlePasswordVisibility} showPassword={showPassword}
+                           label={"Wachtwoord"}
+                           placeholder={"Wachtwoord"}></PasswordInput>
 
             <div className="pt-1 mb-4">
                 <button className={`btn btn-dark btn-lg btn-block ${styles.button}`} type="submit">
@@ -126,7 +99,8 @@ function LoginForm() {
                 </Link>
             </p>
         </form>
-    );
+    )
+        ;
 }
 
 export default LoginForm;
