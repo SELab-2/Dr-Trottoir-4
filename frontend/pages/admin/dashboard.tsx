fc0ab42805711f09e7963db4cd4b6cd6f9cc7022
n00bS-oWn-m3 <n00bS-oWn-m3@users.noreply.github.com>
2023-04-20 12:27:02 +0000

    Auto formatted code

--- frontend/pages/admin/dashboard.tsx
+++ frontend/pages/admin/dashboard.tsx
@@ -1,23 +1,27 @@
-import React, {useEffect, useState} from 'react';
-import {useRouter} from 'next/router';
-import {getAllTours, Tour} from '@/lib/tour';
-import {getAllStudentOnTourFromDate, getAllStudentOnTourFromToday, StudentOnTour} from '@/lib/student-on-tour';
-import {getAllUsers, User} from '@/lib/user';
+import React, { useEffect, useState } from "react";
+import { useRouter } from "next/router";
+import { getAllTours, Tour } from "@/lib/tour";
+import { getAllStudentOnTourFromDate, getAllStudentOnTourFromToday, StudentOnTour } from "@/lib/student-on-tour";
+import { getAllUsers, User } from "@/lib/user";
 import AdminHeader from "@/components/header/adminHeader";
-import {withAuthorisation} from "@/components/withAuthorisation";
+import { withAuthorisation } from "@/components/withAuthorisation";
 import Loading from "@/components/loading";
-import LinearProgress from '@mui/material/LinearProgress';
-import Box from '@mui/material/Box';
-import {styled} from '@mui/system';
-import LiveField from '@/components/liveField';
-import { BuildingOnTour, getAllBuildingsOnTourWithTourID } from '@/lib/building-on-tour';
-import { getRemarksAtBuildingOfSpecificBuilding, RemarkAtBuildingInterface, translateRemartAtBuildingType } from '@/lib/remark-at-building';
+import LinearProgress from "@mui/material/LinearProgress";
+import Box from "@mui/material/Box";
+import { styled } from "@mui/system";
+import LiveField from "@/components/liveField";
+import { BuildingOnTour, getAllBuildingsOnTourWithTourID } from "@/lib/building-on-tour";
+import {
+    getRemarksAtBuildingOfSpecificBuilding,
+    RemarkAtBuildingInterface,
+    translateRemartAtBuildingType,
+} from "@/lib/remark-at-building";
 
 const GreenLinearProgress = styled(LinearProgress)(() => ({
-    height: '20px',
-    backgroundColor: '#c8e6c9', // Light green background color
-    '& .MuiLinearProgress-bar': {
-        backgroundColor: '#4caf50', // Main green progress bar color
+    height: "20px",
+    backgroundColor: "#c8e6c9", // Light green background color
+    "& .MuiLinearProgress-bar": {
+        backgroundColor: "#4caf50", // Main green progress bar color
     },
 }));
 
@@ -30,16 +34,16 @@ function AdminDashboard() {
     const [remarksRecord, setRemarksRecord] = useState<Record<string, number>>({});
     const [progressRecord, setProgressRecord] = useState<Record<string, number>>({});
 
-    const getRemarkText = (numberOfRemarks: number) : string => {
+    const getRemarkText = (numberOfRemarks: number): string => {
         let extension = "";
         if (numberOfRemarks > 1) {
             extension = "en";
         }
 
         return `${numberOfRemarks} opmerking${extension}`;
-    }
+    };
 
-    const fetchRemarks = async (studentOnTour: StudentOnTour) : Promise<number> => {
+    const fetchRemarks = async (studentOnTour: StudentOnTour): Promise<number> => {
         // Fetch remarks based on studentOnTourId
         // Return an array of remarks
         // TODO change this to query the remarks of a specific student_on_tour
@@ -47,20 +51,17 @@ function AdminDashboard() {
         let remarksCount = 0;
         await getAllBuildingsOnTourWithTourID(studentOnTour.tour).then(
             async (buildingRes) => {
-                const buildingsOnTour : BuildingOnTour[] = buildingRes.data;
+                const buildingsOnTour: BuildingOnTour[] = buildingRes.data;
                 for (const buildingOnTour of buildingsOnTour) {
-                    await getRemarksAtBuildingOfSpecificBuilding(buildingOnTour.building).then(
-                        (remarkRes) => {
-                            // don't actually care about the remark, just the count
-                            const remarks : RemarkAtBuildingInterface[] = remarkRes.data;
-                            for (const remark of remarks) {
-                                if (translateRemartAtBuildingType(remark.type) === "Opmerking")
-                                {
-                                    remarksCount++;
-                                }
+                    await getRemarksAtBuildingOfSpecificBuilding(buildingOnTour.building).then((remarkRes) => {
+                        // don't actually care about the remark, just the count
+                        const remarks: RemarkAtBuildingInterface[] = remarkRes.data;
+                        for (const remark of remarks) {
+                            if (translateRemartAtBuildingType(remark.type) === "Opmerking") {
+                                remarksCount++;
                             }
                         }
-                    )
+                    });
                 }
             },
             (err) => {
@@ -70,19 +71,18 @@ function AdminDashboard() {
         return remarksCount;
     };
 
-    const fetchProgress = async (studentOnTourId: number) : Promise<number> => {
+    const fetchProgress = async (studentOnTourId: number): Promise<number> => {
         // Fetch progress
         // TODO update this after the progress can be queried
         return studentOnTourId * 5;
-    }
+    };
 
     const redirectToRemarksPage = async (studentOnTourId: number) => {
         // Redirect to the specific tour page
         await router.push({
             pathname: `tour/`,
-            query: { 
+            query: {
                 studentOnTour: studentOnTourId,
-
             },
         });
     };
@@ -117,80 +117,81 @@ function AdminDashboard() {
             for (const studentOnTour of studentsOnTours) {
                 const remarks = await fetchRemarks(studentOnTour);
                 const progress = await fetchProgress(studentOnTour.id);
-                
+
                 newRemarks[studentOnTour.id] = remarks;
                 newProgress[studentOnTour.id] = progress;
             }
             setRemarksRecord(newRemarks);
             setProgressRecord(newProgress);
-        }
-        
+        };
+
         fetchData();
     }, [tours, studentsOnTours, users]);
 
     if (loading) {
         return (
             <>
-                <AdminHeader/>
-                <Loading/>
+                <AdminHeader />
+                <Loading />
             </>
         );
     }
     return (
         <div>
-            <AdminHeader/>
+            <AdminHeader />
             {studentsOnTours.length > 0 ? (
                 <>
-                <h2>Rondes van vandaag</h2>
-                <table className="table">
-                    <thead>
-                    <tr>
-                        <th>Ronde</th>
-                        <th>Student</th>
-                        <th>Voortgang</th>
-                    </tr>
-                    </thead>
-                    <tbody>
-                    {studentsOnTours.map((studentOnTour) => {
-                        const tour = tours.find((t) => t.id === studentOnTour.tour);
-                        const user = users.find((u) => u.id === studentOnTour.student);
-
-                        if (!tour || !user) return null;
-
-                        return (
-                            <tr key={studentOnTour.id}>
-                                <td>{tour.name}</td>
-                                <td>{`${user.first_name} ${user.last_name}`}</td>
-                                <td>
-                                    <Box sx={{width: '100%'}}>
-                                        <GreenLinearProgress variant="determinate" value={progressRecord[studentOnTour.id] || 0}/>
-                                    </Box>
-                                </td>
-                                <td>
-                                    {remarksRecord[studentOnTour.id] > 0 ? (
-                                        <button onClick={() => redirectToRemarksPage(studentOnTour.id)}>
-                                            <LiveField 
-                                                fetcher={() => fetchRemarks(studentOnTour)}
-                                                formatter={getRemarkText}
-                                                interval={3000}
-                                            />
-                                        </button>
-                                    ) : (
-                                        "Geen opmerkingen"
-                                    )}
-                                </td>
+                    <h2>Rondes van vandaag</h2>
+                    <table className="table">
+                        <thead>
+                            <tr>
+                                <th>Ronde</th>
+                                <th>Student</th>
+                                <th>Voortgang</th>
                             </tr>
-                        );
-                    })}
-                    </tbody>
-                </table>
+                        </thead>
+                        <tbody>
+                            {studentsOnTours.map((studentOnTour) => {
+                                const tour = tours.find((t) => t.id === studentOnTour.tour);
+                                const user = users.find((u) => u.id === studentOnTour.student);
+
+                                if (!tour || !user) return null;
+
+                                return (
+                                    <tr key={studentOnTour.id}>
+                                        <td>{tour.name}</td>
+                                        <td>{`${user.first_name} ${user.last_name}`}</td>
+                                        <td>
+                                            <Box sx={{ width: "100%" }}>
+                                                <GreenLinearProgress
+                                                    variant="determinate"
+                                                    value={progressRecord[studentOnTour.id] || 0}
+                                                />
+                                            </Box>
+                                        </td>
+                                        <td>
+                                            {remarksRecord[studentOnTour.id] > 0 ? (
+                                                <button onClick={() => redirectToRemarksPage(studentOnTour.id)}>
+                                                    <LiveField
+                                                        fetcher={() => fetchRemarks(studentOnTour)}
+                                                        formatter={getRemarkText}
+                                                        interval={3000}
+                                                    />
+                                                </button>
+                                            ) : (
+                                                "Geen opmerkingen"
+                                            )}
+                                        </td>
+                                    </tr>
+                                );
+                            })}
+                        </tbody>
+                    </table>
                 </>
             ) : (
                 <h2>Geen rondes vandaag</h2>
-            )
-            }
+            )}
         </div>
-
     );
 }
 
