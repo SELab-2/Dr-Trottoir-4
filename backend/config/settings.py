ca502c65366de467a32d8431041ab41b77cec6a1
jonathancasters <jonathancasters284@gmail.com>
2023-04-29 14:38:06 +0200

    removing wsgi gunicorn server + updating psycopg2 to psycopg3

--- backend/config/settings.py
+++ backend/config/settings.py
@@ -192,7 +192,7 @@ WSGI_APPLICATION = "config.wsgi.application"
 
 DATABASES = {
     "default": {
-        "ENGINE": "django.db.backends.postgresql_psycopg2",
+        "ENGINE": "django.db.backends.postgresql",
         "NAME": os.environ["POSTGRES_DB"],
         "USER": os.environ["POSTGRES_USER"],
         "PASSWORD": os.environ["POSTGRES_PASSWORD"],
