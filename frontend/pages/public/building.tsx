9f74a83954b0794cba6fb191af5022272b4a78ab
TiboStr <TiboStr@users.noreply.github.com>
2023-04-30 20:17:54 +0000

    Auto formatted code

--- frontend/pages/public/building.tsx
+++ frontend/pages/public/building.tsx
@@ -2,16 +2,13 @@ import React from "react";
 import BuildingPage from "@/components/building/BuildingPage";
 import BaseHeader from "@/components/header/baseHeader";
 
-
 function PublicBuilding() {
-
     return (
         <>
-            <BaseHeader/>
-            <BuildingPage type={"public"}/>
+            <BaseHeader />
+            <BuildingPage type={"public"} />
         </>
     );
-
 }
 
 export default PublicBuilding;
