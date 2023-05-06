df941bc79e2186760b7ad8f06fe52bde430e5bb5
Simon Van den Bussche <simon.vandenbussche@ugent.be>
2023-05-06 11:24:45 +0200

    Changed global styling

--- frontend/pages/_app.tsx
+++ frontend/pages/_app.tsx
@@ -1,4 +1,5 @@
 import "bootstrap/dist/css/bootstrap.min.css";
+import "styles/globals.css";
 import type { AppProps } from "next/app";
 import { useEffect } from "react";
 import { I18nextProvider } from "react-i18next";
