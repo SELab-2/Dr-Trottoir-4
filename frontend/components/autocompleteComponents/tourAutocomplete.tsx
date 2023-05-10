3b9926b6ceef78e80d9f5f9325948ddda87ef49b
Sheng Tao <shengtao.tian1@gmail.com>
2023-05-10 22:39:09 +0200

    #91 test setup complete

--- frontend/components/autocompleteComponents/tourAutocomplete.tsx
+++ frontend/components/autocompleteComponents/tourAutocomplete.tsx
@@ -1,9 +1,9 @@
 import React from "react";
-import AutocompleteComponent, { GenericProps } from "@/components/autocompleteComponents/autocompleteComponent";
-import { BuildingInterface, getAddress, getAllBuildings } from "@/lib/building";
-import { getAllTours, Tour } from "@/lib/tour";
+import AutocompleteComponent, {GenericProps} from "@/components/autocompleteComponents/autocompleteComponent";
+import {BuildingInterface, getAddress, getAllBuildings} from "@/lib/building";
+import {getAllTours, Tour} from "@/lib/tour";
 
-const TourAutocomplete: React.FC<GenericProps> = ({ initialId, setObjectId, required }) => {
+const TourAutocomplete: React.FC<GenericProps> = ({initialId, setObjectId, required}) => {
     return (
         <AutocompleteComponent
             initialId={initialId}
--- frontend/jest.config.js
+++ frontend/jest.config.js
@@ -4,12 +4,15 @@ module.exports = {
     moduleFileExtensions: ['ts', 'tsx', 'js', 'jsx', 'json', 'node'],
     testPathIgnorePatterns: ['/node_modules/', '/.next/'],
     transform: {
-        '^.+\\.(ts|tsx)$': 'ts-jest',
+        // Use babel-jest to transpile tests with the next/babel preset
+        // https://jestjs.io/docs/configuration#transform-objectstring-pathtotransformer--pathtotransformer-object
+        '^.+\\.(js|jsx|ts|tsx)$': ['babel-jest', {presets: ['next/babel']}],
     },
     moduleNameMapper: {
         '^@/(.*)$': '<rootDir>/$1',
         '\\.(css|less|scss|sass)$': 'identity-obj-proxy'
     },
+    testEnvironment: 'jsdom',
     setupFilesAfterEnv: ['@testing-library/jest-dom/extend-expect'],
     testMatch: ['**/__tests__/**/*.+(ts|tsx|js)', '**/?(*.)+(spec|test).+(ts|tsx|js)'],
 }
