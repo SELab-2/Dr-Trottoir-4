f41846fb64e07ef5df0d4860ee962e3627cae9d5
Simon Van den Bussche <simon.vandenbussche@ugent.be>
2023-05-13 09:39:51 +0200

    Add progressbar

--- frontend/pages/admin/analysis/student-on-tour.tsx
+++ frontend/pages/admin/analysis/student-on-tour.tsx
@@ -1,10 +1,11 @@
 import AdminHeader from "@/components/header/adminHeader";
 import {useRouter} from "next/router";
-import {useEffect, useState} from "react";
+import React, {useEffect, useState} from "react";
 import {getStudentOnTour, StudentOnTour} from "@/lib/student-on-tour";
 import {getAnalysisStudentOnTour} from "@/lib/analysis";
 import {BuildingAnalysis} from "@/types";
-import {BuildingInterface, getBuildingInfo} from "@/lib/building";
+import {BuildingInterface, getAddress, getBuildingInfo} from "@/lib/building";
+import {Badge, Container, ListGroup, ListGroupItem, ProgressBar} from "react-bootstrap";
 
 interface ParsedUrlQuery {
 }
@@ -36,19 +37,60 @@ export default function AnalysisStudentOnTour() {
             setBuildingsAnalysis(b);
 
             Promise.all(b.map(building => getBuildingInfo(building.building_id))).then(res => {
-                const buildings : BuildingInterface[] = res.map(r => r.data);
+                const buildings: BuildingInterface[] = res.map(r => r.data);
                 setBuildings(buildings);
             }, () => {
             });
         }, console.error);
     }, [router.isReady]);
 
+    function convertSecondsToString(seconds: number) {
+        if (seconds < 60) {
+            return `${seconds} s`;
+        }
+        let minutes = ~~(seconds / 60);
+        let extraSeconds = seconds % 60;
+        return `${minutes} m ${extraSeconds > 0 ? `${extraSeconds} s` : ""}`;
+    }
+
+    function getTimeIntervalString(arrivalTime: Date, departureTime: Date): string {
+        const arrivalHours = arrivalTime.getHours();
+        const arrivalMinutes = arrivalTime.getMinutes();
+
+        const departureHours = departureTime.getHours();
+        const departureMinutes = departureTime.getMinutes();
+
+        return `${arrivalHours.toString().padStart(2, "0")}:${arrivalMinutes.toString().padStart(2, "0")} - 
+        ${departureHours.toString().padStart(2, "0")}:${departureMinutes.toString().padStart(2, "0")}`;
+    }
 
     return (
         <>
             <AdminHeader/>
-            <p>{JSON.stringify(buildingsAnalysis)}</p>
-            <p>{JSON.stringify(buildings)}</p>
+
+            <Container fluid="md">
+                <ListGroup>
+                    {
+                        buildingsAnalysis.map((analysis, index) => {
+                            const building: BuildingInterface | undefined = buildings.find(b => b.id === analysis.building_id);
+                            return (
+                                <ListGroupItem key={index}>
+                                    <div className="ms-2 me-auto">
+                                        <div className="fw-bold">{building ? getAddress(building) : ""}</div>
+                                        <p>{getTimeIntervalString(new Date(analysis.arrival_time), new Date(analysis.departure_time))}</p>
+                                    </div>
+                                    <ProgressBar now={20}>
+                                        <div className="progress-line"/>
+                                    </ProgressBar>
+                                    <Badge pill>
+                                        <p>{`${convertSecondsToString(analysis.duration_in_seconds)} vs ${convertSecondsToString(analysis.expected_duration_in_seconds)}`}</p>
+                                    </Badge>
+                                </ListGroupItem>
+                            );
+                        })
+                    }
+                </ListGroup>
+            </Container>
         </>
     );
 }
