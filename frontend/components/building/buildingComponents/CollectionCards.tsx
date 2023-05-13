1458d0251e906d22481fe362ce1f55c4cc0a97be
Tibo Stroo <tibo.stroo@hotmail.be>
2023-05-14 01:45:05 +0200

    Support for date query parameter on building info page (#432)

--- frontend/components/building/buildingComponents/CollectionCards.tsx
+++ frontend/components/building/buildingComponents/CollectionCards.tsx
@@ -7,6 +7,8 @@ import {
 import {getPictureOfRemarkOfSpecificRemark, PictureOfRemarkInterface} from "@/lib/picture-of-remark";
 import {Accordion, Card} from "react-bootstrap";
 import ImageEnlargeModal from "@/components/ImageEnlargeModal";
+import {convertToSensibleDateLong, convertToSensibleDateShort} from "@/lib/dateUtil";
+import {AxiosResponse} from "axios/index";
 
 function CollectionCards({building, date}: { building: number; date: string | null }) {
     const [collectionDetails, setCollectionDetails] = useState<
@@ -16,40 +18,57 @@ function CollectionCards({building, date}: { building: number; date: string | nu
     const [enlargeImageShow, setEnlargeImageShow] = useState<boolean>(false);
     const [enlargeImageURL, setEnlargeImageURL] = useState<string>("");
 
-    useEffect(() => {
-        getRemarksAtBuildingOfSpecificBuilding(building, true)
-            .then((response) => {
-                const responseData: RemarkAtBuildingInterface[] = response.data.sort(
-                    (a: RemarkAtBuildingInterface, b: RemarkAtBuildingInterface) => {
-                        for (let t of ["AA", "BI", "VE", "OP"]) {
-                            if (a.type == t) {
-                                return -1;
-                            }
-                            if (b.type == t) {
-                                return 1;
-                            }
-                        }
+
+    function handleRemarksAtBuildingsCall(response: AxiosResponse) {
+        const responseData: RemarkAtBuildingInterface[] = response.data.sort(
+            (a: RemarkAtBuildingInterface, b: RemarkAtBuildingInterface) => {
+                for (let t of ["AA", "BI", "VE", "OP"]) {
+                    if (a.type == t) {
+                        return -1;
                     }
-                );
+                    if (b.type == t) {
+                        return 1;
+                    }
+                }
+            }
+        );
 
-                const fetchPictures = responseData.map((remark) => getPictureOfRemarkOfSpecificRemark(remark.id));
+        const fetchPictures = responseData.map((remark) => getPictureOfRemarkOfSpecificRemark(remark.id));
 
-                Promise.all(fetchPictures)
-                    .then((responses) => {
-                        const pictureData: PictureOfRemarkInterface[][] = responses.map((response) => response.data);
+        Promise.all(fetchPictures)
+            .then((responses) => {
+                const pictureData: PictureOfRemarkInterface[][] = responses.map((response) => response.data);
 
-                        const newCollectionDetails: [RemarkAtBuildingInterface, PictureOfRemarkInterface[]][] =
-                            responseData.map((remark, index) => [remark, pictureData[index]]);
+                const newCollectionDetails: [RemarkAtBuildingInterface, PictureOfRemarkInterface[]][] =
+                    responseData.map((remark, index) => [remark, pictureData[index]]);
 
-                        setCollectionDetails(newCollectionDetails);
-                    })
-                    .catch((error) => {
-                        console.error(error);
-                    });
+                setCollectionDetails(newCollectionDetails);
             })
             .catch((error) => {
                 console.error(error);
             });
+    }
+
+
+    useEffect(() => {
+        if (date) {
+            getRemarksAtBuildingOfSpecificBuilding(building, {date: date})
+                .then((response) => {
+                    handleRemarksAtBuildingsCall(response);
+                })
+                .catch((error) => {
+                    console.error(error);
+                });
+        } else {
+
+            getRemarksAtBuildingOfSpecificBuilding(building, {mostRecent: true})
+                .then((response) => {
+                    handleRemarksAtBuildingsCall(response);
+                })
+                .catch((error) => {
+                    console.error(error);
+                });
+        }
     }, [building]);
 
     function imageClick(url: string) {
@@ -59,19 +78,10 @@ function CollectionCards({building, date}: { building: number; date: string | nu
 
     return (
         <>
-            {date && (
-                <>
-                    <h4>
-                        Details ophaling op datum '{date}' (dit werkt nog niet, maar gebruik in jullie code gerust al de
-                        link met de query param)
-                    </h4>
-                    <br/>
-                </>
-            )}
 
             <ImageEnlargeModal show={enlargeImageShow} setShow={setEnlargeImageShow} imageURL={enlargeImageURL}/>
 
-            <h1>Details laatste ophaling</h1>
+            <h1>Details laatste ophaling {date ? "(" + convertToSensibleDateShort(date) + ")" : null}</h1>
             <div style={{margin: "0 0", width: "75%", maxWidth: "95%"}}>
                 {collectionDetails.length == 0}
                 <Accordion alwaysOpen>
@@ -79,18 +89,9 @@ function CollectionCards({building, date}: { building: number; date: string | nu
                         <Accordion.Item eventKey={remark.id + ""} key={remark.id}>
                             <Accordion.Header>{translateRemarkAtBuildingType(remark.type)}</Accordion.Header>
                             <Accordion.Body>
-                                <Card.Subtitle className="mb-2 text-muted">{
-                                    new Date(remark.timestamp + "").toLocaleString('nl-BE', {
-                                        weekday: 'long',
-                                        day: 'numeric',
-                                        month: 'long',
-                                        year: 'numeric',
-                                        hour: 'numeric',
-                                        minute: 'numeric',
-                                        timeZone: 'Europe/Brussels',
-                                        timeZoneName: 'short'
-                                    })
-                                }</Card.Subtitle>
+                                <Card.Subtitle className="mb-2 text-muted">
+                                    {convertToSensibleDateLong(remark.timestamp + "")}
+                                </Card.Subtitle>
                                 <Card.Text>{remark.remark}</Card.Text>
                                 <div>
                                     {pictures.map((picture: PictureOfRemarkInterface) => (
