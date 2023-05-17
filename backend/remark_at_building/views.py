34e76dfacf2026bbbb76dd0cf01cf5243ede6982
jonathancasters <jonathancasters284@gmail.com>
2023-05-17 16:40:41 +0200

    Add TODO for translation

--- backend/remark_at_building/views.py
+++ backend/remark_at_building/views.py
@@ -119,6 +119,7 @@ class RemarkAtBuildingIndividualView(APIView):
         forbidden_keys = ['timestamp', 'building', 'student_on_tour', 'type', 'id']
         if any(k in forbidden_keys for k in data.keys()):
             return Response({
+                # TODO: Translate
                 'message': _("You can only edit the 'remark' text on a remark at building")
             }, status=status.HTTP_400_BAD_REQUEST)
 
