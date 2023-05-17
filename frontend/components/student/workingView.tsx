b8b71d1018e51c6553aac9732504ffd46e2a2e79
Simon Van den Bussche <simon.vandenbussche@ugent.be>
2023-05-17 15:44:39 +0200

    set right current building index

--- frontend/components/student/workingView.tsx
+++ frontend/components/student/workingView.tsx
@@ -68,15 +68,13 @@ export function WorkingView({ redirectTo, studentOnTourId }: { redirectTo: strin
                     tour: sots.tour,
                     date: new Date(sots.date),
                     max_building_index: sots.max_building_index - 1,
-                    current_building_index: sots.current_building_index <= 0 ? 0 : sots.current_building_index - 1,
+                    current_building_index: sots.current_building_index <= 1 ? 0 : sots.current_building_index - 1,
                     started_tour: sots.started_tour,
                     completed_tour: sots.completed_tour,
                 };
                 if (sot.completed_tour) {
                     // Tour is completed, redirect to no access
-                    router.replace("/no-access").then((_) => {
-                        console.error("No access");
-                    });
+                    router.replace("/no-access").then((_) => {});
                 }
                 setProgress({
                     step: 0,
