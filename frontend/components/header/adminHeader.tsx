676f5e6a985c33c6c9dce6117391c78fbea05e70
emneirin <emneirin@users.noreply.github.com>
2023-05-18 19:39:41 +0000

    Auto formatted code

--- frontend/components/header/adminHeader.tsx
+++ frontend/components/header/adminHeader.tsx
@@ -6,7 +6,7 @@ import person from "@/public/icons/person.svg";
 import menu from "@/public/icons/menu.svg";
 import Link from "next/link";
 import Logout from "@/components/logout";
-import {Nav, Navbar, NavDropdown} from "react-bootstrap";
+import { Nav, Navbar, NavDropdown } from "react-bootstrap";
 
 const AdminHeader = () => {
     return (
@@ -14,11 +14,13 @@ const AdminHeader = () => {
             <div className="container-fluid">
                 <Link href="/admin/dashboard">
                     <Navbar.Brand>
-                        <Image src={logo} alt="My App Logo" width={120} height={30}/>
+                        <Image src={logo} alt="My App Logo" width={120} height={30} />
                     </Navbar.Brand>
                 </Link>
-                <Navbar.Collapse id="navbarSupportedContent"
-                                 style={{height: '100%', marginBottom: '0px', paddingBottom: '0px'}}>
+                <Navbar.Collapse
+                    id="navbarSupportedContent"
+                    style={{ height: "100%", marginBottom: "0px", paddingBottom: "0px" }}
+                >
                     <Nav className="ms-auto mb-2 mb-lg-0">
                         <Nav.Link className={styles.text} href="/admin/dashboard">
                             Dashboard
@@ -32,12 +34,7 @@ const AdminHeader = () => {
                             <NavDropdown.Item href="/admin/data/schedules">Rondes</NavDropdown.Item>
                             <NavDropdown.Item href="/admin/data/garbage-collection">Vuilophaling</NavDropdown.Item>
                         </NavDropdown>
-                        <NavDropdown
-                            title="Data"
-                            className={styles.text}
-                            id="navbarDropdownData"
-                            menuVariant="dark"
-                        >
+                        <NavDropdown title="Data" className={styles.text} id="navbarDropdownData" menuVariant="dark">
                             <NavDropdown.Item href="/admin/data/users">Gebruikers</NavDropdown.Item>
                             <NavDropdown.Item href="/admin/data/tours">Rondes</NavDropdown.Item>
                             <NavDropdown.Item href="/admin/data/buildings">Gebouwen</NavDropdown.Item>
@@ -56,32 +53,25 @@ const AdminHeader = () => {
                         </Nav.Link>
                     </Nav>
                 </Navbar.Collapse>
-                <div style={{display: 'flex', alignItems: 'center'}}>
+                <div style={{ display: "flex", alignItems: "center" }}>
                     <Navbar.Toggle
                         className={styles.menuIcon}
                         aria-controls="navbarSupportedContent"
                         aria-label="Toggle navigation"
                     >
-                        <Image src={menu} alt="My App Menu" height={24}/>
+                        <Image src={menu} alt="My App Menu" height={24} />
                     </Navbar.Toggle>
                     <NavDropdown
                         id="navbarDropdown1"
                         menuVariant="dark"
                         align="end"
-                        style={{marginLeft: '10px', marginRight: '-10px'}}
-                        title={
-                            <Image
-                                src={person}
-                                alt="My App Logo"
-                                className={styles.person}
-                                height={24}
-                            />
-                        }
+                        style={{ marginLeft: "10px", marginRight: "-10px" }}
+                        title={<Image src={person} alt="My App Logo" className={styles.person} height={24} />}
                     >
                         <NavDropdown.Item href="/user/profile">Bewerk account</NavDropdown.Item>
-                        <NavDropdown.Divider/>
+                        <NavDropdown.Divider />
                         <NavDropdown.Item>
-                            <Logout/>
+                            <Logout />
                         </NavDropdown.Item>
                     </NavDropdown>
                 </div>
