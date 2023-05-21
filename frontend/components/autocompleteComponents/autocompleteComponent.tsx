2989eac68f145e49a11b5b4147450559c59f419e
Emma N <neirinck.emma@gmail.com>
2023-05-21 18:56:26 +0200

    minWidth autocomplete change

--- frontend/components/autocompleteComponents/autocompleteComponent.tsx
+++ frontend/components/autocompleteComponents/autocompleteComponent.tsx
@@ -111,7 +111,7 @@ const AutocompleteComponent: React.FC<Props> = ({
                 getOptionLabel={(option: any) => option.label || ""}
                 disabled={disabled}
                 renderInput={(params: AutocompleteRenderInputParams) => (
-                    <TextField {...params} variant="outlined" fullWidth style={{ minWidth: "300px" }} />
+                    <TextField {...params} variant="outlined" fullWidth style={{ minWidth: "150px" }} />
                 )}
                 ListboxProps={{ style: { maxHeight: 200, overflowY: "auto" } }}
             />
